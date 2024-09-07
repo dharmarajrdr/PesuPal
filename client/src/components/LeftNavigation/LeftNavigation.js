0af3c126d604e9a00086d0ef46b015322ca513e0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 18:41:01 +0530

    Update LeftNavigation component styles and functionality

--- client/src/components/LeftNavigation/LeftNavigation.js
+++ client/src/components/LeftNavigation/LeftNavigation.js
@@ -4,21 +4,35 @@ import ListOfNavigations from './ListOfNavigations'
 import Nav from './Nav'
 
 const LeftNavigation = () => {
+    const hideNavContainer = () => {
+        const LeftNavigationOverlay = document.getElementById('LeftNavigationOverlay'),
+            leftNavContainer = document.getElementById('LeftNavigation');
+        leftNavContainer.style.transition = 'transform 0.25s ease-in-out';
+        leftNavContainer.style.transform = 'translateX(-100%)';
+        setTimeout(() => {
+            LeftNavigationOverlay.style.display = 'none';
+        }, 100);
+    }
     return (
-        <div id='LeftNavigation' className='FCCB'>
-            <div className='w100'>
-                {ListOfNavigations.top.map((navigation, index) => {
-                    return (
-                        <Nav key={index} icon={navigation.icon} title={navigation.title} route={navigation.route} />
-                    )
-                })}
-            </div>
-            <div className='w100'>
-                {ListOfNavigations.bottom.map((navigation, index) => {
-                    return (
-                        <Nav key={index} icon={navigation.icon} title={navigation.title} route={navigation.route} />
-                    )
-                })}
+        <div id='LeftNavigationOverlay'>
+            <div id='LeftNavigation' className='FCCB'>
+                <div className='w100'>
+                    <div>
+                        <i class="fa-solid fa-angles-left" id='closeLeftNav' onClick={hideNavContainer}></i>
+                    </div>
+                    {ListOfNavigations.top.map((navigation, index) => {
+                        return (
+                            <Nav key={index} icon={navigation.icon} title={navigation.title} route={navigation.route} />
+                        )
+                    })}
+                </div>
+                <div className='w100'>
+                    {ListOfNavigations.bottom.map((navigation, index) => {
+                        return (
+                            <Nav key={index} icon={navigation.icon} title={navigation.title} route={navigation.route} />
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
