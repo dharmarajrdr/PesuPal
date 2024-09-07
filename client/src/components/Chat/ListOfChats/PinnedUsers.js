f9bf6adf2a86a0e5a86f33e286fde36ae62246c4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 23:40:34 +0530

    Update CSS and components

--- client/src/components/Chat/ListOfChats/PinnedUsers.js
+++ client/src/components/Chat/ListOfChats/PinnedUsers.js
@@ -6,7 +6,7 @@ import PinnedUser from './PinnedUser';
 const PinnedUsers = () => {
 
     return (
-        <div id='PinnedUsers' className='noScollbar selectNone'>
+        <div id='PinnedUsers' className='noScrollbar selectNone'>
             <div id='PinnedUsersFrame'>
                 {pinnedUsersList.map(({ name, status, image }, index) => (
                     <PinnedUser name={name} status={status} image={image} key={index} />
--- client/src/components/Home/Main.js
+++ client/src/components/Home/Main.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import LeftNavigation from '../LeftNavigation/LeftNavigation'
 import ChatLayout from '../Chat/ChatLayout'
+import Profile from '../OthersProfile/Profile'
+import SomeProfile from '../OthersProfile/SomeProfile'
 
 const Main = () => {
     return (
-        <div className='FRSS' style={{height:'100%'}}>
+        <div className='FRSS' style={{ height: '100%' }}>
             <LeftNavigation />
             <ChatLayout />
+            <Profile Profile={SomeProfile} />
         </div>
     )
 }
