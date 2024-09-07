7df8ab51e408f515fbbe5ce5390a5cc4babbb85a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 15:51:45 +0530

    Delete Home component and add Main component

---
+++ client/src/components/Home/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import LeftNavigation from '../LeftNavigation/LeftNavigation'
+import ChatLayout from '../Chat/ChatLayout'
+
+const Main = () => {
+    return (
+        <div className='FRSS'>
+            <LeftNavigation />
+            <ChatLayout />
+        </div>
+    )
+}
+
+export default Main
