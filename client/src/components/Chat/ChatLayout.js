9fbfd3d3e34aeb7dea3b19d2f1f70532c44f67e1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 18:01:06 +0530

    Update ChatLayout style

--- client/src/components/Chat/ChatLayout.js
+++ client/src/components/Chat/ChatLayout.js
@@ -3,8 +3,13 @@ import ConversationScreen from './ConversationScreen/ConversationScreen'
 import ListOfChats from './ListOfChats/ListOfChats'
 
 const ChatLayout = () => {
+    const chatLayoutStyle = {
+        width: 'calc(100% - 75px)',
+        border: '1px solid red',
+        overflow: 'hidden'
+    }
     return (
-        <div className='FRSS w100 h100'>
+        <div className='h100' style={chatLayoutStyle}>
             <ListOfChats />
             <ConversationScreen />
         </div>
