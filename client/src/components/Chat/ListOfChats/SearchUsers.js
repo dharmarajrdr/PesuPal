864af966ff9078776dfbef4f9cdbdd6c260213c4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 18:58:06 +0530

    Update SearchUsers component styling and functionality

--- client/src/components/Chat/ListOfChats/SearchUsers.js
+++ client/src/components/Chat/ListOfChats/SearchUsers.js
@@ -2,8 +2,19 @@ import React from 'react'
 import './SearchUsers.css'
 
 const SearchUsers = () => {
+    const showLeftNavContainer = () => {
+        const LeftNavigationOverlay = document.getElementById('LeftNavigationOverlay'),
+            leftNavContainer = document.getElementById('LeftNavigation');
+        LeftNavigationOverlay.style.display = 'block';
+        const timer = setTimeout(() => {
+            leftNavContainer.style.transition = 'transform 0.25s ease-in';
+            leftNavContainer.style.transform = 'translateX(100%)';
+            clearTimeout(timer);
+        }, 100);
+    }
     return (
         <div className='FRCB w100' id='SearchUserQuickAction'>
+            <i className='fas fa-bars navbar cursP' onClick={showLeftNavContainer}></i>
             <div className='searchBox'>
                 <input type='text' placeholder='Search Users' className='w100' autoComplete='off' spellCheck='false' />
                 <i className='fas fa-search'></i>
