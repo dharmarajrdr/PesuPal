13c01891378bcc7c52ced5d71b8a461098bfeb70
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 23:40:10 +0530

    Add SomeProfile component with contact and social information

---
+++ client/src/components/OthersProfile/SomeProfile.js
@@ -0,0 +1,47 @@
+export default {
+    "Name": 'Dharmaraj R',
+    'Designation': 'UI/UX Designer',
+    'Department': 'CRM - Client Performance - Audit & Tools',
+    'Contact': {
+        'Phone': '+91 6383392245',
+        'Email': 'dharmaraj.171215@gmail.com'
+    },
+    'Social': [
+        {
+            'Name': 'Facebook',
+            'icon': 'fa-facebook',
+            'color': '#1877F2',
+            'link': 'https://www.facebook.com/dharmaraj'
+        },
+        {
+            'Name': 'GitHub',
+            'icon': 'fa-github',
+            'color': '#181717',
+            'link': 'https://github.com/dharmarajrdr'
+        },
+        {
+            'Name': 'Instagram',
+            'icon': 'fa-instagram',
+            'color': '#E4405F',
+            'link': 'https://www.instagram.com/dharmaraj_rathinavel/'
+        },
+        {
+            'Name': 'LinkedIn',
+            'icon': 'fa-linkedin',
+            'color': '#0A66C2',
+            'link': 'https://www.linkedin.com/in/dharmarajrathinavel/'
+        },
+        {
+            'Name': 'Twitter',
+            'icon': 'fa-twitter',
+            'color': '#1DA1F2',
+            'link': 'https://twitter.com/dharmarajrdr'
+        },
+        {
+            'Name': 'WhatsApp',
+            'icon': 'fa-whatsapp',
+            'color': '#008000',
+            'link': 'https://wa.me/916383392245'
+        },
+    ]
+}
