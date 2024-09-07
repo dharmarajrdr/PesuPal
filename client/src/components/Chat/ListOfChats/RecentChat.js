cbbd0fdd056924d57455db541be03d1b7e923f14
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 21:35:10 +0530

    Update RecentChat component and styles

--- client/src/components/Chat/ListOfChats/RecentChat.js
+++ client/src/components/Chat/ListOfChats/RecentChat.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import './RecentChats.css';
+import { StatusIndicator } from '../../Auth/utils';
 
-const RecentChat = ({ image, name, recent_message }) => {
-    const { message, is_media, date_time, sender } = recent_message;
+const RecentChat = ({ image, name, status, recent_message }) => {
+    const { message, is_media, date_time, sender, number_of_unread_messages } = recent_message;
     return (
         <div className='RecentChatContainer cursP FRCS w100'>
-            <img src={image} />
+            <div className='pR'>
+                <img src={image} />
+                <StatusIndicator status={status} />
+            </div>
             <div className='FCSC w100 name_message_date'>
                 <div className='FRCB w100'>
                     <span className='name'>{name}</span>
                     <span className='date_time'>{date_time}</span>
                 </div>
-                <span className='message'>
-                    {sender != name ? <span className='sent_by_me'>{sender}: </span> : ''}
-                    {message}
-                </span>
+                <div className='FRCB w100'>
+                    <span className='message FRCS'>
+                        {sender != name ? <span className='sent_by_me mR5'>{sender}:</span> : ''}
+                        {is_media && <i className='fa-solid fa-image fs10 mR5 color777'></i>}
+                        {message}
+                    </span>
+                    {number_of_unread_messages > 0 && <span className='number_of_unread_messages'>{number_of_unread_messages}</span>}
+                </div>
             </div>
         </div>
     )
--- client/src/components/Chat/ListOfChats/RecentChats.js
+++ client/src/components/Chat/ListOfChats/RecentChats.js
@@ -6,8 +6,8 @@ import RecentChat from './RecentChat'
 const RecentChats = () => {
     return (
         <div id='RecentChats' className='FCCS w100'>
-            {PinnedUsersList.map(({ name, image, recent_message }, index) =>
-                <RecentChat key={index} name={name} image={image} recent_message={recent_message} />
+            {PinnedUsersList.map(({ name, status, image, recent_message }, index) =>
+                <RecentChat key={index} name={name} image={image} status={status} recent_message={recent_message} />
             )}
         </div>
     )
