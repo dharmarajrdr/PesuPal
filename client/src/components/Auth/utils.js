69dc02a1507f2e163fd3551c06c3726520062adf
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-07 21:35:46 +0530

    Refactor CSS styles and add utility functions

--- client/src/components/Auth/utils.js
+++ client/src/components/Auth/utils.js
@@ -1,5 +1,20 @@
-const hasCookie = () => {
+export const hasCookie = () => {
     return document.cookie.split(';').some((item) => item.trim().startsWith('token='));
+}, StatusIndicator = ({ status, style }) => {
+    let color = '#aaa', icon = 'fa-circle';
+    switch (status) {
+        case 'available':
+            color = 'green';
+            icon = 'fa-circle';
+            break;
+        case 'offline':
+            color = '#aaa';
+            icon = 'fa-circle';
+            break;
+        case 'call':
+            color = 'red';
+            icon = 'fa-phone';
+            break;
+    }
+    return <i class={`fa-solid ${icon} user_status`} style={{ ...style, color, backgroundColor: '#fff', 'borderRadius': '50%', padding: '5px' }}></i>;
 }
-
-export { hasCookie };
