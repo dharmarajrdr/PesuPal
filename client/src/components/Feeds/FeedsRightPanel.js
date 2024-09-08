92ca90e00ae0a5af90e934ccc2358d811cbcf3c6
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 10:05:08 +0530

    Add FeedsRightPanel component and its CSS

---
+++ client/src/components/Feeds/FeedsRightPanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FeedsRightPanel.css'
+
+const FeedsRightPanel = () => {
+  return (
+    <div id='FeedsRightPanel'>
+
+    </div>
+  )
+}
+
+export default FeedsRightPanel
