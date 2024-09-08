040af5d6d253d3ecac52e890e4d5b2809cb956bb
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 14:23:42 +0530

    Add Quote component and its styles

---
+++ client/src/components/Feeds/FeedsRightPanel/Quote.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Quote.css'
+
+const Quote = ({ quote, author }) => {
+    return (
+        <div id='Quote' className='w100'>
+            <p id='title' className='w100 selectNone'>
+                <i className='fa-solid fa-quote-left mR5' style={{ color: 'orange' }} ></i>Quote of the day
+            </p>
+            <div className='FCSS'>
+                <p id='QuoteText'>{quote}</p>
+                <i className='w100 color777 mT10' style={{ textAlign: 'right', fontSize: '12px' }}>- {author}</i>
+            </div>
+        </div>
+    )
+}
+
+export default Quote
