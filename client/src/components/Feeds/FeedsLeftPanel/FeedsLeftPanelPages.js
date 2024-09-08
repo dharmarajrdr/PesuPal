7f057ad8dd28e0efe89ef2dc3bb91172e194e3de
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 11:48:25 +0530

    Add FeedsLeftPanelPages component

---
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelPages.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import './FeedsLeftPanel.css'
+import PagesList from './PagesList'
+import FeedsLeftPanelPagesItem from './FeedsLeftPanelPagesItem'
+
+const FeedsLeftPanelPages = () => {
+    const [toggleState, setToggleState] = useState('show'),
+        [toggleIcon, setToggleIcon] = useState('fas fa-chevron-up'),
+        toggleHandler = () => {
+            if (toggleState === 'show') {
+                setToggleState('hide')
+                setToggleIcon('fas fa-chevron-down')
+            } else {
+                setToggleState('show')
+                setToggleIcon('fas fa-chevron-up')
+            }
+        }
+    return (
+        <>
+            {
+                PagesList.length > 0 ?
+                    <>
+                        <h6 id='subtitle' className='FRCB cursP selectNone' onClick={toggleHandler}>
+                            <span>Pages ({PagesList.length})</span>
+                            <i className={toggleIcon}></i>
+                        </h6>
+                        {
+                            toggleState === 'show' ?
+                                PagesList.map((item, index) => <FeedsLeftPanelPagesItem key={index} item={item} />)
+                                :
+                                null
+                        }
+                    </>
+                    :
+                    null
+            }
+        </>
+    )
+}
+
+export default FeedsLeftPanelPages
