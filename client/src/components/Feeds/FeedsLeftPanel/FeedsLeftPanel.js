3e70cd32d6457fcabedb3f3837a0ad4a1249528f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 11:49:05 +0530

    Update FeedsLeftPanel CSS and add FeedsLeftPanelPages component

--- client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanel.js
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanel.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './FeedsLeftPanel.css'
 import FeedsLeftPanelList from './FeedsLeftPanelList'
 import FeedsLeftPanelItem from './FeedsLeftPanelItem'
+import FeedsLeftPanelPages from './FeedsLeftPanelPages'
 
 const FeedsLeftPanel = () => {
     return (
         <div id='FeedsLeftPanel' className='h100'>
             {FeedsLeftPanelList.map((item, index) => <FeedsLeftPanelItem key={index} item={item} />)}
+            <FeedsLeftPanelPages />
         </div>
     )
 }
