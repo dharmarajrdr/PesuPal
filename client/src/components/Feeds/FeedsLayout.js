1889f452af105ef776457b7646e2fb15ded00f66
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 10:05:24 +0530

    Add FeedsLayout component and its CSS styles

---
+++ client/src/components/Feeds/FeedsLayout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './FeedsLayout.css'
+import FeedsLeftPanel from './FeedsLeftPanel'
+import FeedList from './FeedList'
+import FeedsRightPanel from './FeedsRightPanel'
+
+const FeedsLayout = () => {
+    return (
+        <div id='FeedsLayout' className='w100 h100 FRSS'>
+            <FeedsLeftPanel />
+            <div className='FRSC w100 h100' id='FeedsMain'>
+                <FeedList />
+                <FeedsRightPanel />
+            </div>
+        </div>
+    )
+}
+
+export default FeedsLayout
