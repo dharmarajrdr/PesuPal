2cf03f677abbf85954e9b1968f23476911186c5b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 10:05:16 +0530

    Add FeedsLeftPanel component and its CSS

---
+++ client/src/components/Feeds/FeedsLeftPanel.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FeedsLeftPanel.css'
+
+const FeedsLeftPanel = () => {
+    return (
+        <div id='FeedsLeftPanel' className='h100'>
+
+        </div>
+    )
+}
+
+export default FeedsLeftPanel
