87cf1340107364d2fda4eb991b70bf7eeee283fd
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 11:48:46 +0530

    Update FeedsLeftPanelItem.css and FeedsLeftPanelItem.js

--- client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelItem.js
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelItem.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import './FeedsLeftPanelItem.css'
 
 const FeedsLeftPanelItem = ({ item }) => {
-    const { icon, title, is_active, color } = item;
+    const { icon, title, is_active, color, visibility, route, participants } = item;
     return (
-        <div id='FeedsLeftPanelItem' className={`FRCS w100 ${is_active ? 'active' : ''}`}>
+        <Link to={route} id='FeedsLeftPanelItem' className={`FRCS w100 ${is_active ? 'active' : ''}`}>
             <i className={icon} style={{ color }} ></i>
             <span>{title}</span>
-        </div>
+            <span>{participants}</span>
+        </Link>
     )
 }
 
