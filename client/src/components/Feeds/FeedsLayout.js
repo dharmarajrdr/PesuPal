ff3ef88246a3e87b2c15fe1853ceec5d2bf1e791
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 15:40:01 +0530

    Remove FeedList component and related files

--- client/src/components/Feeds/FeedsLayout.js
+++ client/src/components/Feeds/FeedsLayout.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './FeedsLayout.css'
 import FeedsLeftPanel from './FeedsLeftPanel/FeedsLeftPanel'
-import FeedList from './FeedList'
 import FeedsRightPanel from './FeedsRightPanel/FeedsRightPanel'
+import FeedsMainPanel from './FeedsMainPanel/FeedsMainPanel'
 
 const FeedsLayout = () => {
     return (
         <div id='FeedsLayout' className='w100 h100 FRSS'>
             <FeedsLeftPanel />
             <div className='FRSC w100 h100' id='FeedsMain'>
-                <FeedList />
+                <FeedsMainPanel />
                 <FeedsRightPanel />
             </div>
         </div>
