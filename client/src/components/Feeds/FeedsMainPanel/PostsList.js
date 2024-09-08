534b54589514018e3ea9de836f7ffec833cb3c3b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 16:59:25 +0530

    Add PostsList component with sample data

---
+++ client/src/components/Feeds/FeedsMainPanel/PostsList.js
@@ -0,0 +1,53 @@
+export default [
+    {
+        'author': {
+            'name': 'John Doe',
+            'image': '/images/Users/user_3.jpg'
+        },
+        'title': 'Hello world!',
+        'content': 'This is my first post on this platform. I am excited to share my thoughts with you all.',
+        'created_at': '2021-08-01T12:00:00Z',
+        'likes': 10,
+        'comments': 5
+    },
+    {
+        'author': {
+            'name': 'Dharmaraj R',
+            'image': '/images/Users/user_7.jpg'
+        },
+        'content': 'Participated in System Deign of Instagram. It was a great learning experience.',
+        'created_at': '2024-08-12T12:20:20Z',
+        'likes': 10,
+        'comments': 5,
+        'media': [
+            '/images/Posts/post_1.jpg',
+            '/images/Posts/post_4.jpg'
+        ],
+        'mentions': [
+            '@cricket_addicts'
+        ],
+        'tags': [
+            '#cricket', '#viratkohli', '#sachintendulkar'
+        ]
+    },
+    {
+        'author': {
+            'name': 'Sara Smith',
+            'image': '/images/Users/user_5.jpg'
+        },
+        'content': 'Finally, Virat Kohli has scored his 50th century in international cricket. Congratulations to him!',
+        'created_at': '2021-08-01T12:00:00Z',
+        'likes': 10,
+        'comments': 5,
+        'media': [
+            '/images/Posts/post_2.jpg',
+            '/images/Posts/post_3.jpg',
+        ],
+        'mentions': [
+            '@cricket_addicts'
+        ],
+        'tags': [
+            '#cricket', '#viratkohli', '#sachintendulkar'
+        ]
+    }
+]
