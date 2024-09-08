b2389887ff79f0078f542d673c06af9ac121c631
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 16:59:41 +0530

    Update FeedsMainPanel CSS and add AllPosts component

--- client/src/components/Feeds/FeedsMainPanel/FeedsMainPanel.js
+++ client/src/components/Feeds/FeedsMainPanel/FeedsMainPanel.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './FeedsMainPanel.css'
 import PostYourThoughts from './PostYourThoughts'
+import AllPosts from './AllPosts'
 
 const FeedsMainPanel = () => {
   return (
     <div id='FeedsMainPanel'>
         <PostYourThoughts />
+        <AllPosts />
     </div>
   )
 }
