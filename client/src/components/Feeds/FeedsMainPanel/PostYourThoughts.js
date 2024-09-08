2a86ebdd09ee93bbc48859ba82b3d8e10d1a45e8
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 15:39:30 +0530

    Add PostYourThoughts component with CSS styling

---
+++ client/src/components/Feeds/FeedsMainPanel/PostYourThoughts.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './PostYourThoughts.css'
+
+const PostYourThoughts = () => {
+    return (
+        <div id='PostYourThoughts' className='FCSS'>
+            <label>Post Something</label>
+            <div className='FRSB w100' id='img_text_file'>
+                <img src='/images/Users/user_10.jpg' className='img_40_40' />
+                <textarea placeholder="What's on your mind?" spellCheck="false" autoComplete='off'></textarea>
+            </div>
+            <div className='w100 FRCB'>
+                <div className='FRCS'>
+                    <span class='actions_post_creation'>
+                        <i className='fa-regular fa-image'></i>
+                        Add attachment
+                    </span>
+                    <span class='actions_post_creation'>
+                        <i className='fa-regular fa-hashtag'></i>
+                        Add tag
+                    </span>
+                    <span class='actions_post_creation'>
+                        <i className='fa-regular fa-at'></i>
+                        Add mentions
+                    </span>
+                    <span class='actions_post_creation'>
+                        <i class="fa-solid fa-square-poll-vertical"></i>
+                        Add poll
+                    </span>
+                </div>
+                <div>
+                    <span className='actions_post_creation'>
+                        <i className='fa-regular fa-calendar-days'></i>
+                        Schedule post
+                    </span>
+                    <button id='shareMyThoughtButton'>Share</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostYourThoughts
