42f3d28f0b16dc2ac211c5236909ee076f8b73fd
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 11:49:24 +0530

    Add route to FeedsLeftPanelList items

--- client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelList.js
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelList.js
@@ -3,41 +3,49 @@ export default [
         'title': 'My Feed',
         'icon': 'fa-regular fa-newspaper',
         'is_active': true,
-        'color': '#0000FF'
+        'color': '#0000FF',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Bookmarks',
         'icon': 'fa-regular fa-bookmark',
-        'color': '#008000'
+        'color': '#008000',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Read Later',
         'icon': 'fa-regular fa-clock',
-        'color': '#FFA500'
+        'color': '#FFA500',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'My Scheduled Posts',
         'icon': 'fa-regular fa-calendar-days',
-        'color': '#FFC0CB'
+        'color': '#FFC0CB',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Posts I Follow',
         'icon': 'fa-solid fa-hand-holding-heart',
-        'color': '#FF0000'
+        'color': '#FF0000',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Corp Announcements',
         'icon': 'fa-solid fa-bullhorn',
-        'color': '#800080'
+        'color': '#800080',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Mandatory Reads',
         'icon': 'fa-solid fa-triangle-exclamation',
-        'color': '#ff0000'
+        'color': '#ff0000',
+        'route': '/feeds/936252837'
     },
     {
         'title': 'Quick Links',
         'icon': 'fa-solid fa-link',
-        'color': '#008080'
+        'color': '#008080',
+        'route': '/feeds/936252837'
     }
 ]
