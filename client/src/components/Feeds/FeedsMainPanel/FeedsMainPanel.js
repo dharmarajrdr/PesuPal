789dee8488f22fc633aaf59265307a23edd17202
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 15:39:44 +0530

    Add FeedsMainPanel component and its styles

---
+++ client/src/components/Feeds/FeedsMainPanel/FeedsMainPanel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './FeedsMainPanel.css'
+import PostYourThoughts from './PostYourThoughts'
+
+const FeedsMainPanel = () => {
+  return (
+    <div id='FeedsMainPanel'>
+        <PostYourThoughts />
+    </div>
+  )
+}
+
+export default FeedsMainPanel
