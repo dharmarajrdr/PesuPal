c627eb6cb8b56bf8c90ab7805a28832045ced3cc
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 10:05:31 +0530

    Add FeedList component and its CSS

---
+++ client/src/components/Feeds/FeedList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FeedList.css'
+
+const FeedList = () => {
+    return (
+        <div id='FeedList'>
+            
+        </div>
+    )
+}
+
+export default FeedList
