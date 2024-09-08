a34db93c2333045fde17cb1be735f75b0189fa1a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 14:23:47 +0530

    Add TrendingPosts and Quote components to FeedsRightPanel

--- client/src/components/Feeds/FeedsRightPanel/FeedsRightPanel.js
+++ client/src/components/Feeds/FeedsRightPanel/FeedsRightPanel.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './FeedsRightPanel.css'
 import TrendingTags from './TrendingTags'
+import TrendingPosts from './TrendingPosts'
+import Quote from './Quote'
 
 const FeedsRightPanel = () => {
-  return (
-    <div id='FeedsRightPanel' className='noScrollbar'>
-        <TrendingTags />
-    </div>
-  )
+    return (
+        <div id='FeedsRightPanel' className='noScrollbar'>
+            <TrendingTags />
+            <TrendingPosts />
+            <Quote quote="One day, you'll leave this world behind So live a life you will remember." author="Avicii" />
+        </div>
+    )
 }
 
 export default FeedsRightPanel
