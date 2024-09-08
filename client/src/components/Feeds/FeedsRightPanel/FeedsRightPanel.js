7af9c0ce953f59a3d63577ed41ed4c1d3bb2948a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-09-08 12:16:19 +0530

    Delete old FeedsRightPanel component and add new FeedsRightPanel component with CSS

--- client/src/components/Feeds/FeedsRightPanel/FeedsRightPanel.js
+++ client/src/components/Feeds/FeedsRightPanel/FeedsRightPanel.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './FeedsRightPanel.css'
+import TrendingTags from './TrendingTags'
 
 const FeedsRightPanel = () => {
   return (
-    <div id='FeedsRightPanel'>
-
+    <div id='FeedsRightPanel' className='noScrollbar'>
+        <TrendingTags />
     </div>
   )
 }
