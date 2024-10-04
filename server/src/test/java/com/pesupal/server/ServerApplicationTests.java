a67ad13eadc7a4c478d8379c7e379e6f1350ee64
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-04 09:13:38 +0530

    Initialised backend | Spring boot

---
+++ server/src/test/java/com/pesupal/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesupal.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
