947117b88a6ce1a8b70a644f6a3f2e6a775912c5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-05 10:52:33 +0530

    Add SubTabs component with CSS styling

---
+++ client/src/components/Chat/ListOfChats/SubTabs.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './SubTabs.css';
+import { Link, useNavigate } from 'react-router-dom'
+
+const SubTabs = () => {
+    return (
+        <div className='FRCC' id='subtab_container'>
+            <Link to='/chat' className='subtabs'>
+                <span>Messages</span>
+                <b className='notifyCount'>17</b>
+            </Link>
+            <Link to='/chat/groups' className='subtabs'>
+                <span>Groups</span>
+                <b className='notifyCount'>12</b>
+            </Link>
+            <Link to='/chat/channels' className='subtabs'>
+                <span>Channels</span>
+                <b className='notifyCount'>15</b>
+            </Link>
+        </div>
+    )
+}
+
+export default SubTabs
