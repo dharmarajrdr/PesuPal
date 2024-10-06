7741ac3266204762db25350e38fd5cf9531495e9
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 18:10:21 +0530

    Add ID generation strategy to FeedConfig model

--- server/src/main/java/com/pesupal/server/model/FeedConfig.java
+++ server/src/main/java/com/pesupal/server/model/FeedConfig.java
@@ -1,7 +1,10 @@
 package com.pesupal.server.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,13 +16,13 @@ import lombok.NoArgsConstructor;
 public class FeedConfig {
 
     @Id
+    @SequenceGenerator(name = "feed_config_id_seq", sequenceName = "feed_config_id_seq", allocationSize = 1)
+    @GeneratedValue(generator = "feed_config_id_seq", strategy = GenerationType.SEQUENCE)
     private Long feedConfigId;
 
     private Boolean canComment;
 
-    private String displayName;
+    private Boolean canShare;
 
-    private Integer contactId;
-
-    private Integer socialId;
+    private Boolean canBookmark;
 }
