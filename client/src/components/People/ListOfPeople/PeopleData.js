ddb16aaf8361d6afa7bdcc58ea4974a011316851
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 22:18:11 +0530

    Add new people data to PeopleData.js

--- client/src/components/People/ListOfPeople/PeopleData.js
+++ client/src/components/People/ListOfPeople/PeopleData.js
@@ -58,5 +58,65 @@ export default [
         "department": "IT Support",
         "status": "available",
         "profile_image": "/images/Users/user_6.jpg"
+    },
+    {
+        "id": 1,
+        "name": "John Doe",
+        "display_name": "John",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "IT Support",
+        "status": "available",
+        "profile_image": "/images/Users/user_1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Kevin Johnson",
+        "display_name": "Kevin",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "Frontend Developer",
+        "status": "available",
+        "profile_image": "/images/Users/user_2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Alex clark",
+        "display_name": "Alex",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "Lead Engineer",
+        "status": "call",
+        "profile_image": "/images/Users/user_3.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Michael Brown",
+        "display_name": "Brown",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "UI/UX Designer",
+        "status": "offline",
+        "profile_image": "/images/Users/user_4.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Sophia Anderson",
+        "display_name": "Sophi",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "Marketing Lead",
+        "status": "available",
+        "profile_image": "/images/Users/user_5.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Kevin Johnson",
+        "display_name": "John",
+        "email": "johndoe@gmail.com",
+        "phone": "123-456-7890",
+        "department": "IT Support",
+        "status": "available",
+        "profile_image": "/images/Users/user_6.jpg"
     }
 ]
