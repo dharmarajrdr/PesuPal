3eb0aad69e56856c3b6b29c70ea8f6a2f2863788
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 22:18:17 +0530

    Add search functionality to ListOfPeople component

--- client/src/components/People/ListOfPeople/SearchPeople.js
+++ client/src/components/People/ListOfPeople/SearchPeople.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './SearchPeople.css';
 
 const SearchPeople = () => {
-  return (
-    <div>SearchPeople</div>
-  )
+    return (
+        <div id='search_people' className='w100 FRSS'>
+            <div id='search_people_input_container'>
+                <input type='input' placeholder='Search people, department,..' id='search_people_input' autoComplete='off' spellCheck='false' />
+            </div>
+        </div>
+    )
 }
 
 export default SearchPeople
