07644408f5e89557347268ce8319d04d247429c8
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 18:27:33 +0530

    Add annotations and relationships to Feed model class

--- server/src/main/java/com/pesupal/server/model/Feed.java
+++ server/src/main/java/com/pesupal/server/model/Feed.java
@@ -2,8 +2,17 @@ package com.pesupal.server.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,17 +24,25 @@ import lombok.NoArgsConstructor;
 public class Feed {
 
     @Id
+    @SequenceGenerator(name = "feed_id_seq", sequenceName = "feed_id_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "feed_id_seq")
     private Long feedId;
 
-    private Long orgId;
+    @OneToOne
+    @JoinColumn(name = "org_id") // Join column for the OneToOne relationship
+    private Org org;
 
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id") // Join column for the ManyToOne relationship
+    private Users user_field;
 
     private String message;
 
-    private Long feedConfigId;
+    @OneToOne
+    @JoinColumn(name = "feedConfigId") // Join column for the OneToOne relationship
+    private FeedConfig feedConfigId_field;
 
-    private Integer socialId;
-
-    private List<String> files;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER) // OneToMany relationship
+    @JoinColumn(name = "feed_id") // Join column for the OneToMany relationship
+    private List<FeedMedia> media_files; // List of FeedMedia associated with this Feed
 }
