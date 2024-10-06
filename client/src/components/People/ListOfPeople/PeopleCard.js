4b4b216d8c498b1746bc843e796a6faed29882d1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 19:54:59 +0530

    Add PeopleCard component and its styles

---
+++ client/src/components/People/ListOfPeople/PeopleCard.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './PeopleCard.css';
+import { StatusIndicator } from '../../Auth/utils';
+
+const PeopleCard = ({ person }) => {
+
+    const { name, profile_image, department, status, id } = person;
+    return (
+        <div className='FCCC PeopleCard'>
+            <div className='FCCC'>
+                <div className='FRCC profile_picture_container'>
+                    <img src={profile_image} className='img_75_75 mB10' />
+                    <StatusIndicator status={status}/>
+                </div>
+                <b className='mx5 user_name'>{name}</b>
+                <span className='color777 fs10'>{department}</span>
+            </div>
+            <div className='mx10 FRCC'>
+                <i className='profile_contacts fa fa-comment' style={{ backgroundColor: 'blue' }} />
+                <i className='profile_contacts fa fa-phone' style={{ backgroundColor: 'green' }} />
+                <i className='profile_contacts fa fa-video' style={{ backgroundColor: 'red' }} />
+            </div>
+            <div className='mT5'>
+                <Link to={"/user/" + id} className='view_profile_button'>View Profile</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default PeopleCard
