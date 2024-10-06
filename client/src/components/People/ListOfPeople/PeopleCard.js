a4088d6cddb0a999f22b0a53404df2974e234873
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-06 21:39:38 +0530

    Update PeopleCard styles and layout

--- client/src/components/People/ListOfPeople/PeopleCard.js
+++ client/src/components/People/ListOfPeople/PeopleCard.js
@@ -9,7 +9,7 @@ const PeopleCard = ({ person }) => {
     return (
         <div className='FCCC PeopleCard'>
             <i className="fa fa-ellipsis-vertical three_dots"></i>
-            <div className='FCCC'>
+            <div className='FCCC mB5'>
                 <div className='FRCC profile_picture_container'>
                     <img src={profile_image} className='img_75_75 mB10' />
                     <StatusIndicator status={status} />
@@ -17,15 +17,11 @@ const PeopleCard = ({ person }) => {
                 <b className='mx5 user_name'>{name}</b>
                 <span className='color777 fs10'>{department}</span>
             </div>
-            <div className='mx10 FRCC'>
+            <div className='mT10 FRCC'>
                 <i className='profile_contacts fa fa-comment' style={{ backgroundColor: 'blue' }} />
                 <i className='profile_contacts fa fa-phone' style={{ backgroundColor: 'green' }} />
                 <i className='profile_contacts fa fa-video' style={{ backgroundColor: 'red' }} />
             </div>
-            <div className='mT5'>
-                <Link to={"/user/" + id} className='view_profile_button'>View Profile</Link>
-            </div>
-
         </div>
     )
 }
