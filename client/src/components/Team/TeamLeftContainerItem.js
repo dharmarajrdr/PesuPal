e6faa1a1cbf79206d8b872a9d5beae4da17332f1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-07 09:31:29 +0530

    Add TeamLeftContainerItem component and its CSS

---
+++ client/src/components/Team/TeamLeftContainerItem.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './TeamLeftContainerItem.css'
+
+const TeamLeftContainerItem = ({ item }) => {
+    const { title, route, icon, notify_count } = item;
+    return (
+        <Link to={route} class='TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'>
+            <i className={icon}></i>
+            <div className='FRCB w100 pL10 title_notify'>
+                <span className='color555 title'>{title}</span>
+                {notify_count > 0 && <span className='color555 notify_count'>{notify_count}</span>}
+            </div>
+        </Link>
+    )
+}
+
+export default TeamLeftContainerItem
