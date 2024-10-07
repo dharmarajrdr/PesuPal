e0c0f7c4e80a5953a709147e7c7abee258dc88a6
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-07 08:24:21 +0530

    Add TeamLayout, TeamLeftContainer, and TeamMainContainer components

---
+++ client/src/components/Team/TeamLeftContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TeamLeftContainer.css'
+
+const TeamLeftContainer = () => {
+  return (
+    <div id='TeamLeftContainer'>
+        
+    </div>
+  )
+}
+
+export default TeamLeftContainer
---
+++ client/src/components/Team/TeamMainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './TeamMainContainer.css';
+
+const TeamMainContainer = () => {
+  return (
+    <div id='TeamMainContainer'>
+
+    </div>
+  )
+}
+
+export default TeamMainContainer
