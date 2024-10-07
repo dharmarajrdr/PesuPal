786d14f5296d190c0954e2750272da7665407e7f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-07 09:31:25 +0530

    Add overflow-y property to TeamLeftContainer.css and update TeamLeftContainer.js

--- client/src/components/Team/TeamLeftContainer.js
+++ client/src/components/Team/TeamLeftContainer.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import ListOfTeamNavigations from './ListOfTeamNavigations';
 import './TeamLeftContainer.css'
+import TeamLeftContainerItem from './TeamLeftContainerItem';
 
 const TeamLeftContainer = () => {
-  return (
-    <div id='TeamLeftContainer'>
-        
-    </div>
-  )
+    return (
+        <div id='TeamLeftContainer' className='FCSS p10 custom-scrollbar'>
+            {ListOfTeamNavigations.map((item, index) => <TeamLeftContainerItem key={index} item={item} />)}
+        </div>
+    )
 }
 
 export default TeamLeftContainer
