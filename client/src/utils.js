8ec5854788402b4e59c697f2edd9f6a25fb98873
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-11 17:50:20 +0530

    Update icon colors and background colors in utils.js

--- client/src/utils.js
+++ client/src/utils.js
@@ -4,27 +4,62 @@ export default {
         switch (category) {
             case "Document": {
                 icon.icon = "fas fa-file-alt"
-                icon.icon_color = "#ff7bff"
+                icon.icon_color = "#8081ff"
+                icon.bg_color = "rgb(32, 33, 165)";
                 break;
             }
             case "Video": {
                 icon.icon = "fas fa-video"
                 icon.icon_color = "#ff7bff"
+                icon.bg_color = "rgb(131, 15, 131)";
                 break;
             }
             case "Audio": {
                 icon.icon = "fas fa-music"
-                icon.icon_color = "#ff7bff"
+                icon.icon_color = "#fa23ac"
+                icon.bg_color = "rgb(128, 21, 89)";
                 break;
             }
             case "Image": {
                 icon.icon = "fas fa-file-image"
-                icon.icon_color = "#ff7bff"
+                icon.icon_color = "#42aa1b"
+                icon.bg_color = "rgb(32, 92, 9)";
                 break;
             }
             case "Folder": {
                 icon.icon = "fas fa-folder"
-                icon.icon_color = "#8081ff"
+                icon.icon_color = "#ff7a12"
+                icon.bg_color = "rgb(32, 92, 9)";
+                break;
+            }
+            case "Recent": {
+                icon.icon = "fas fa-history"
+                icon.icon_color = "#007bff"
+                icon.bg_color = "rgb(32, 33, 165)";
+                break;
+            }
+            case "Trash": {
+                icon.icon = "fas fa-trash"
+                icon.icon_color = "#ff1524"
+                icon.bg_color = "rgb(32, 33, 165)";
+                break;
+            }
+            case "Shared": {
+                icon.icon = "fas fa-share-alt"
+                icon.icon_color = "#935102"
+                icon.bg_color = "rgb(32, 33, 165)";
+                break;
+            }
+            case "Starred": {
+                icon.icon = "fas fa-star"
+                icon.icon_color = "#fa23ac"
+                icon.bg_color = "rgb(32, 33, 165)";
+                break;
+            }
+            case "Home": {
+                icon.icon = "fas fa-home"
+                icon.icon_color = "#42aa1b"
+                icon.bg_color = "rgb(32, 33, 165)";
                 break;
             }
         }
