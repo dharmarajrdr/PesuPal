d6a3abd891d11fa0d8e71129e35944e78e93272b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 00:35:47 +0530

    Add ManageWorkLayout component and styles

---
+++ client/src/components/Team/ManageWork/ManageWorkLayout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './ManageWorkLayout.css'
+import Header from './Header'
+import ManageWorkBody from './ManageWorkBody'
+
+const ManageWorkLayout = () => {
+  return (
+    <div id='ManageWorkLayout'>
+        <Header />
+        <ManageWorkBody />
+    </div>
+  )
+}
+
+export default ManageWorkLayout
