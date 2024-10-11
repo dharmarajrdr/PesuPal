be6e10a1715e7bcd75f44e7aedd20e485288786d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 00:35:20 +0530

    Add FilterComponent.css and FilterComponent.js

---
+++ client/src/components/Team/ManageWork/FilterComponent.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './FilterComponent.css'
+
+const FilterComponentItem = ({ item }) => {
+    const { id, title, icon, dom, options } = item;
+    return (
+        <div className='FilterComponentItem p5 FRCC'>
+            <i className={icon + ' img_40_40 alignCenter'}></i>
+            <select>
+                <option value='' disabled selected>Select {title}</option>
+                {options.map((option, index) => <option key={index} value={option.id}>{option.title}</option>)}
+            </select>
+        </div>
+    )
+}
+
+const FilterComponent = ({ FiltersList }) => {
+    return (
+        <div id='FilterComponent' className='W100 FRCS noScrollbar'>
+            {FiltersList.map((item, index) => <FilterComponentItem key={index} item={item} />)}
+            <div className='FRCC mR10 mL10' id='createRecord' style={{ backgroundColor: 'green' }}>
+                <span>Save Filter</span>
+            </div>
+            <div className='FRCC' id='createRecord' style={{ backgroundColor: 'white', border: '1px solid red' }}>
+                <span style={{ color: 'red' }}>Clear Filter</span>
+            </div>
+        </div>
+    )
+}
+
+export default FilterComponent
