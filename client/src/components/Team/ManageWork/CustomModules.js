bdae3d8ad180194d2fd8acfd8259659f787f5963
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 00:35:06 +0530

    Add CustomModules component

---
+++ client/src/components/Team/ManageWork/CustomModules.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './CustomModulesItem.css';
+
+const CustomModulesItem = ({ item }) => {
+    const { name, icon, icon_color, route, created_info, update_permission } = item,
+        { created_by, created_date } = created_info;
+    return (
+        <Link to={route} className='CustomModulesItem mR10 FRCC' title={"Created by: " + created_by + " on " + created_date}>
+            <i className={icon + " pR10 img_20_20"} style={{ color: icon_color }}></i>
+            <span className='color555 pL5'>{name}</span>
+        </Link>
+    )
+}
+
+const CustomModules = ({ CustomModulesList }) => {
+    return (
+        <div id='CustomModules' className='FRCS'>
+            {CustomModulesList.map((item, index) => <CustomModulesItem key={index} item={item} />)}
+        </div>
+    )
+}
+
+export default CustomModules
