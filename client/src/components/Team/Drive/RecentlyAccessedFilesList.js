2086c3941c7703e6b2b1ecbede27c6a5c5ce90d1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-11 16:46:35 +0530

    Add Recently Accessed Files List component

---
+++ client/src/components/Team/Drive/RecentlyAccessedFilesList.js
@@ -0,0 +1,273 @@
+export default {
+    "title": "Recently Accessed Files",
+    "header": [
+        {
+            "id": 1,
+            "title": "File Name",
+            "sort": "ASC",
+            "width": "40%"
+        },
+        {
+            "id": 2,
+            "title": "Owner",
+            "width": "20%"
+        },
+        {
+            "id": 3,
+            "title": "Created on",
+            "width": "20%"
+        },
+        {
+            "id": 4,
+            "title": "File size",
+            "width": "10%"
+        },
+        {
+            "id": 5,
+            "title": "",
+            "width": "10%"
+        }
+    ],
+    "body": [
+        [
+            {
+                "data": "[Basic] Spring JPA.mp4",
+                "category": "Video",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Test.java",
+                "category": "Document",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Hukum.mp3",
+                "category": "Audio",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "HelloWorld.js",
+                "category": "Document",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Spring Boot for beginners | Telusko | Java | Zero to Hero.mp4",
+                "category": "Video",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Sample_output.json",
+                "category": "Document",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Hukum.mp3",
+                "category": "Audio",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "HelloWorld.js",
+                "category": "Document",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Spring Boot for beginners | Telusko | Java | Zero to Hero.mp4",
+                "category": "Video",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ],
+        [
+            {
+                "data": "Sample_output.json",
+                "category": "Document",
+                "width": "40%"
+            },
+            {
+                "data": "Dharmaraj R",
+                "width": "20%"
+            },
+            {
+                "data": "29 Aug, 2001",
+                "width": "20%"
+            },
+            {
+                "data": "1.2KB",
+                "width": "10%"
+            },
+            {
+                "data": "",
+                "width": "10%",
+                "option_icon": "fa fa-ellipsis-vertical"
+            }
+        ]
+    ]
+}
