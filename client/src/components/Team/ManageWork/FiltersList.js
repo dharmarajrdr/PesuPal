c42dcd1b9c8c9f070818ff88259a13c67059d7a3
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 00:35:25 +0530

    Add FiltersList component

---
+++ client/src/components/Team/ManageWork/FiltersList.js
@@ -0,0 +1,90 @@
+export default [
+    {
+        "id": 1,
+        "title": "Owner",
+        "icon": "fas fa-user",
+        "dom_type": "select",
+        "options": [
+            {
+                "id": 1,
+                "title": "Dharmaraj"
+            },
+            {
+                "id": 2,
+                "title": "Bala Sundar C"
+            },
+            {
+                "id": 3,
+                "title": "Sudharshan"
+            },
+            {
+                "id": 4,
+                "title": "Pradheep"
+            },
+            {
+                "id": 5,
+                "title": "Praveen"
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "title": "Tag",
+        "icon": "fas fa-tag",
+        "dom_type": "select",
+        "options": [
+            {
+                "id": 1,
+                "title": "Bug"
+            },
+            {
+                "id": 2,
+                "title": "Feature"
+            },
+            {
+                "id": 3,
+                "title": "Task"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "title": "Project",
+        "icon": "fas fa-project-diagram",
+        "dom_type": "select",
+        "options": [
+            {
+                "id": 1,
+                "title": "PesuPal"
+            },
+            {
+                "id": 2,
+                "title": "Todo App"
+            },
+            {
+                "id": 3,
+                "title": "E-Commerce"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "title": "Priority",
+        "icon": "fas fa-exclamation-circle",
+        "dom_type": "select",
+        "options": [
+            {
+                "id": 1,
+                "title": "High"
+            },
+            {
+                "id": 2,
+                "title": "Medium"
+            },
+            {
+                "id": 3,
+                "title": "Low"
+            }
+        ]
+    }
+]
