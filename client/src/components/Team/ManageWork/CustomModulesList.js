26c6f022eb2058db4bb7b374536bf2ff24444556
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 00:35:14 +0530

    Add CustomModulesItem.css and CustomModulesList.js

---
+++ client/src/components/Team/ManageWork/CustomModulesList.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        "id": 1,
+        "name": "All",
+        "icon": "fas fa-map",
+        "icon_color": "#2196f3",
+        "route": "/team/managework",
+        "created_info": {
+            "created_by": "John Doe",
+            "created_date": "2021-07-01"
+        },
+        "update_permission": false
+    },
+    {
+        "id": 1,
+        "name": "Bugs",
+        "icon": "fas fa-bug",
+        "icon_color": "#f44336",
+        "route": "/team/managework/bugs",
+        "created_info": {
+            "created_by": "John Doe",
+            "created_date": "2021-07-01"
+        },
+        "update_permission": false
+    },
+    {
+        "id": 2,
+        "name": "Features",
+        "icon": "fas fa-star",
+        "icon_color": "#ff9800",
+        "route": "/team/managework/features",
+        "created_info": {
+            "created_by": "Kevin Smith",
+            "created_date": "2021-07-01"
+        },
+        "update_permission": false
+    },
+    {
+        "id": 3,
+        "name": "Tasks",
+        "icon": "fas fa-tasks",
+        "icon_color": "#4caf50",
+        "route": "/team/managework/tasks",
+        "created_info": {
+            "created_by": "Mark Johnson",
+            "created_date": "2021-07-01"
+        },
+        "update_permission": false
+    }
+]
