03cd2ca97685ebc4e86c54d5e9ec8f297cfd0ab5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 13:11:18 +0530

    Update FilterComponent width and add SaveClearFilter component

--- client/src/components/Team/ManageWork/FilterComponent.js
+++ client/src/components/Team/ManageWork/FilterComponent.js
@@ -6,24 +6,32 @@ const FilterComponentItem = ({ item }) => {
     return (
         <div className='FilterComponentItem p5 FRCC'>
             <i className={icon + ' img_40_40 alignCenter'}></i>
-            <select>
-                <option value='' disabled>Select {title}</option>
+            <select defaultValue="All">
                 {options.map((option, index) => <option key={index} value={option.id}>{option.title}</option>)}
             </select>
         </div>
     )
 }
 
-const FilterComponent = ({ FiltersList }) => {
+const SaveClearFilter = () => {
     return (
-        <div id='FilterComponent' className='W100 FRCS noScrollbar'>
-            {FiltersList.map((item, index) => <FilterComponentItem key={index} item={item} />)}
+        <>
             <div className='FRCC mR10 mL10' id='createRecord' style={{ backgroundColor: 'green' }}>
                 <span>Save Filter</span>
             </div>
             <div className='FRCC' id='createRecord' style={{ backgroundColor: 'white', border: '1px solid red' }}>
                 <span style={{ color: 'red' }}>Clear Filter</span>
             </div>
+        </>
+    )
+}
+
+const FilterComponent = ({ FiltersList }) => {
+
+    return (
+        <div id='FilterComponent' className='W100 FRCS noScrollbar'>
+            {FiltersList.map((item, index) => <FilterComponentItem key={index} item={item} />)}
+            {/* <SaveClearFilter /> */}
         </div>
     )
 }
