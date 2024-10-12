b564b288cff0852af8f4cff62d6536b358f51543
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 21:54:46 +0530

    Update file manager routes

--- client/src/components/Team/Drive/FileManagerList.js
+++ client/src/components/Team/Drive/FileManagerList.js
@@ -2,43 +2,36 @@ export default [
     {
         "id": 1,
         "title": "Home",
-        "route": "/team/drive/home",
-        "active": true
+        "route": "/team/drive/home"
     },
     {
         "id": 2,
         "title": "Document",
-        "route": "/team/drive/files",
-        "active": false
+        "route": "/team/drive/files"
     },
     {
         "id": 3,
         "title": "Folder",
-        "route": "/team/drive/folder",
-        "active": false
+        "route": "/team/drive/folder"
     },
     {
         "id": 3,
         "title": "Starred",
-        "route": "/team/drive/starred",
-        "active": false
+        "route": "/team/drive/starred"
     },
     {
         "id": 4,
         "title": "Shared",
-        "route": "/team/drive/shared",
-        "active": false
+        "route": "/team/drive/shared"
     },
     {
         "id": 5,
         "title": "Recent",
-        "route": "/team/drive/recent",
-        "active": false
+        "route": "/team/drive/recent"
     },
     {
         "id": 6,
         "title": "Trash",
-        "route": "/team/drive/trash",
-        "active": false
+        "route": "/team/drive/trash"
     }
 ]
