87bce534ebcf4e9d8d39bc9072409add04898f31
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 12:25:01 +0530

    Update ManageWorkBody component

--- client/src/components/Team/ManageWork/ManageWorkBody.js
+++ client/src/components/Team/ManageWork/ManageWorkBody.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './ManageWorkBody.css'
 import KanbanView from './Views/KanbanView'
-import ManageWorkList from './ManageWorkList';
+import ManageWorkListKanban from './ManageWorkListKanban';
 import ListView from './Views/ListView';
+import ManageWorkList from './ManageWorkList';
 
 const ManageWorkBody = () => {
     return (
         <div id='ManageWorkBody' className='w100 custom-scrollbar'>
-            {/* <KanbanView ManageWorkList={ManageWorkList} /> */}
+            {/* <KanbanView ManageWorkListKanban={ManageWorkListKanban} /> */}
             <ListView ManageWorkList={ManageWorkList} />
         </div>
     )
