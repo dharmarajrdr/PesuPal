76c91538b1ae7d03bb59c3ad530cc2920f18450d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 12:24:39 +0530

    Refactor code to improve performance and readability

---
+++ client/src/components/Team/ManageWork/ManageWorkListKanban.js
@@ -0,0 +1,306 @@
+export default [
+    {
+        "id": 1,
+        "status": "Yet to start",
+        "items": [
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering. Some long text here",
+                "owner": {
+                    "ownerName": "Dharmaraj Rathinavel",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Bug",
+                "priority": "Medium",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/2",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_4.jpg"
+                },
+                "tag": "Feature",
+                "priority": "Low",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/3",
+                "draggable": false
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_1.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/4",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Task",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": false
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_8.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_2.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/5",
+                "draggable": true
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_1.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+        ]
+    },
+    {
+        "id": 2,
+        "status": "In progress",
+        "items": [
+            {
+                "id": 3,
+                "title": "Add a new feature to the app",
+                "owner": {
+                    "ownerName": "Praveen",
+                    "ownerImage": "/images/Users/user_2.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Pradheep",
+                "created_date": "2024-12-11",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "status": "On hold",
+        "items": []
+    },
+    {
+        "id": 4,
+        "status": "Completed",
+        "items": [
+            {
+                "id": 4,
+                "title": "Update the app logo",
+                "owner": {
+                    "ownerName": "Pradheep",
+                    "ownerImage": "/images/Users/user_6.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Pradheep",
+                "created_date": "2024-12-11",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "status": "Released",
+        "items": [
+            {
+                "id": 1,
+                "title": "[Issue] Component not rendering",
+                "owner": {
+                    "ownerName": "Dharmaraj",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Bug",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2020-12-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+            {
+                "id": 2,
+                "title": "Change the color of the button",
+                "owner": {
+                    "ownerName": "Sudharshan",
+                    "ownerImage": "/images/Users/user_5.jpg"
+                },
+                "tag": "Feature",
+                "priority": "High",
+                "project": "PesuPal",
+                "created_by": "Bala Sundar C",
+                "created_date": "2021-02-01",
+                "route": "/team/manage_work/item/1",
+                "draggable": true
+            },
+        ]
+    },
+    {
+        "id": 6,
+        "status": "Dropped",
+        "items": []
+    }
+]
