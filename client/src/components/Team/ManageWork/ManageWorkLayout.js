4ed88a69326c35eaa86baf48e740e637f236281d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 13:10:54 +0530

    Refactor ManageWorkLayout component to use useState hook

--- client/src/components/Team/ManageWork/ManageWorkLayout.js
+++ client/src/components/Team/ManageWork/ManageWorkLayout.js
@@ -2,14 +2,16 @@ import React from 'react'
 import './ManageWorkLayout.css'
 import Header from './Header'
 import ManageWorkBody from './ManageWorkBody'
+import { useState } from 'react';
 
 const ManageWorkLayout = () => {
-  return (
-    <div id='ManageWorkLayout'>
-        <Header />
-        <ManageWorkBody />
-    </div>
-  )
+    const viewState = useState('list');
+    return (
+        <div id='ManageWorkLayout'>
+            <Header viewState={viewState} />
+            <ManageWorkBody viewState={viewState} />
+        </div>
+    )
 }
 
 export default ManageWorkLayout
