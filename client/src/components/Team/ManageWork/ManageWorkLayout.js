00985d1eb66d681ac1b5301db0eb9d6e96aa69fd
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 18:52:44 +0530

    Refactor ManageWorkLayout component

--- client/src/components/Team/ManageWork/ManageWorkLayout.js
+++ client/src/components/Team/ManageWork/ManageWorkLayout.js
@@ -2,14 +2,12 @@ import React from 'react'
 import './ManageWorkLayout.css'
 import Header from './Header'
 import ManageWorkBody from './ManageWorkBody'
-import { useState } from 'react';
 
 const ManageWorkLayout = () => {
-    const viewState = useState('list');
     return (
         <div id='ManageWorkLayout'>
-            <Header viewState={viewState} />
-            <ManageWorkBody viewState={viewState} />
+            <Header />
+            <ManageWorkBody />
         </div>
     )
 }
