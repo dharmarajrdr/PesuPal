720299c5192e176ba61d8b40d84d5f349a955f01
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 18:53:15 +0530

    Refactor Nav component to remove unused code

--- client/src/components/LeftNavigation/Nav.js
+++ client/src/components/LeftNavigation/Nav.js
@@ -1,21 +1,11 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom';
 import './LeftNavigation.css'
-import themes from '../../theme';
-import { useDispatch } from 'react-redux';
-import { setActiveNavigation } from '../../store/actions/Navigation';
 
-const Nav = ({ icon, image, title, route, isActive, notifyCount }) => {
-
-    const { activeNav } = themes,
-        dispatch = useDispatch(),
-        handleNavClick = (event) => {
-            // event.preventDefault();
-            dispatch(setActiveNavigation({ route }));
-        }
+const Nav = ({ icon, image, title, route, notifyCount }) => {
 
     return (
-        <NavLink to={route} style={isActive ? activeNav : null} className={`LeftNavigationItem cursP FCCC selectNone ${isActive ? 'activeNav' : ''}`} onClick={handleNavClick}  >
+        <NavLink to={route} className={({ isActive }) => (isActive ? 'activeNav' : '') + ` LeftNavigationItem cursP FCCC selectNone`}  >
             {
                 icon ? <i className={`fas ${icon} colorFFF`}></i> : <img src={image} />
             }
