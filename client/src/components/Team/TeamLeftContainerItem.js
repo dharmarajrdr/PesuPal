ec9ed0d14d346bb06b6376b9ffe80c7dae24e2e1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-10-12 17:47:21 +0530

    Update link to use NavLink in TeamLeftContainerItem.js

--- client/src/components/Team/TeamLeftContainerItem.js
+++ client/src/components/Team/TeamLeftContainerItem.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './TeamLeftContainerItem.css'
 
 const TeamLeftContainerItem = ({ item }) => {
     const { title, route, icon, notify_count } = item;
     return (
-        <Link to={route} className='TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'>
+        <NavLink to={route} className={({ isActive }) => (isActive ? "active-tab" : "") + ' TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'}>
             <i className={icon}></i>
             <div className='FRCB w100 pL10 title_notify'>
                 <span className='color555 title'>{title}</span>
                 {notify_count > 0 && <span className='color555 notify_count'>{notify_count}</span>}
             </div>
-        </Link>
+        </NavLink>
     )
 }
 
