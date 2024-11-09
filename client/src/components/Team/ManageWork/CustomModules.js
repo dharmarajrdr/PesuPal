452810b5b946bd9a36f5881730e85c05e98f7eed
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 15:40:48 +0530

    Update link to use NavLink in CustomModulesItem component

--- client/src/components/Team/ManageWork/CustomModules.js
+++ client/src/components/Team/ManageWork/CustomModules.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './CustomModulesItem.css';
 
 const CustomModulesItem = ({ item }) => {
     const { name, icon, icon_color, route, created_info, active } = item,
         { created_by, created_date } = created_info;
     return (
-        <Link to={route} className={(active ? 'ActiveCustomView' : '') + ' CustomModulesItem mR10 FRCC'} title={"Created by: " + created_by + " on " + created_date}>
+        <NavLink to={route} className={(active ? 'ActiveCustomView' : '') + ' CustomModulesItem mR10 FRCC'} title={"Created by: " + created_by + " on " + created_date}>
             <i className={icon + " pR10 img_20_20"} style={active ? {} : { color: icon_color }}></i>
             <span className='color555 pL5'>{name}</span>
-        </Link>
+        </NavLink>
     )
 }
 
