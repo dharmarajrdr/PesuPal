bf2ba875e9891938d6005a80cf19313d8203ed0d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 12:36:17 +0530

    Updating width from component

--- client/src/components/Team/TeamLayout.js
+++ client/src/components/Team/TeamLayout.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import TeamLeftContainer from './TeamLeftContainer'
 import TeamMainContainer from './TeamMainContainer'
 
 const TeamLayout = () => {
+    const leftNavigationState = useState(true),
+        [leftNavOpened,] = leftNavigationState;
     return (
         <div id='TeamLayout' className='Layout FRSS'>
-            <TeamLeftContainer />
-            <TeamMainContainer />
+            <TeamLeftContainer leftNavigationState={leftNavigationState} width={leftNavOpened ? "20%" : "75px"} />
+            <TeamMainContainer width={leftNavOpened ? "calc(100% - 20%)" : "calc(100% - 75px)"} />
         </div>
     )
 }
