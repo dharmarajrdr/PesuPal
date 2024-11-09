e135cc287201941d7a19f2ccb5c372c8de0fbd1e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 13:23:15 +0530

    Add left navigation state and dynamic width to FeedsLayout

--- client/src/components/Feeds/FeedsLayout.js
+++ client/src/components/Feeds/FeedsLayout.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './FeedsLayout.css'
 import FeedsLeftPanel from './FeedsLeftPanel/FeedsLeftPanel'
 import FeedsRightPanel from './FeedsRightPanel/FeedsRightPanel'
 import FeedsMainPanel from './FeedsMainPanel/FeedsMainPanel'
 
 const FeedsLayout = () => {
+    const leftNavigationState = useState(true),
+        [leftNavOpened,] = leftNavigationState,
+        width = {
+            'leftNavOpened': "24.5%",
+            "leftNavClosed": "85px"
+        }
     return (
         <div id='FeedsLayout' className='Layout FRCS'>
-            <FeedsLeftPanel />
-            <div className='FRSC w100 h100' id='FeedsMain'>
+            <FeedsLeftPanel leftNavigationState={leftNavigationState} width={leftNavOpened ? width.leftNavOpened : width.leftNavClosed} />
+            <div className='FRSC h100' id='FeedsMain' width={leftNavOpened ? `calc(100% - ${width.leftNavOpened})` : `calc(100% - ${width.leftNavClosed})`} >
                 <FeedsMainPanel />
                 <FeedsRightPanel />
             </div>
