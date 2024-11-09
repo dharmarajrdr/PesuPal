eeb5cdda005584af59863d6800957f6670feb5a4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 16:45:12 +0530

    Add PerformanceChart component and its CSS

---
+++ client/src/components/Tracker/KPI/PerformanceChart.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './PerformanceChart.css'
+
+const PerformanceChart = () => {
+  return (
+    <div className='PerformanceChart'>PerformanceChart</div>
+  )
+}
+
+export default PerformanceChart
