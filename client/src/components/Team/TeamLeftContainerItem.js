a0b9c8da89a2ef191a5d85f3d0bc98e9b67097a0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 12:43:59 +0530

    Add notification count to TeamLeftContainerItem

--- client/src/components/Team/TeamLeftContainerItem.js
+++ client/src/components/Team/TeamLeftContainerItem.js
@@ -2,14 +2,17 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 import './TeamLeftContainerItem.css'
 
-const TeamLeftContainerItem = ({ item }) => {
+const TeamLeftContainerItem = ({ item, leftNavOpened }) => {
     const { title, route, icon, notify_count } = item;
     return (
         <NavLink to={route} className={({ isActive }) => (isActive ? "active-tab" : "") + ' TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'}>
-            <i className={icon}></i>
+            <div className='pR'>
+                <i className={icon}></i>
+                {!leftNavOpened && notify_count > 0 && <span className='notifyCount'>{notify_count}</span>}
+            </div>
             <div className='FRCB w100 pL10 title_notify'>
                 <span className='color555 title'>{title}</span>
-                {notify_count > 0 && <span className='color555 notify_count'>{notify_count}</span>}
+                {leftNavOpened && notify_count > 0 && <span className='color555 notify_count'>{notify_count}</span>}
             </div>
         </NavLink>
     )
