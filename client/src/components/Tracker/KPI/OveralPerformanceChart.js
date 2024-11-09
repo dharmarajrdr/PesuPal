684c02f81de21139283a5414d559da824b9ebb71
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 16:45:07 +0530

    Add OverallPerformanceChart component and its CSS

---
+++ client/src/components/Tracker/KPI/OveralPerformanceChart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './OveralPerformanceChart.css';
+
+const OveralPerformanceChart = () => {
+  return (
+    <div className='OveralPerformanceChart w100'>
+        OveralPerformanceChart
+    </div>
+  )
+}
+
+export default OveralPerformanceChart
