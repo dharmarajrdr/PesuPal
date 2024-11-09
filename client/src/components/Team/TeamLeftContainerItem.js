196b6b6a208b3d65fe0a85d29d3e6fd4622806c7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 12:47:45 +0530

    Show hint/title when left nav is closed.

--- client/src/components/Team/TeamLeftContainerItem.js
+++ client/src/components/Team/TeamLeftContainerItem.js
@@ -5,7 +5,7 @@ import './TeamLeftContainerItem.css'
 const TeamLeftContainerItem = ({ item, leftNavOpened }) => {
     const { title, route, icon, notify_count } = item;
     return (
-        <NavLink to={route} className={({ isActive }) => (isActive ? "active-tab" : "") + ' TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'}>
+        <NavLink to={route} className={({ isActive }) => (isActive ? "active-tab" : "") + ' TeamLeftContainerItem FRCS w100 pX10 pY10 mx5'} title={leftNavOpened ? null : title}>
             <div className='pR'>
                 <i className={icon}></i>
                 {!leftNavOpened && notify_count > 0 && <span className='notifyCount'>{notify_count}</span>}
