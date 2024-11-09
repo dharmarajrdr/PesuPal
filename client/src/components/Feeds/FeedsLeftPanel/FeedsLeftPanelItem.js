d8b7391f2b2c52913b27d17f92ed84c8f9d7fd8e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2024-11-09 13:23:34 +0530

    Refactor FeedsLeftPanelItem component to conditionally render participants based on leftNavOpened prop

--- client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelItem.js
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelItem.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import './FeedsLeftPanelItem.css'
 
-const FeedsLeftPanelItem = ({ item }) => {
+const FeedsLeftPanelItem = ({ item, leftNavOpened }) => {
     const { icon, title, is_active, color, visibility, route, participants } = item;
     return (
-        <Link to={route} id='FeedsLeftPanelItem' className={`FRCS w100 ${is_active ? 'active' : ''}`}>
+        <Link to={route} id='FeedsLeftPanelItem' className={`FRCS w100 ${is_active ? 'active' : ''}`} title={leftNavOpened ? null : title}>
             <i className={icon} style={{ color }} ></i>
             <span className='noTextWrap'>{title}</span>
-            <span>{participants}</span>
+            {leftNavOpened && <span>{participants}</span>}
         </Link>
     )
 }
