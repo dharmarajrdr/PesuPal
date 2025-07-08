ce5358f837a9a69b44a6982ca96e77f0611d1931
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-08 08:10:07 +0530

    Add creation time auditing to CreationTimeAuditable model

--- server/src/main/java/com/pesupal/server/model/CreationTimeAuditable.java
+++ server/src/main/java/com/pesupal/server/model/CreationTimeAuditable.java
@@ -1,5 +1,6 @@
 package com.pesupal.server.model;
 
+import jakarta.persistence.PrePersist;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -8,4 +9,11 @@ import java.time.LocalDateTime;
 public class CreationTimeAuditable extends BaseModel {
 
     private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreationTime() {
+        if (this.createdAt == null) {
+            this.createdAt = LocalDateTime.now();
+        }
+    }
 }
