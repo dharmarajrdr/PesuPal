796043ce6c420417b6f072adeb9fbc3d01996050
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-08 21:49:39 +0530

    Enhance SecurityConfig to include user details service and password encoder for authentication

--- server/src/main/java/com/pesupal/server/config/SecurityConfig.java
+++ server/src/main/java/com/pesupal/server/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,8 +24,31 @@ public class SecurityConfig {
                         .anyRequest().authenticated()   // All other requests require authentication
         );
 
-        httpSecurity.formLogin(form -> form.permitAll());   // No authentication required for Login form
+        httpSecurity.formLogin(form ->
+                form.permitAll().defaultSuccessUrl("/login-successful")
+        );   // Configuring "No authentication required" for Login form
 
         return httpSecurity.build();
+    }
+
+    /**
+     * Login form authentication
+     */
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+
+        UserDetails user1 = User.withUsername("dharma").password(passwordEncoder.encode("123")).roles("USER").build();
+        UserDetails user2 = User.withUsername("mohan").password(passwordEncoder.encode("123")).roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
+
+    /**
+     * Password encoder bean for encoding passwords
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
     }
 }
