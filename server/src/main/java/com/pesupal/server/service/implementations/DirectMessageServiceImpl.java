9283f28784abe1dc152855d6ea82f2cff65e6405
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-09 18:38:08 +0530

    Add save method to DirectMessageServiceImpl for storing chat messages

--- server/src/main/java/com/pesupal/server/service/implementations/DirectMessageServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/DirectMessageServiceImpl.java
@@ -1,15 +1,23 @@
 package com.pesupal.server.service.implementations;
 
+import com.pesupal.server.config.RequestContext;
+import com.pesupal.server.dto.request.ChatMessageDto;
 import com.pesupal.server.dto.request.GetConversationBetweenUsers;
 import com.pesupal.server.dto.response.DirectMessageResponseDto;
 import com.pesupal.server.enums.ReadReceipt;
 import com.pesupal.server.exceptions.ActionProhibitedException;
 import com.pesupal.server.exceptions.DataNotFoundException;
 import com.pesupal.server.exceptions.PermissionDeniedException;
+import com.pesupal.server.helpers.Chat;
 import com.pesupal.server.model.chat.DirectMessage;
+import com.pesupal.server.model.org.Org;
+import com.pesupal.server.model.user.User;
 import com.pesupal.server.repository.DirectMessageRepository;
+import com.pesupal.server.security.SecurityUtil;
 import com.pesupal.server.service.interfaces.DirectMessageReactionService;
 import com.pesupal.server.service.interfaces.DirectMessageService;
+import com.pesupal.server.service.interfaces.OrgService;
+import com.pesupal.server.service.interfaces.UserService;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -24,10 +32,16 @@ public class DirectMessageServiceImpl implements DirectMessageService {
 
     private final DirectMessageRepository directMessageRepository;
     private final DirectMessageReactionService directMessageReactionService;
+    private final SecurityUtil securityUtil;
+    private final UserService userService;
+    private final OrgService orgService;
 
-    public DirectMessageServiceImpl(DirectMessageRepository directMessageRepository, @Lazy DirectMessageReactionService directMessageReactionService) {
+    public DirectMessageServiceImpl(DirectMessageRepository directMessageRepository, @Lazy DirectMessageReactionService directMessageReactionService, SecurityUtil securityUtil, UserService userService, OrgService orgService) {
         this.directMessageRepository = directMessageRepository;
         this.directMessageReactionService = directMessageReactionService;
+        this.securityUtil = securityUtil;
+        this.userService = userService;
+        this.orgService = orgService;
     }
 
     /**
@@ -94,4 +108,33 @@ public class DirectMessageServiceImpl implements DirectMessageService {
         directMessageRepository.delete(directMessage);
     }
 
+    /**
+     * Saves a chat message to the database.
+     *
+     * @param chatMessageDto
+     */
+    @Override
+    public void save(ChatMessageDto chatMessageDto) {
+
+        Long senderId = securityUtil.getCurrentUserId();
+        User sender = userService.getUserById(senderId);
+
+        Long receiverId = chatMessageDto.getReceiverId();
+        User receiver = userService.getUserById(receiverId);
+
+        Long orgId = RequestContext.getLong("X-ORG-ID");
+        Org org = orgService.getOrgById(orgId);
+
+        String chatId = Chat.getChatId(senderId, receiverId, orgId);
+        DirectMessage directMessage = new DirectMessage();
+        directMessage.setSender(sender);
+        directMessage.setReceiver(receiver);
+        directMessage.setOrg(org);
+        directMessage.setChatId(chatId);
+        directMessage.setDeleted(false);
+        directMessage.setContainsMedia(false);
+        directMessage.setReadReceipt(ReadReceipt.SENT);
+        directMessage.setMessage(chatMessageDto.getMessage());
+        directMessageRepository.save(directMessage);
+    }
 }
