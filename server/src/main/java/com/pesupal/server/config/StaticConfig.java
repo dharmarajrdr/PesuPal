649ec6aa82975015041222e6cffcd1f8e9a70981
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-09 17:37:54 +0530

    Add eligibility check for subscription plan creation based on user role

--- server/src/main/java/com/pesupal/server/config/StaticConfig.java
+++ server/src/main/java/com/pesupal/server/config/StaticConfig.java
@@ -1,8 +1,15 @@
 package com.pesupal.server.config;
 
+import com.pesupal.server.enums.Role;
+
 public class StaticConfig {
 
     public static final int MAXIMUM_TTL_MINUTES = 30;
 
     public static final int FREE_TRIAL_DAYS = 5;
+
+    public static boolean eligibleToCreateSubscriptionPlan(Role role) {
+
+        return role.equals(Role.SUPER_ADMIN);
+    }
 }
