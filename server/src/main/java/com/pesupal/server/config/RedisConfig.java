986014b4a39ee48f3ec20ae26884392672c64f93
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-09 13:26:36 +0530

    Add Redis configuration and dependency for RedisTemplate

---
+++ server/src/main/java/com/pesupal/server/config/RedisConfig.java
@@ -0,0 +1,15 @@
+package com.pesupal.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> getRedisTemplate() {
+
+        return new RedisTemplate<>();
+    }
+}
