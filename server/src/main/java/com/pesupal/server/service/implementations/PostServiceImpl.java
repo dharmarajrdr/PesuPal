d4c90ef5583700047a972809b0b977e27fbf644c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-10 16:19:17 +0530

    Refactor PostService and PostServiceImpl to return Post and PostDto objects, adding getPostById method for post retrieval

--- server/src/main/java/com/pesupal/server/service/implementations/PostServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/PostServiceImpl.java
@@ -1,11 +1,16 @@
 package com.pesupal.server.service.implementations;
 
 import com.pesupal.server.dto.request.CreatePostDto;
+import com.pesupal.server.dto.response.PostDto;
+import com.pesupal.server.dto.response.PostImpressionDto;
+import com.pesupal.server.dto.response.UserBasicInfoDto;
 import com.pesupal.server.enums.PostStatus;
+import com.pesupal.server.exceptions.DataNotFoundException;
 import com.pesupal.server.model.org.Org;
 import com.pesupal.server.model.post.Post;
 import com.pesupal.server.model.post.PostMedia;
 import com.pesupal.server.model.post.PostTag;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.model.user.User;
 import com.pesupal.server.repository.PostRepository;
 import com.pesupal.server.service.interfaces.*;
@@ -20,10 +25,10 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService {
 
     private final OrgService orgService;
+    private final TagService tagService;
     private final UserService userService;
     private final PostRepository postRepository;
     private final OrgMemberService orgMemberService;
-    private final TagService tagService;
 
     /**
      * Creates a new post.
@@ -31,7 +36,7 @@ public class PostServiceImpl implements PostService {
      * @param createPostDto
      */
     @Override
-    public void createPost(CreatePostDto createPostDto, Long userId, Long orgId) {
+    public Post createPost(CreatePostDto createPostDto, Long userId, Long orgId) {
 
         Org org = orgService.getOrgById(orgId);
         User user = userService.getUserById(userId);
@@ -47,6 +52,28 @@ public class PostServiceImpl implements PostService {
         post.setPostMedia(postMedia);
         List<PostTag> postTags = createPostDto.getTags().stream().map(tagName -> PostTag.builder().post(post).tag(tagService.createOrGet(tagName)).build()).collect(Collectors.toList());
         post.setTags(postTags);
-        postRepository.save(post);
+        return postRepository.save(post);
+    }
+
+    /**
+     * Retrieves a post by its ID.
+     *
+     * @param postId
+     * @param userId
+     * @param orgId
+     * @return PostDto
+     */
+    @Override
+    public PostDto getPostById(Long postId, Long userId, Long orgId) {
+
+        OrgMember orgMember = orgMemberService.getOrgMemberByUserIdAndOrgId(userId, orgId);
+
+        Post post = postRepository.findByIdAndOrgId(postId, orgId).orElseThrow(() -> new DataNotFoundException("Post not found with ID: " + postId));
+        PostDto postDto = PostDto.fromPost(post);
+        postDto.setTags(post.getTags().stream().map(postTag -> postTag.getTag().getName()).toList());
+        postDto.setMedia(post.getPostMedia().stream().map(PostMedia::getMediaId).toList());
+        postDto.setOwner(UserBasicInfoDto.fromOrgMember(orgMember));
+        postDto.setImpression(PostImpressionDto.builder().likes(post.getLikes().size()).comments(post.getComments().size()).build());
+        return postDto;
     }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/PostService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/PostService.java
@@ -1,8 +1,12 @@
 package com.pesupal.server.service.interfaces;
 
 import com.pesupal.server.dto.request.CreatePostDto;
+import com.pesupal.server.dto.response.PostDto;
+import com.pesupal.server.model.post.Post;
 
 public interface PostService {
 
-    void createPost(CreatePostDto createPostDto, Long userId, Long orgId);
+    Post createPost(CreatePostDto createPostDto, Long userId, Long orgId);
+
+    PostDto getPostById(Long postId, Long userId, Long orgId);
 }
