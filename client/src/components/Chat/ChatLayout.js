3171c29597c90fe573e35a115bb94f29b78aea5c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-10 10:51:54 +0530

    Add organization list and preview components with styling

--- client/src/components/Chat/ChatLayout.js
+++ client/src/components/Chat/ChatLayout.js
@@ -3,12 +3,14 @@ import ConversationScreen from './ConversationScreen/ConversationScreen'
 import ListOfChats from './ListOfChats/ListOfChats';
 import './ChatLayout.css'
 import SubscriptionPlan from '../Subscription/SubscriptionPlan';
+import OrgList from '../Org/OrgList';
 
 const ChatLayout = () => {
     return (
         <div className='Layout FRCS'>
             <ListOfChats />
             <ConversationScreen />
+            {/* <OrgList /> */}
             {/* <SubscriptionPlan /> */}
         </div>
     )
---
+++ client/src/components/Org/OrgPreview.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './OrgPreview.css';
+
+const OrgPreview = ({ org }) => {
+
+    const { id, displayName, role, displayPicture, members, status, subscription } = org;
+    const { planName, expiresAt, status: subscriptionStatus } = subscription || {};
+    const isOwner = role === 'ADMIN';
+    const isTrial = planName == 'FREE_TRIAL';
+
+    return (
+        <div className='FRCB org-preview p20' key={id}>
+            {isTrial && (
+                <h5 className='trial-badge'>
+                    TRIAL
+                </h5>
+            )}
+            <div className='display-picture FCCC'>
+                <img src={displayPicture} alt='Organization Logo' />
+            </div>
+            <div className='FCSS org-details'>
+                <div className='FRCB w100 mb5'>
+                    <b className='org-displayname'>{displayName}</b>
+                    <span className={`org-status ${subscriptionStatus.toLowerCase()}`}>
+                        {subscriptionStatus}
+                        <i className='fa fa-circle'></i>
+                    </span>
+                </div>
+                <div className='FRCB w100'>
+                    <span className='org-role'>
+                        <i className={`fa ${isOwner ? 'fa-user-shield' : 'fa-user'}`}></i>
+                        {role}
+                    </span>
+                    <span className='org-members-count'>
+                        <i class="fa-solid fa-users"></i>
+                        {members}
+                    </span>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OrgPreview
