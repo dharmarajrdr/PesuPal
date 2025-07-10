f33e0ff6eeabbd17841c04f7845facb1f4004d52
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-10 17:45:04 +0530

    add PostLikeController for liking and unliking posts

---
+++ server/src/main/java/com/pesupal/server/controller/PostLikeController.java
@@ -0,0 +1,30 @@
+package com.pesupal.server.controller;
+
+import com.pesupal.server.dto.response.ApiResponseDto;
+import com.pesupal.server.helpers.CurrentValueRetriever;
+import com.pesupal.server.service.interfaces.PostLikeService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/post/like")
+public class PostLikeController extends CurrentValueRetriever {
+
+    private final PostLikeService postLikeService;
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<ApiResponseDto> likePost(@PathVariable Long postId) {
+
+        postLikeService.likePost(postId, getCurrentUserId(), getCurrentOrgId());
+        return ResponseEntity.ok().body(new ApiResponseDto("Post liked successfully"));
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<ApiResponseDto> unlikePost(@PathVariable Long postId) {
+
+        postLikeService.unlikePost(postId, getCurrentUserId(), getCurrentOrgId());
+        return ResponseEntity.ok().body(new ApiResponseDto("Post unliked successfully"));
+    }
+}
