34709f35b3804ea0dc4ba43fb09a30373fc51d06
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-11 00:40:35 +0530

    Implement createFolder and getFolderById methods in FolderServiceImpl and update FolderService interface

--- server/src/main/java/com/pesupal/server/service/implementations/FolderServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/FolderServiceImpl.java
@@ -3,7 +3,15 @@ package com.pesupal.server.service.implementations;
 import com.pesupal.server.dto.request.CreateFolderDto;
 import com.pesupal.server.dto.response.FolderDto;
 import com.pesupal.server.enums.Workspace;
+import com.pesupal.server.exceptions.ActionProhibitedException;
+import com.pesupal.server.exceptions.DataNotFoundException;
+import com.pesupal.server.factory.WorkspaceFactory;
+import com.pesupal.server.model.user.OrgMember;
+import com.pesupal.server.model.workdrive.Folder;
+import com.pesupal.server.repository.FolderRepository;
 import com.pesupal.server.service.interfaces.FolderService;
+import com.pesupal.server.service.interfaces.OrgMemberService;
+import com.pesupal.server.service.interfaces.WorkdriveSpace;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,17 +21,50 @@ import java.util.List;
 @AllArgsConstructor
 public class FolderServiceImpl implements FolderService {
 
+    private final OrgMemberService orgMemberService;
+    private final FolderRepository folderRepository;
+    private final WorkspaceFactory workspaceFactory;
+
     /**
      * Creates a new folder in the specified workspace.
      *
      * @param createFolderDto
      * @param userId
      * @param orgId
-     * @return
+     * @return FolderDto
      */
     @Override
     public FolderDto createFolder(CreateFolderDto createFolderDto, Long userId, Long orgId) {
-        return null;
+
+        OrgMember orgMember = orgMemberService.getOrgMemberByUserIdAndOrgId(userId, orgId);
+
+        if (folderRepository.existsByNameAndSpaceAndParentFolderId(createFolderDto.getName(), createFolderDto.getSpace(), createFolderDto.getParentFolderId())) {
+            throw new ActionProhibitedException("A folder with the name '" + createFolderDto.getName() + "' already exists.");
+        }
+
+        Folder folder = createFolderDto.toFolder();
+        folder.setOrg(orgMember.getOrg());
+        folder.setOwner(orgMember.getUser());
+        if (createFolderDto.getParentFolderId() != null) {
+            folder.setParentFolder(getFolderById(createFolderDto.getParentFolderId()));
+        }
+        WorkdriveSpace workdriveSpace = workspaceFactory.getFactory(createFolderDto.getSpace());
+        folder = workdriveSpace.save(folder, createFolderDto, orgMember);
+        FolderDto folderDto = FolderDto.fromFolderAndOrgMember(folder, orgMember);
+        folderDto.setSecurity(createFolderDto.getSecurity());
+        return folderDto;
+    }
+
+    /**
+     * Retrieves a folder by its ID.
+     *
+     * @param folderId
+     * @return
+     */
+    @Override
+    public Folder getFolderById(Long folderId) {
+
+        return folderRepository.findById(folderId).orElseThrow(() -> new DataNotFoundException("Folder with ID " + folderId + " not found."));
     }
 
     /**
--- server/src/main/java/com/pesupal/server/service/interfaces/FolderService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/FolderService.java
@@ -3,12 +3,15 @@ package com.pesupal.server.service.interfaces;
 import com.pesupal.server.dto.request.CreateFolderDto;
 import com.pesupal.server.dto.response.FolderDto;
 import com.pesupal.server.enums.Workspace;
+import com.pesupal.server.model.workdrive.Folder;
 
 import java.util.List;
 
 public interface FolderService {
 
     FolderDto createFolder(CreateFolderDto createFolderDto, Long userId, Long orgId);
+
+    Folder getFolderById(Long folderId);
 
     List<FolderDto> getAllFolders(Workspace space, Long userId, Long orgId);
 
