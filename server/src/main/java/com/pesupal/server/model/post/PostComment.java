c48ecf59bfee3129755dafcabfdfd0e34556010f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-10 18:56:06 +0530

    Refactor PostComment class to simplify imports and ensure proper mapping for replies

--- server/src/main/java/com/pesupal/server/model/post/PostComment.java
+++ server/src/main/java/com/pesupal/server/model/post/PostComment.java
@@ -2,9 +2,11 @@ package com.pesupal.server.model.post;
 
 import com.pesupal.server.model.CreationTimeAuditable;
 import com.pesupal.server.model.user.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -17,4 +19,7 @@ public class PostComment extends CreationTimeAuditable {
     private User commenter;
 
     private String message;
+
+    @OneToMany(mappedBy = "postComment", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<PostReply> replies = new ArrayList<>();
 }
