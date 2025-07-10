de0c32f434c6b107a426ef2be438b5365b5637b2
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-10 16:18:41 +0530

    Refactor PostController to return created post in response and streamline imports

--- server/src/main/java/com/pesupal/server/controller/PostController.java
+++ server/src/main/java/com/pesupal/server/controller/PostController.java
@@ -3,14 +3,13 @@ package com.pesupal.server.controller;
 import com.pesupal.server.config.RequestContext;
 import com.pesupal.server.dto.request.CreatePostDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
+import com.pesupal.server.dto.response.PostDto;
+import com.pesupal.server.model.post.Post;
 import com.pesupal.server.security.SecurityUtil;
 import com.pesupal.server.service.interfaces.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -25,7 +24,15 @@ public class PostController {
 
         Long userId = securityUtil.getCurrentUserId();
         Long orgId = RequestContext.getLong("X-ORG-ID");
-        postService.createPost(createPostDto, userId, orgId);
-        return ResponseEntity.ok().body(new ApiResponseDto("Post created successfully"));
+        Post post = postService.createPost(createPostDto, userId, orgId);
+        return ResponseEntity.ok().body(new ApiResponseDto("Post created successfully", post));
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<ApiResponseDto> getPostById(@PathVariable Long postId) {
+        Long userId = securityUtil.getCurrentUserId();
+        Long orgId = RequestContext.getLong("X-ORG-ID");
+        PostDto post = postService.getPostById(postId, userId, orgId);
+        return ResponseEntity.ok().body(new ApiResponseDto("Post retrieved successfully.", post));
     }
 }
