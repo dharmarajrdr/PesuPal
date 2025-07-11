1211d7cce4196cbf44aedd5315d87d5efbc9c348
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-11 19:05:21 +0530

    Add CreateJobOpeningDto and JobOpeningDto classes; implement createJobOpening method in JobOpeningServiceImpl

--- server/src/main/java/com/pesupal/server/service/implementations/JobOpeningServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/JobOpeningServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pesupal.server.service.implementations;
 
+import com.pesupal.server.dto.request.CreateJobOpeningDto;
+import com.pesupal.server.dto.response.JobOpeningDto;
+import com.pesupal.server.repository.JobOpeningRepository;
 import com.pesupal.server.service.interfaces.JobOpeningService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,4 +10,17 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class JobOpeningServiceImpl implements JobOpeningService {
+
+    private final JobOpeningRepository jobOpeningRepository;
+
+    /**
+     * Create a new job opening.
+     *
+     * @param createJobOpeningDto
+     * @return JobOpeningDto
+     */
+    @Override
+    public JobOpeningDto createJobOpening(CreateJobOpeningDto createJobOpeningDto) {
+        return null;
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/JobOpeningService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/JobOpeningService.java
@@ -1,4 +1,9 @@
 package com.pesupal.server.service.interfaces;
 
+import com.pesupal.server.dto.request.CreateJobOpeningDto;
+import com.pesupal.server.dto.response.JobOpeningDto;
+
 public interface JobOpeningService {
+
+    JobOpeningDto createJobOpening(CreateJobOpeningDto createJobOpeningDto);
 }
