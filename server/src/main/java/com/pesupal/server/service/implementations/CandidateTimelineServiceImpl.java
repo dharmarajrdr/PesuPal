4eee21618a515b4cd7e22145f0b785eb898acebc
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-11 19:05:31 +0530

    Add CreateCandidateTimelineDto and CandidateTimelineDto classes; implement CandidateTimelineService methods for managing candidate timelines

--- server/src/main/java/com/pesupal/server/service/implementations/CandidateTimelineServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/CandidateTimelineServiceImpl.java
@@ -1,10 +1,40 @@
 package com.pesupal.server.service.implementations;
 
+import com.pesupal.server.dto.request.CreateCandidateTimelineDto;
+import com.pesupal.server.dto.response.CandidateTimelineDto;
 import com.pesupal.server.service.interfaces.CandidateTimelineService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CandidateTimelineServiceImpl implements CandidateTimelineService {
+
+    /**
+     * Creates a new candidate timeline entry.
+     *
+     * @param createCandidateTimelineDto
+     * @param userId
+     * @param orgId
+     * @return
+     */
+    @Override
+    public CandidateTimelineDto createCandidateTimeline(CreateCandidateTimelineDto createCandidateTimelineDto, Long userId, Long orgId) {
+        return null;
+    }
+
+    /**
+     * Retrieves the timeline of a candidate by their ID.
+     *
+     * @param candidateId
+     * @param userId
+     * @param orgId
+     * @return
+     */
+    @Override
+    public List<CandidateTimelineDto> getTimelineByCandidateIdAndJobId(Long candidateId, Long jobId, Long userId, Long orgId) {
+        return List.of();
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/CandidateTimelineService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/CandidateTimelineService.java
@@ -1,4 +1,13 @@
 package com.pesupal.server.service.interfaces;
 
+import com.pesupal.server.dto.request.CreateCandidateTimelineDto;
+import com.pesupal.server.dto.response.CandidateTimelineDto;
+
+import java.util.List;
+
 public interface CandidateTimelineService {
+
+    CandidateTimelineDto createCandidateTimeline(CreateCandidateTimelineDto createCandidateTimelineDto, Long userId, Long orgId);
+
+    List<CandidateTimelineDto> getTimelineByCandidateIdAndJobId(Long candidateId, Long jobId, Long userId, Long orgId);
 }
