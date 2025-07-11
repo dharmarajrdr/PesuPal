945efeae9ca8f58f34291dabc35d50291965c74f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-11 19:54:35 +0530

    Implement getCandidateById method in CandidateService and update CandidateServiceImpl to include candidate timeline creation on status update

--- server/src/main/java/com/pesupal/server/service/implementations/CandidateServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/CandidateServiceImpl.java
@@ -1,11 +1,19 @@
 package com.pesupal.server.service.implementations;
 
 import com.pesupal.server.dto.request.CreateCandidateDto;
+import com.pesupal.server.dto.request.CreateCandidateTimelineDto;
 import com.pesupal.server.dto.request.CreateReferralDto;
 import com.pesupal.server.dto.response.CandidateDto;
 import com.pesupal.server.enums.JobApplicationStatus;
+import com.pesupal.server.exceptions.ActionProhibitedException;
+import com.pesupal.server.exceptions.DataNotFoundException;
+import com.pesupal.server.exceptions.PermissionDeniedException;
+import com.pesupal.server.model.recruit.Candidate;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.repository.CandidateRepository;
 import com.pesupal.server.service.interfaces.CandidateService;
+import com.pesupal.server.service.interfaces.CandidateTimelineService;
+import com.pesupal.server.service.interfaces.OrgMemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +23,21 @@ import java.util.List;
 @AllArgsConstructor
 public class CandidateServiceImpl implements CandidateService {
 
+    private final OrgMemberService orgMemberService;
     private final CandidateRepository candidateRepository;
+    private final CandidateTimelineService candidateTimelineService;
+
+    /**
+     * Retrieves a candidate by their ID.
+     *
+     * @param candidateId
+     * @return
+     */
+    @Override
+    public Candidate getCandidateById(Long candidateId) {
+
+        return candidateRepository.findById(candidateId).orElseThrow(() -> new DataNotFoundException("Candidate with ID " + candidateId + " not found."));
+    }
 
     /**
      * Creates a new candidate for a job opening.
@@ -65,5 +87,24 @@ public class CandidateServiceImpl implements CandidateService {
     @Override
     public void updateCandidateStatus(Long candidateId, JobApplicationStatus status, Long userId, Long orgId) {
 
+        List<String> eligibleRoles = List.of("HR", "Human Resource", "Recruiter");
+
+        OrgMember orgMember = orgMemberService.getOrgMemberByUserIdAndOrgId(userId, orgId);
+        if (!eligibleRoles.contains(orgMember.getDesignation().getName())) {
+            throw new PermissionDeniedException("You do not have permission to update candidate status.");
+        }
+
+        Candidate candidate = getCandidateById(candidateId);
+        if (candidate.getStatus().equals(status)) {
+            throw new ActionProhibitedException("Candidate status is already set to " + status + ".");
+        }
+
+        candidate.setStatus(status);
+        candidateRepository.save(candidate);
+
+        CreateCandidateTimelineDto createCandidateTimelineDto = new CreateCandidateTimelineDto();
+        createCandidateTimelineDto.setCandidate(candidate);
+        createCandidateTimelineDto.setDescription("Status updated from " + candidate.getStatus() + " to " + status + " by " + orgMember.getDisplayName() + ".");
+        candidateTimelineService.createCandidateTimeline(createCandidateTimelineDto);
     }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/CandidateService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/CandidateService.java
@@ -4,10 +4,13 @@ import com.pesupal.server.dto.request.CreateCandidateDto;
 import com.pesupal.server.dto.request.CreateReferralDto;
 import com.pesupal.server.dto.response.CandidateDto;
 import com.pesupal.server.enums.JobApplicationStatus;
+import com.pesupal.server.model.recruit.Candidate;
 
 import java.util.List;
 
 public interface CandidateService {
+
+    Candidate getCandidateById(Long candidateId);
 
     CandidateDto createCandidate(CreateCandidateDto createCandidateDto, Long userId, Long orgId);
 
