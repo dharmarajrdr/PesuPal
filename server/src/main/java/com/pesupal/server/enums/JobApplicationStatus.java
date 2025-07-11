a27b1ea63b38d09282e325ba64d594109b22e85b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-11 18:29:47 +0530

    Add enums for JobApplicationStatus, JobOpeningStatus, and JobType

---
+++ server/src/main/java/com/pesupal/server/enums/JobApplicationStatus.java
@@ -0,0 +1,13 @@
+package com.pesupal.server.enums;
+
+public enum JobApplicationStatus {
+
+    UNDER_REVIEW,
+    SHORTLISTED,
+    INTERVIEWING,
+    ON_HOLD,
+    REJECTED_BY_INTERVIEWER,
+    REJECTED_BY_CANDIDATE,
+    OFFERED,
+    ONBOARDED;
+}
