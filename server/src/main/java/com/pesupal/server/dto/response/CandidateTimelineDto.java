9cf27e23ed36fa0e14c472a15a18a8c2711da6ca
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 10:09:49 +0530

    Refactor CandidateTimelineDto to include createdBy field and enhance fromCandidateTimelineAndOrgMember method

--- server/src/main/java/com/pesupal/server/dto/response/CandidateTimelineDto.java
+++ server/src/main/java/com/pesupal/server/dto/response/CandidateTimelineDto.java
@@ -1,7 +1,26 @@
 package com.pesupal.server.dto.response;
 
+import com.pesupal.server.model.recruit.CandidateTimeline;
+import com.pesupal.server.model.user.OrgMember;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class CandidateTimelineDto {
+
+    private UserBasicInfoDto createdBy;
+
+    private LocalDateTime createdAt;
+
+    private String description;
+
+    public static CandidateTimelineDto fromCandidateTimelineAndOrgMember(CandidateTimeline candidateTimeline, OrgMember orgMember) {
+
+        CandidateTimelineDto dto = new CandidateTimelineDto();
+        dto.setCreatedAt(candidateTimeline.getCreatedAt());
+        dto.setDescription(candidateTimeline.getDescription());
+        dto.setCreatedBy(UserBasicInfoDto.fromOrgMember(orgMember));
+        return dto;
+    }
 }
