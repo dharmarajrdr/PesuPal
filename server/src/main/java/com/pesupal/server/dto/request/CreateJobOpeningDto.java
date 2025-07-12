2de4c3d730f0ed8341ed207f46e1b18c01b0047f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 09:30:36 +0530

    Add JobOpeningFilterDto class for filtering job openings by status

--- server/src/main/java/com/pesupal/server/dto/request/CreateJobOpeningDto.java
+++ server/src/main/java/com/pesupal/server/dto/request/CreateJobOpeningDto.java
@@ -1,7 +1,44 @@
 package com.pesupal.server.dto.request;
 
+import com.pesupal.server.enums.JobType;
+import com.pesupal.server.model.recruit.JobOpening;
+import com.pesupal.server.model.recruit.JobOpeningCriteria;
 import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @Data
 public class CreateJobOpeningDto {
+
+    private String title;
+
+    private String description;
+
+    private String location;
+
+    private Long budget;
+
+    private LocalDateTime openTill;
+
+    private Integer positionsAvailable;
+
+    private JobType jobType;
+
+    private Map<String, String> criteria;
+
+
+    public JobOpening toJobOpening() {
+
+        JobOpening jobOpening = new JobOpening();
+        jobOpening.setTitle(this.getTitle());
+        jobOpening.setDescription(this.getDescription());
+        jobOpening.setLocation(this.getLocation());
+        jobOpening.setBudget(this.getBudget());
+        jobOpening.setOpenTill(this.getOpenTill());
+        jobOpening.setPositionsAvailable(this.getPositionsAvailable());
+        jobOpening.setJobType(this.getJobType());
+        jobOpening.setCriteria(this.getCriteria().entrySet().stream().map(entry -> new JobOpeningCriteria(entry.getKey(), entry.getValue())).toList());
+        return jobOpening;
+    }
 }
