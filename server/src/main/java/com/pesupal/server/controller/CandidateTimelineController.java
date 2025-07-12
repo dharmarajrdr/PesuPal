fe6e9f2590117fb24acb9bda0f0cf351d019f206
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 10:09:43 +0530

    Add getCandidateTimeline endpoint to retrieve candidate timeline events

--- server/src/main/java/com/pesupal/server/controller/CandidateTimelineController.java
+++ server/src/main/java/com/pesupal/server/controller/CandidateTimelineController.java
@@ -1,8 +1,12 @@
 package com.pesupal.server.controller;
 
+import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.helpers.CurrentValueRetriever;
 import com.pesupal.server.service.interfaces.CandidateTimelineService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CandidateTimelineController extends CurrentValueRetriever {
 
     private final CandidateTimelineService candidateTimelineService;
+
+    @GetMapping("/{candidateId}")
+    public ResponseEntity<ApiResponseDto> getCandidateTimeline(@PathVariable Long candidateId) {
+
+        var timelineEvents = candidateTimelineService.getTimelineByCandidateIdAndJobId(candidateId, getCurrentUserId(), getCurrentOrgId());
+        return ResponseEntity.ok(new ApiResponseDto("Candidate timeline retrieved successfully", timelineEvents));
+    }
 }
