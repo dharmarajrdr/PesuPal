78e815ed580fd2e01389a7d84c8943e8c7ee5758
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 09:30:55 +0530

    Add method to find job openings by organization ID and status

--- server/src/main/java/com/pesupal/server/repository/JobOpeningRepository.java
+++ server/src/main/java/com/pesupal/server/repository/JobOpeningRepository.java
@@ -1,9 +1,15 @@
 package com.pesupal.server.repository;
 
+import com.pesupal.server.enums.JobOpeningStatus;
 import com.pesupal.server.model.recruit.JobOpening;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JobOpeningRepository extends JpaRepository<JobOpening, Long> {
+
+    List<JobOpening> findAllByOrgIdAndStatus(Long orgId, JobOpeningStatus status);
+
 }
