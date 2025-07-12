419806fbbb47e6a48691364d3b4e089891b63fa6
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 10:10:15 +0530

    Add missing import statements in CandidateRepository and CandidateTimelineRepository

--- server/src/main/java/com/pesupal/server/repository/CandidateTimelineRepository.java
+++ server/src/main/java/com/pesupal/server/repository/CandidateTimelineRepository.java
@@ -1,9 +1,14 @@
 package com.pesupal.server.repository;
 
+import com.pesupal.server.model.recruit.Candidate;
 import com.pesupal.server.model.recruit.CandidateTimeline;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CandidateTimelineRepository extends JpaRepository<CandidateTimeline, Long> {
+
+    List<CandidateTimeline> findAllByCandidateOrderByCreatedAtDesc(Candidate candidate);
 }
