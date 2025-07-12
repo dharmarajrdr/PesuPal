f5e80cc4cff2a8ab22cc0ad0f1faa7cdff58cb24
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-12 13:38:39 +0530

    Enhance CreateCandidateDto and CandidateDto with userId and referredBy fields for improved candidate tracking and referral handling

--- server/src/main/java/com/pesupal/server/dto/response/CandidateDto.java
+++ server/src/main/java/com/pesupal/server/dto/response/CandidateDto.java
@@ -1,7 +1,40 @@
 package com.pesupal.server.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.pesupal.server.enums.JobApplicationStatus;
+import com.pesupal.server.model.recruit.Candidate;
+import com.pesupal.server.model.user.OrgMember;
 import lombok.Data;
 
+import java.util.List;
+import java.util.UUID;
+
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CandidateDto {
+
+    private String name;
+
+    private JobOpeningDto jobOpening;
+
+    private UUID resumeId;
+
+    private UserBasicInfoDto referredBy;
+
+    private JobApplicationStatus applicationStatus;
+
+    private List<CandidateTimelineDto> timeline;
+
+    public static CandidateDto fromCandidateAndOrgMember(Candidate candidate, OrgMember referredByOrgMember) {
+
+        CandidateDto candidateDto = new CandidateDto();
+        candidateDto.setName(candidate.getName());
+        candidateDto.setResumeId(candidate.getResumeId());
+        candidateDto.setApplicationStatus(candidate.getStatus());
+        candidateDto.setJobOpening(JobOpeningDto.fromJobOpening(candidate.getJobOpening()));
+        if (referredByOrgMember != null) {
+            candidateDto.setReferredBy(UserBasicInfoDto.fromOrgMember(referredByOrgMember));
+        }
+        return candidateDto;
+    }
 }
