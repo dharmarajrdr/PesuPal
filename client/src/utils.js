5f2fdf392a6201953c16c32d9443f2b2eb232995
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-13 16:47:25 +0530

    Fix typo in agoTimeCalculator function: change 'second' to 'sec'

--- client/src/utils.js
+++ client/src/utils.js
@@ -123,6 +123,6 @@ export default {
         if (days > 0) return `${days} day${days > 1 ? 's' : ''} ago`;
         if (hours > 0) return `${hours} hr${hours > 1 ? 's' : ''} ago`;
         if (minutes > 0) return `${minutes} min${minutes > 1 ? 's' : ''} ago`;
-        return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+        return `${seconds} sec${seconds > 1 ? 's' : ''} ago`;
     }
 }
