502b1564f25031e938cfd19a2f7b66641a10c9b9
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-13 18:04:51 +0530

    Add loading and error handling to OrgList component

--- client/src/components/Org/OrgList.js
+++ client/src/components/Org/OrgList.js
@@ -1,13 +1,35 @@
-import React from 'react'
-import OrgListDetails from './OrgListDetails'
+import { useEffect, useState } from 'react'
 import OrgPreview from './OrgPreview'
 import './OrgList.css'
+import ErrorMessage from '../ErrorMessage';
+import { apiRequest } from '../../http_request';
+import Loader from '../Loader';
 
 const OrgList = ({ toggleOrgList, closeOrgList }) => {
+
+    const [orgListDetails, setOrgListDetails] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        apiRequest('/api/v1/people/orgs', 'GET').then(({ data }) => {
+            setLoading(false);
+            setOrgListDetails(data);
+        }).catch((error) => {
+            setLoading(false);
+            setError(error.message);
+        });
+    }, []);
     return (
         <div className='FCCS w100' id='org-list-modal' onClick={closeOrgList}>
-            <div className='org-list-popup FCCB'>
-                {OrgListDetails.map((org, index) => <OrgPreview org={org} key={index} toggleOrgList={toggleOrgList} />)}
+            <div className='org-list-popup FCCS'>
+                {
+                    loading ? <Loader /> :
+                        error ? <ErrorMessage message={error} /> :
+                            orgListDetails.length ?
+                                orgListDetails.map((org, index) => <OrgPreview org={org} key={index} toggleOrgList={toggleOrgList} />) :
+                                <p className='no-orgs'>No organizations found</p>
+                }
             </div>
         </div>
     )
