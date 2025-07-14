2292f8f51190e291d9ff79b38feb08e69152367e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-14 13:34:09 +0530

    Implement PinnedDirectMessageService for retrieving pinned direct messages in an organization

---
+++ server/src/main/java/com/pesupal/server/service/implementations/PinnedDirectMessageServiceImpl.java
@@ -0,0 +1,38 @@
+package com.pesupal.server.service.implementations;
+
+import com.pesupal.server.dto.request.PinnedDirectMessageDto;
+import com.pesupal.server.model.org.Org;
+import com.pesupal.server.model.user.OrgMember;
+import com.pesupal.server.repository.PinnedDirectMessageRepository;
+import com.pesupal.server.service.interfaces.OrgMemberService;
+import com.pesupal.server.service.interfaces.PinnedDirectMessageService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PinnedDirectMessageServiceImpl implements PinnedDirectMessageService {
+
+    private final PinnedDirectMessageRepository pinnedDirectMessageRepository;
+    private final OrgMemberService orgMemberService;
+
+    /**
+     * Retrieves all pinned direct messages for the current user in the current organization.
+     *
+     * @param userId
+     * @param orgId
+     * @return
+     */
+    @Override
+    public List<PinnedDirectMessageDto> getAllPinnedDirectMessages(Long userId, Long orgId) {
+
+        OrgMember orgMember = orgMemberService.getOrgMemberByUserIdAndOrgId(userId, orgId);
+        Org org = orgMember.getOrg();
+        return pinnedDirectMessageRepository.findAllByOrgIdAndPinnedByIdOrderByOrderIndexAscPinnedUser_IdAsc(userId, orgId).stream().map(pinnedDirectMessage -> {
+            OrgMember pinnedUser = orgMemberService.getOrgMemberByUserAndOrg(pinnedDirectMessage.getPinnedUser(), org);
+            return PinnedDirectMessageDto.fromUserAndOrgMember(orgMember.getUser(), pinnedUser);
+        }).toList();
+    }
+}
