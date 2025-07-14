1200991ff0975f3b552d302c54b16111f1a13bff
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-14 22:52:06 +0530

    Implement DepartmentMain component with member loading logic and styling

--- client/src/components/Department/DepartmentMain.js
+++ client/src/components/Department/DepartmentMain.js
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react';
+import './DepartmentMain.css';
+import OrgMembers from './OrgMembers';
+import { apiRequest } from '../../http_request';
+import Loader from '../Loader';
+import ErrorMessage from '../ErrorMessage';
 
-const DepartmentMain = () => {
-  return (
-    <div>DepartmentMain</div>
-  )
+const DepartmentMain = ({ departmentId }) => {
+
+  const [orgMembersList, setOrgMembersList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    apiRequest(`/api/v1/department/${departmentId}/members`, "GET").then(({ data }) => {
+      setLoading(false);
+      setOrgMembersList(data);
+    }).catch(({ message }) => {
+      setLoading(false);
+      setError(message);
+    });
+  }, []);
+
+  const availableMembers = orgMembersList.filter(member => member.status === 'available');
+  const unavailableMembers = orgMembersList.filter(member => member.status !== 'available');
+
+  return loading ? <Loader /> :
+    error ? <ErrorMessage message={error} /> : (
+      <div id="department-main" className='w100 h100 FRSB'>
+        <OrgMembers title="Available Members" orgMembersList={availableMembers} noMembersAvailableMessage="Currently, all of them are offline." />
+        <OrgMembers title="Unavailable Members" orgMembersList={unavailableMembers} noMembersAvailableMessage="Currently, all of them are online." />
+      </div>
+    )
 }
 
 export default DepartmentMain
