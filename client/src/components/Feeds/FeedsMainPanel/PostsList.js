31b2fb4324f29228eeb1a89db1af630de7f3bc22
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-14 14:30:55 +0530

    Refactor PostsList to unify post structure and enhance data representation; removed author details and added media and impression fields.

--- client/src/components/Feeds/FeedsMainPanel/PostsList.js
+++ client/src/components/Feeds/FeedsMainPanel/PostsList.js
@@ -1,53 +1,37 @@
 export default [
+
     {
-        'author': {
-            'name': 'John Doe',
-            'image': '/images/Users/user_3.jpg'
+        "id": 1,
+        "owner": {
+            "userId": 1,
+            "displayName": "Dharmaraj",
+            "displayPicture": "https://avatars.githubusercontent.com/u/66894285?v=4",
+            "designation": "HR",
+            "department": "Executive Department",
+            "status": "available",
+            "email": "dharmaraj.171215@gmail.com",
+            "phone": "6383392245"
         },
-        'title': 'Hello world!',
-        'content': 'This is my first post on this platform. I am excited to share my thoughts with you all.',
-        'created_at': '2021-08-01T12:00:00Z',
-        'likes': 10,
-        'comments': 5
-    },
-    {
-        'author': {
-            'name': 'Dharmaraj R',
-            'image': '/images/Users/user_7.jpg'
+        "title": "You can make your anchor tags visually consistent with your brand",
+        "description": `Lorem Ipsum is simply <b>dummy text</b> of the printing and typesetting industry. Lorem Ipsum has been the <u>industrys standard dummy</u> text ever since the 1500s, when an <a href="https://localhost:8080" target="_blank" rel="noopener noreferrer">unknown printer</a> took a galley of type and scrambled it to make a type specimen book. <br /><br /> It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of <i>Letraset sheets containing Lorem Ipsum passages</i>, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+        "createdAt": "2025-07-14T13:35:24.705911",
+        "tags": [
+            "#zoho",
+            "#chennai"
+        ],
+        "media": [
+            "00bb0d71-fe90-4325-99b8-13529ef63037",
+            "00bb0d71-fe90-4325-99b8-13529ef63037",
+            "00bb0d71-fe90-4325-99b8-13529ef63037",
+            "00bb0d71-fe90-4325-99b8-13529ef63037",
+            "00bb0d71-fe90-4325-99b8-13529ef63037"
+        ],
+        "impression": {
+            "likes": 20,
+            "comments": 30
         },
-        'content': 'Participated in System Deign of Instagram. It was a great learning experience.',
-        'created_at': '2024-08-12T12:20:20Z',
-        'likes': 10,
-        'comments': 5,
-        'media': [
-            '/images/Posts/post_1.jpg',
-            '/images/Posts/post_4.jpg'
-        ],
-        'mentions': [
-            '@cricket_addicts'
-        ],
-        'tags': [
-            '#cricket', '#viratkohli', '#sachintendulkar'
-        ]
-    },
-    {
-        'author': {
-            'name': 'Sara Smith',
-            'image': '/images/Users/user_5.jpg'
-        },
-        'content': 'Finally, Virat Kohli has scored his 50th century in international cricket. Congratulations to him!',
-        'created_at': '2021-08-01T12:00:00Z',
-        'likes': 10,
-        'comments': 5,
-        'media': [
-            '/images/Posts/post_2.jpg',
-            '/images/Posts/post_3.jpg',
-        ],
-        'mentions': [
-            '@cricket_addicts'
-        ],
-        'tags': [
-            '#cricket', '#viratkohli', '#sachintendulkar'
-        ]
+        "commentable": true,
+        "shareable": true,
+        "bookmarkable": true
     }
 ]
