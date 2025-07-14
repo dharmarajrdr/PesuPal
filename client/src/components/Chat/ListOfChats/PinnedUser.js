27100bec706c06cbcd8f1e0394377f36d418cb9e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-14 13:33:16 +0530

    Refactor PinnedUser and PinnedUsers components to use item prop for better data handling and add error handling for API requests

--- client/src/components/Chat/ListOfChats/PinnedUser.js
+++ client/src/components/Chat/ListOfChats/PinnedUser.js
@@ -2,15 +2,18 @@ import React from 'react'
 import './PinnedUsers.css'
 import { StatusIndicator } from '../../Auth/utils'
 
-const PinnedUser = ({ name, status, image }) => {
+const PinnedUser = ({ item }) => {
+
     const customStyle = { bottom: '0px', right: '-3px', fontSize: '9px', padding: '0px' };
+    const { displayName, displayPicture, status, chatId } = item || {};
+
     return (
         <div className='PinnedUserContainer FCCC'>
             <div className='pR'>
-                <img src={image} />
+                <img src={displayPicture} className='objectFitCover' />
                 <StatusIndicator status={status} style={customStyle} />
             </div>
-            <span>{name}</span>
+            <span>{displayName}</span>
         </div>
     )
 }
