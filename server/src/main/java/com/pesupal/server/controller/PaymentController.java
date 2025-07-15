66087605604d100530199b3fdafe73f3a4064024
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 18:52:23 +0530

    Refactor PaymentController to return ApiResponseDto for payment link generation

--- server/src/main/java/com/pesupal/server/controller/PaymentController.java
+++ server/src/main/java/com/pesupal/server/controller/PaymentController.java
@@ -1,11 +1,12 @@
 package com.pesupal.server.controller;
 
+import com.pesupal.server.dto.response.ApiResponseDto;
+import com.pesupal.server.service.interfaces.PaymentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.pesupal.server.service.interfaces.PaymentService;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,8 +16,9 @@ public class PaymentController {
     private final PaymentService paymentService;
 
     @PostMapping("/payment-link")
-    public String generatePaymentLink() throws Exception {
+    public ResponseEntity<ApiResponseDto> generatePaymentLink() throws Exception {
 
-        return this.paymentService.initiatePayment();
+        String paymentLink = this.paymentService.initiatePayment();
+        return ResponseEntity.ok().body(new ApiResponseDto("Payment link generated successfully", paymentLink));
     }
 }
