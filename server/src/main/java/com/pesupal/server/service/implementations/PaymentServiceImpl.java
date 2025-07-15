22ac7df3ca11e49c40a6bc6ad5877999b7ad4c91
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 18:35:10 +0530

    Add PaymentService interface and PaymentServiceImpl class for payment processing functionality

---
+++ server/src/main/java/com/pesupal/server/service/implementations/PaymentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pesupal.server.service.implementations;
+
+import com.pesupal.server.factory.PaymentGatewayFactory;
+import com.pesupal.server.enums.SupportedGateway;
+import com.pesupal.server.service.interfaces.PaymentService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentGatewayFactory paymentGatewayFactory;
+
+    @Override
+    public String initiatePayment() throws Exception {
+
+        SupportedGateway gateway = SupportedGateway.STRIPE; // Dynamic
+        return paymentGatewayFactory.getPaymentGateway(gateway).generatePaymentLink();
+    }
+}
---
+++ server/src/main/java/com/pesupal/server/service/interfaces/PaymentService.java
@@ -0,0 +1,6 @@
+package com.pesupal.server.service.interfaces;
+
+public interface PaymentService {
+
+    String initiatePayment() throws Exception;
+}
