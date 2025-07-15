432c4ee296a178e32ba3e71c75928c38586532d0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 22:12:48 +0530

    Add ListOfSettingNavigations component for user settings navigation

---
+++ client/src/components/Settings/ListOfSettingNavigations.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        title: 'General',
+        route: '/settings/general',
+        icon: 'fa fa-cog',
+        active: false,
+        has_access: true
+    },
+    {
+        title: 'Account',
+        route: '/settings/account',
+        icon: 'fa fa-user',
+        active: false,
+        has_access: true
+    },
+    {
+        title: 'Manage People',
+        route: '/settings/manage-admins',
+        icon: 'fa fa-users',
+        active: false,
+        has_access: true
+    },
+    {
+        title: 'Subscription',
+        route: '/settings/subscription',
+        icon: 'fa fa-credit-card',
+        active: false,
+        has_access: true
+    },
+    {
+        title: 'Privacy',
+        route: '/settings/privacy',
+        icon: 'fa fa-lock',
+        active: false,
+        has_access: true
+    },
+    {
+        title: 'Configurations',
+        route: '/settings/configurations',
+        icon: 'fa fa-wrench',
+        active: false,
+        has_access: false
+    }
+]
