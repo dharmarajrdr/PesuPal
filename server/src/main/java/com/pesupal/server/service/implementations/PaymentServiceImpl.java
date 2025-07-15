161315980addf1c01b53beef0b93cda13bc0fcab
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 20:40:33 +0530

    Update initiatePayment method to accept PaymentDto and generate payment link

--- server/src/main/java/com/pesupal/server/service/implementations/PaymentServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/PaymentServiceImpl.java
@@ -1,7 +1,8 @@
 package com.pesupal.server.service.implementations;
 
-import com.pesupal.server.factory.PaymentGatewayFactory;
+import com.pesupal.server.dto.request.PaymentDto;
 import com.pesupal.server.enums.SupportedGateway;
+import com.pesupal.server.factory.PaymentGatewayFactory;
 import com.pesupal.server.service.interfaces.PaymentService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,9 @@ public class PaymentServiceImpl implements PaymentService {
     private final PaymentGatewayFactory paymentGatewayFactory;
 
     @Override
-    public String initiatePayment() throws Exception {
+    public String initiatePayment(PaymentDto paymentDto) throws Exception {
 
         SupportedGateway gateway = SupportedGateway.STRIPE; // Dynamic
-        return paymentGatewayFactory.getPaymentGateway(gateway).generatePaymentLink();
+        return paymentGatewayFactory.getPaymentGateway(gateway).generatePaymentLink(paymentDto);
     }
 }
