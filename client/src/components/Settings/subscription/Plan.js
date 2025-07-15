db39f1ff1de7b91a0f1053c82e5f0b919c8e8906
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 22:38:54 +0530

    Refactor SubscriptionPlan component to fetch plans from API and remove PlanDetails; update Plan component to use 'badge' instead of 'highlight'

--- client/src/components/Settings/subscription/Plan.js
+++ client/src/components/Settings/subscription/Plan.js
@@ -2,13 +2,13 @@ import './Plan.css';
 
 const Plan = ({ plan }) => {
 
-    const { id, name, price, interval, code, active, description, numberOfDays, features, highlight } = plan;
+    const { id, name, price, interval, code, active, description, numberOfDays, features, badge } = plan;
 
     return active ? (
         <div className='plan FCCS bgFFF h100' id={id}>
             {/* Recommended tag */}
-            {highlight && <p className='recommended-plan ribbon'>{highlight}</p>}
-            <h4 className='plan-code black-text'>{code}</h4>
+            {badge && <p className='recommended-plan ribbon'>{badge}</p>}
+            <h4 className='plan-code black-text'>{name}</h4>
             <div className='FCCC plan-price'>
                 <h5 className='black-text mL5'>{interval}</h5>
                 <div className='FRCC w100'>
@@ -17,14 +17,16 @@ const Plan = ({ plan }) => {
                 </div>
             </div>
             <h5 className='black-text plan-validity-days'>Validity {numberOfDays} days</h5>
-            <button className='tryButton bgFFF' title={description}>
+            <button className='tryButton bgFFF' title={description} code={code}>
                 Try {name}
             </button>
-            <ul className='plan-features'>
-                {features.map((feature, index) => <li className='black-text'>
-                    <i className="fa-solid fa-square-check black-text"></i>{feature}
-                </li>)}
-            </ul>
+            {features?.length &&
+                <ul className='plan-features'>
+                    {features.map((feature, index) => <li className='black-text'>
+                        <i className="fa-solid fa-square-check black-text"></i>{feature}
+                    </li>)}
+                </ul>
+            }
         </div>
     ) : null;
 }
