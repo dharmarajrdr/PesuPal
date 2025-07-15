f7e70c054165372802c83857bb6ab5d2c44e18e5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-16 00:24:28 +0530

    Add MoreFeaturesLayout component with associated CSS styles

---
+++ client/src/components/More/MoreFeaturesLayout.js
@@ -0,0 +1,13 @@
+import MoreFeaturesList from "./MoreFeaturesList"
+import './MoreFeaturesLayout.css';
+
+const MoreFeaturesLayout = () => {
+
+    return (
+        <div id='more-features-layout' className="w100 h100">
+            <MoreFeaturesList />
+        </div>
+    )
+}
+
+export default MoreFeaturesLayout
