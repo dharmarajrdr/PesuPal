9a7780f7bfcf0d73b7f7da52e2b6fb720a164970
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 22:13:33 +0530

    Add SubscriptionLayout component with initial CSS styling

---
+++ client/src/components/Settings/subscription/SubscriptionLayout.js
@@ -0,0 +1,12 @@
+import './SubscriptionLayout.css';
+import SubscriptionPlan from './SubscriptionPlan';
+
+const SubscriptionLayout = () => {
+    return (
+        <div id='SubscriptionLayout'>
+            <SubscriptionPlan />
+        </div>
+    )
+}
+
+export default SubscriptionLayout
