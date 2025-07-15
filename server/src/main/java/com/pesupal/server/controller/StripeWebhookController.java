728593304d0b9feaba7ac2cf14afa2a464557bf8
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 18:27:10 +0530

    Add StripeWebhookController to manage webhooks for Stripe integration

---
+++ server/src/main/java/com/pesupal/server/controller/StripeWebhookController.java
@@ -0,0 +1,33 @@
+package com.pesupal.server.controller;
+
+import com.pesupal.server.dto.response.Webhook;
+import com.pesupal.server.dto.request.WebhookDto;
+import com.pesupal.server.service.interfaces.WebhookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/webhook")
+public class StripeWebhookController {
+
+    @Autowired
+    private WebhookService webhookService;
+
+    @PostMapping("/webhook")
+    public Webhook createWebhook(@RequestBody WebhookDto webhookDto) {
+
+        return webhookService.createWebhook(webhookDto.getUrl(), webhookDto.getEvents());
+    }
+
+    @DeleteMapping("/webhook/{id}")
+    public Boolean deleteWebhook(@PathVariable String id) {
+
+        return webhookService.deleteWebhook(id);
+    }
+
+    @PatchMapping("/webhook/{id}")
+    public Webhook deleteWebhook(@RequestBody WebhookDto webhookDto, @PathVariable String id) {
+
+        return webhookService.updateWebhook(webhookDto.getUrl(), webhookDto.getEvents(), id);
+    }
+}
