2afcb98ac4b2519efdd20bb79dfd6d4f6f156cfa
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-15 20:41:14 +0530

    Update PaymentGateway and PaymentService interfaces to accept PaymentDto parameter in generatePaymentLink and initiatePayment methods

--- server/src/main/java/com/pesupal/server/service/interfaces/PaymentGateway.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/PaymentGateway.java
@@ -1,6 +1,8 @@
 package com.pesupal.server.service.interfaces;
 
+import com.pesupal.server.dto.request.PaymentDto;
+
 public interface PaymentGateway {
 
-    String generatePaymentLink() throws Exception;
+    String generatePaymentLink(PaymentDto paymentDto) throws Exception;
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/PaymentService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/PaymentService.java
@@ -1,6 +1,8 @@
 package com.pesupal.server.service.interfaces;
 
+import com.pesupal.server.dto.request.PaymentDto;
+
 public interface PaymentService {
 
-    String initiatePayment() throws Exception;
+    String initiatePayment(PaymentDto paymentDto) throws Exception;
 }
