b2eead126a5b50edcdaa9f9780bc9a87555f8914
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-16 10:00:01 +0530

    Refactor Nav component to use NavContent for rendering content

--- client/src/components/LeftNavigation/Nav.js
+++ client/src/components/LeftNavigation/Nav.js
@@ -1,24 +1,26 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom';
 import './LeftNavigation.css'
+
+const NavContent = ({ icon, image, notifyCount, title }) => {
+    console.log({ icon, image });
+    return <>
+        {
+            icon ? <i className={`fas ${icon} colorFFF`}></i> : <img src={image} />
+        }
+        {notifyCount && notifyCount != '0' && <b className='notifyCount'>{notifyCount}</b>}
+        <span className='colorFFF'>{title}</span>
+    </>
+}
 
 const Nav = ({ icon, image, title, route, notifyCount, showOrgListHandler }) => {
 
     return route ? (
         <NavLink to={route} className={({ isActive }) => (isActive ? 'activeNav' : '') + ` LeftNavigationItem cursP FCCC selectNone`}  >
-            {
-                icon ? <i className={`fas ${icon} colorFFF`}></i> : <img src={image} />
-            }
-            {notifyCount && notifyCount != '0' && <b className='notifyCount'>{notifyCount}</b>}
-            <span className='colorFFF'>{title}</span>
+            <NavContent icon={icon} image={image} notifyCount={notifyCount} title={title} />
         </NavLink>
     ) : (
         <div className='LeftNavigationItem FCCC selectNone' onClick={showOrgListHandler} >
-            {
-                icon ? <i className={`fas ${icon} colorFFF`}></i> : <img src={image} />
-            }
-            {notifyCount && notifyCount != '0' && <b className='notifyCount'>{notifyCount}</b>}
-            <span className='colorFFF'>{title}</span>
+            <NavContent icon={icon} image={image} notifyCount={notifyCount} title={title} />
         </div>
     )
 }
