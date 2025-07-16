62010edef763be9ec7dc0c981353b7f63a6db6b4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-16 22:33:27 +0530

    Refactor PeopleLayout to use Routes for navigation and remove unused components

--- client/src/App.js
+++ client/src/App.js
@@ -64,7 +64,7 @@ function App() {
                 <Routes>
                     <Route path="/feeds" element={<FeedsLayout />} />
                     <Route path="/chat" element={<ChatLayout />} />
-                    <Route path="/people" element={<PeopleLayout />} />
+                    <Route path="/people/*" element={<PeopleLayout />} />
                     <Route path="/team/*" element={<TeamLayout />} />
                     <Route path="/tracker" element={<TrackerLayout />} />
                     <Route path="/settings/*" element={<SettingsLayout />} />
--- client/src/components/People/PeopleLayout.js
+++ client/src/components/People/PeopleLayout.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import SearchPeople from './ListOfPeople/SearchPeople'
-import PeopleCards from './ListOfPeople/PeopleCards'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import AllUsers from './ListOfPeople/AllUsers'
+import UserPostsLayout from './UserPosts/UserPostsLayout'
+import PageNotFound from '../Auth/PageNotFound'
 
 const PeopleLayout = () => {
     return (
         <div className='Layout'>
-            <SearchPeople />
-            <PeopleCards />
+            <Routes>
+                <Route path='/' element={<AllUsers />} />
+                <Route path='/:id/posts' element={<UserPostsLayout />} />
+                <Route path='*' element={<PageNotFound />} />
+            </Routes>
         </div>
     )
 }
