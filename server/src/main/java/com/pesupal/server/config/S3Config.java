d8712d45316ba0c0553c6aa5c1ee8feaa69b130a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-16 19:43:45 +0530

    Add S3Config class for AWS S3 client configuration with access and secret keys

---
+++ server/src/main/java/com/pesupal/server/config/S3Config.java
@@ -0,0 +1,31 @@
+package com.pesupal.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.s3.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.s3.region}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(awsBasicCredentials);
+
+        return S3Client.builder().region(Region.of(region)).credentialsProvider(credentialsProvider).build();
+    }
+}
