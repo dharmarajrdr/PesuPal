49e3d676dcd89fad8a45da550d31c57ff0eb908c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-17 15:38:15 +0530

    Add PostOptions component with styling for user interaction

---
+++ client/src/components/Feeds/FeedsMainPanel/PostOptions.js
@@ -0,0 +1,14 @@
+import './PostOptions.css';
+
+const PostOptions = () => {
+    return (
+        <div className="FCSS" id="post-options">
+            <div className='option'>Show Post Likes</div>
+            <div className='option'>Show Post Comments</div>
+            <div className='option'>Enable Post Comments</div>
+            <div className='option'>Enable Post Bookmarks</div>
+        </div>
+    )
+}
+
+export default PostOptions
