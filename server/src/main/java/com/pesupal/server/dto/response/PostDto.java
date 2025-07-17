e53d894a2e0ab1f873afff971ac098135427c6b7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-17 22:22:05 +0530

    Change media type in PostDto from UUID to URL for improved media handling

--- server/src/main/java/com/pesupal/server/dto/response/PostDto.java
+++ server/src/main/java/com/pesupal/server/dto/response/PostDto.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pesupal.server.model.post.Post;
 import lombok.Data;
 
+import java.net.URL;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.UUID;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -24,7 +24,7 @@ public class PostDto {
 
     private List<String> tags;
 
-    private List<UUID> media;
+    private List<URL> media;
 
     private PostImpressionDto impression;
 
