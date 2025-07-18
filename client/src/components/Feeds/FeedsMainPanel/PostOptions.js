e6c5b865ed487f32e84f82bb1638306db9391c6a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-18 12:37:37 +0530

    Add delete post functionality with confirmation popup in PostOptions

--- client/src/components/Feeds/FeedsMainPanel/PostOptions.js
+++ client/src/components/Feeds/FeedsMainPanel/PostOptions.js
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import './PostOptions.css';
 import PostsLikedBy from './PostsLikedBy';
 import { apiRequest } from '../../../http_request';
+import ConfirmationPopup from '../../Utils/ConfirmationPopup';
 
 const PostOptions = ({ postId, commentable, setCommentable, isCreator }) => {
 
     const [showLikesList, setShowLikesList] = useState(false);
+    const [deletePostClicked, setDeletePostClicked] = useState(false);
 
     const toggleCommentSectionHandler = () => {
         apiRequest(`/api/v1/post/${postId}`, "PATCH", { commentable: !commentable }).then(({ data }) => {
@@ -15,13 +17,37 @@ const PostOptions = ({ postId, commentable, setCommentable, isCreator }) => {
         });
     }
 
+    const deletePopupOptions = [
+        {
+            title: "Yes",
+            color: "green",
+            onClick: () => {
+                apiRequest(`/api/v1/post/${postId}`, "DELETE").then(() => {
+                    setDeletePostClicked(false);
+                    window.location.reload();
+                }).catch(({ message }) => {
+                    console.error({ 'module': 'deletePopupOptions', message }); // eslint-disable-line no-console
+                });
+            }
+        },
+        {
+            title: "No",
+            color: "red",
+            onClick: () => setDeletePostClicked(false)
+        }
+    ];
+
     return (
         <div className="FCSS" id="post-options">
 
             {showLikesList && <PostsLikedBy postId={postId} closeShowLikesList={() => setShowLikesList(false)} />}
             <div className='option' onClick={() => setShowLikesList(true)}>Show Post Likes</div>
 
-            {isCreator && <div className='option' onClick={toggleCommentSectionHandler} >{commentable ? 'Disable' : 'Enable'} Post Comments</div>}
+            {isCreator && <>
+                <div className='option' onClick={toggleCommentSectionHandler} >{commentable ? 'Disable' : 'Enable'} Post Comments</div>
+                <div className='option' onClick={() => setDeletePostClicked(true)}>Delete Post</div>
+                {deletePostClicked && <ConfirmationPopup message={"Are you sure you want to delete this post?"} onClose={() => setDeletePostClicked(false)} options={deletePopupOptions} />}
+            </>}
         </div>
     )
 }
