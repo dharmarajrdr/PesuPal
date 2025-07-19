8214741ca3896ba82412cdfa16db1b3a8e0ececc
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-19 11:52:56 +0530

    Enhance user onboarding by initiating onboarding process and updating email verification template

--- server/src/main/java/com/pesupal/server/service/implementations/UserServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.pesupal.server.service.implementations;
 
+import com.pesupal.server.config.StaticConfig;
 import com.pesupal.server.dto.request.CreateUserDto;
 import com.pesupal.server.dto.request.EmailNotificationRequestDto;
 import com.pesupal.server.dto.response.UserLoginCheckDto;
 import com.pesupal.server.exceptions.DataNotFoundException;
 import com.pesupal.server.model.user.User;
+import com.pesupal.server.model.user.UserOnboarding;
 import com.pesupal.server.repository.UserRepository;
+import com.pesupal.server.service.interfaces.UserOnboardingService;
 import com.pesupal.server.service.interfaces.UserService;
 import com.pesupal.server.strategies.notification.EmailNotification;
 import com.pesupal.server.strategies.notification_template.SignupConfirmationTemplate;
@@ -23,6 +26,7 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final EmailNotification emailNotification;
+    private final UserOnboardingService userOnboardingService;
 
     /**
      * Gets a user by their ID.
@@ -49,9 +53,10 @@ public class UserServiceImpl implements UserService {
         User user = createUserDto.toUser();
         user.setPassword(passwordEncoder.encode(user.getPassword()));   // Encode the password before saving
         user = userRepository.save(user);
+        UserOnboarding userOnboarding = userOnboardingService.initiateOnboarding(user);
         EmailNotificationRequestDto<SignupConfirmationTemplate> emailNotificationRequestDto = new EmailNotificationRequestDto<>();
         emailNotificationRequestDto.setRecipientEmail(user.getEmail());
-        emailNotificationRequestDto.setTemplate(new SignupConfirmationTemplate("https://www.youtube.com"));
+        emailNotificationRequestDto.setTemplate(new SignupConfirmationTemplate(StaticConfig.SERVER_DOMAIN + "/api/v1/user/onboarding/email-verification/" + userOnboarding.getId()));
         emailNotification.sendNotification(emailNotificationRequestDto);
     }
 
@@ -79,4 +84,5 @@ public class UserServiceImpl implements UserService {
 
         return userRepository.findByEmail(email).orElseThrow(() -> new DataNotFoundException("User with email '" + email + "' not found"));
     }
+
 }
