6a6c31d1e0f61904d416c08a43d3b0275b8d4a64
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 10:44:09 +0530

    Add initial conversation data for chat functionality

---
+++ client/src/components/Chat/ConversationScreen/Conversation.js
@@ -0,0 +1,92 @@
+export default [
+    {
+        "id": 9,
+        "createdAt": "2025-07-19T22:44:09.85472",
+        "sender": 2,
+        "receiver": 1,
+        "message": "onnu illa",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {
+            "LIKE": 1
+        },
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 8,
+        "createdAt": "2025-07-19T22:44:03.539605",
+        "sender": 1,
+        "receiver": 2,
+        "message": "soldra",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 7,
+        "createdAt": "2025-07-19T22:43:50.242872",
+        "sender": 2,
+        "receiver": 1,
+        "message": "hello",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 6,
+        "createdAt": "2025-07-19T22:25:09.913578",
+        "sender": 2,
+        "receiver": 1,
+        "message": "hello",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 5,
+        "createdAt": "2025-07-19T22:09:11.832678",
+        "sender": 1,
+        "receiver": 2,
+        "message": "enna pandra?",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 4,
+        "createdAt": "2025-07-19T22:08:39.598451",
+        "sender": 2,
+        "receiver": 1,
+        "message": "bye",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 3,
+        "createdAt": "2025-07-19T22:08:01.819876",
+        "sender": 2,
+        "receiver": 1,
+        "message": "hello",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    },
+    {
+        "id": 1,
+        "createdAt": "2025-07-13T18:37:56.258381",
+        "sender": 1,
+        "receiver": 2,
+        "message": "Hi anna",
+        "deleted": false,
+        "readReceipt": "SENT",
+        "reactions": {},
+        "directMessageMediaFiles": []
+    }
+]
