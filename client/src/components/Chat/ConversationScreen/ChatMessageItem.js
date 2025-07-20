eec8180c0b96d7c8c9c88953922224ac9546a0c5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 21:48:53 +0530

    Refactor ChatMessages and ChatMessageItem components: streamline message rendering and enhance code organization

--- client/src/components/Chat/ConversationScreen/ChatMessageItem.js
+++ client/src/components/Chat/ConversationScreen/ChatMessageItem.js
@@ -1,35 +1,22 @@
 import MessageDeleted from './MessageDeleted';
-import ReadReceipt from './ReadReceipt';
 import Message from './Message';
+import MessageActions from './MessageActions';
+import MessageMeta from './MessageMeta';
 
 const ChatMessageItem = ({ msg, currentUserId }) => {
-    const formatTime = (iso) => new Date(iso).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
     const isCurrentUser = msg.sender == currentUserId;
 
     return (
         <div className='row w100 FRCS'>
             <div className={`message ${isCurrentUser ? 'sent' : 'received'}`}>
-                {msg.deleted ? (
-                    <MessageDeleted />
-                ) : (
-                    <>
-                        <div className="message-content">
-                            <Message html={msg.message} />
-                            <div className="actions">
-                                <i className='fa fa-trash delete-icon' title="Delete" />
-                                <div className="reactions">
-                                    {['LIKE', 'LOVE', 'FUNNY', 'ANGRY', 'DISLIKE'].map((reaction) => (
-                                        <i key={reaction} className='fa fa-smile reaction-icon' title={reaction} />
-                                    ))}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="message-meta FRCE">
-                            <span className="time">{formatTime(msg.createdAt)}</span>
-                            {isCurrentUser && <ReadReceipt readReceipt={msg.readReceipt} />}
-                        </div>
-                    </>
-                )}
+                {msg.deleted ? <MessageDeleted /> : <>
+                    <div className="message-content">
+                        <Message html={msg.message} />
+                        <MessageActions />
+                    </div>
+                    <MessageMeta createdAt={msg.createdAt} readReceipt={msg.readReceipt} isCurrentUser={isCurrentUser} />
+                </>}
             </div>
         </div>
     );
--- client/src/components/Chat/ConversationScreen/ChatMessages.js
+++ client/src/components/Chat/ConversationScreen/ChatMessages.js
@@ -1,11 +1,8 @@
 import { useEffect, useRef } from 'react';
 import './ChatMessages.css'
 import Loader from '../../Loader';
-import MessageDeleted from './MessageDeleted';
-import Message from './Message';
 import StartNewConversation from './StartNewConversation';
-import MessageActions from './MessageActions';
-import MessageMeta from './MessageMeta';
+import ChatMessageItem from './ChatMessageItem';
 
 const ChatMessages = ({ messages, currentUserId, chatId, retrievingChat }) => {
 
@@ -31,7 +28,6 @@ const ChatMessages = ({ messages, currentUserId, chatId, retrievingChat }) => {
 
             {retrievingChat ? <Loader /> : messages.length ? messages.map((msg) => {
 
-                const isCurrentUser = msg.sender == currentUserId;
                 const newDate = formatDate(msg.createdAt);
                 const showDate = newDate !== lastDate;
                 lastDate = newDate;
@@ -39,17 +35,7 @@ const ChatMessages = ({ messages, currentUserId, chatId, retrievingChat }) => {
                 return (
                     <div key={msg.id} className='w100'>
                         {showDate && <div className="date-label">{newDate}</div>}
-                        <div className='row w100 FRCS'>
-                            <div className={`message ${isCurrentUser ? 'sent' : 'received'}`}>
-                                {msg.deleted ? <MessageDeleted /> : <>
-                                    <div className="message-content">
-                                        <Message html={msg.id} />
-                                        <MessageActions />
-                                    </div>
-                                    <MessageMeta createdAt={msg.createdAt} readReceipt={msg.readReceipt} isCurrentUser={isCurrentUser} />
-                                </>}
-                            </div>
-                        </div>
+                        <ChatMessageItem currentUserId={currentUserId} msg={msg} />
                     </div>
                 );
             }) : <StartNewConversation />}
