6b70619569f9f32326d3d14fa917add869926637
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 10:43:55 +0530

    Implement ConversationScreen component with chat header, messages, and input

--- client/src/components/Chat/ConversationScreen/ConversationScreen.js
+++ client/src/components/Chat/ConversationScreen/ConversationScreen.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
 import './ConversationScreen.css';
+import ChatHeader from './ChatHeader';
+import ChatMessages from './ChatMessages';
+import ChatInput from './ChatInput';
+import Conversation from './Conversation';
 
 const ConversationScreen = () => {
+
+  const [conversationInfo, setConversationInfo] = useState({
+    'type': 'Direct Message',
+    'participants': [
+      { 'id': 2, 'name': 'MohanKumar', 'avatar': 'M' }
+    ]
+  });
+  const [messages, setMessages] = useState(Conversation);
   return (
-    <div id='ConversationScreen'></div>
+    <div id='ConversationScreen'>
+      <ChatHeader conversationInfo={conversationInfo} />
+      <ChatMessages messages={messages} />
+      <ChatInput />
+    </div>
   )
 }
 
