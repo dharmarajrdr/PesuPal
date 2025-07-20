42c3479d21e4c332b6ec1bf60caa0f9dc66693a4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 11:12:58 +0530

    Refactor UserAvatar component to include dynamic avatar color generation and adjust CSS for consistent sizing

--- client/src/components/User/UserAvatar.js
+++ client/src/components/User/UserAvatar.js
@@ -1,11 +1,32 @@
 import { useState } from "react";
 import './UserAvatar.css';
 
-const UserAvatar = ({ displayPicture }) => {
+const getAvatarColor = (name) => {
+    const colors = [
+        '#FF5733', '#33FF57', '#3357FF', '#FF33A6', '#33FFF5',
+        '#F3FF33', '#8E44AD', '#3498DB', '#E67E22', '#2ECC71',
+        '#E74C3C', '#1ABC9C', '#9B59B6', '#34495E', '#7F8C8D',
+        '#F1C40F', '#D35400', '#C0392B', '#16A085', '#2980B9'
+    ];
+
+    let hash = 0;
+    for (let i = 0; i < name.length; i++) {
+        hash = name.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    const index = Math.abs(hash) % colors.length;
+    return colors[index];
+}
+
+const UserAvatar = ({ displayPicture, displayName }) => {
 
     const [imageError, setImageError] = useState(false);
 
-    return imageError || !displayPicture ? (
+    return (imageError || !displayPicture) ? displayName ? (
+        <div className="user-avatar-placeholder-first-character mR10" style={{ backgroundColor: getAvatarColor(displayName) }}>
+            {displayName.charAt(0).toUpperCase()}
+        </div>
+    ) : (
         <i className="fa fa-user-circle user-avatar-placeholder mR10" aria-hidden="true" />
     ) : (
         <img src={displayPicture} alt="User" className="img_40_40 mR10" onError={() => setImageError(true)} />
