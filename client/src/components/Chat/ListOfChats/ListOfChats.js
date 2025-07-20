9e8a2309d196b4384ff55cc1c2c532f6b7e9a519
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 16:44:16 +0530

    Enhance ListOfChats component: pass activeRecentChat prop to RecentChats for improved chat context

--- client/src/components/Chat/ListOfChats/ListOfChats.js
+++ client/src/components/Chat/ListOfChats/ListOfChats.js
@@ -5,7 +5,7 @@ import PinnedUsers from './PinnedUsers'
 import RecentChats from './RecentChats'
 import SubTabs from './SubTabs'
 
-const ListOfChats = () => {
+const ListOfChats = ({ activeRecentChat }) => {
     return (
         <div id='ListOfChats' className='custom-scrollbar'>
             <div id='searchPinnedFixedContainer'>
@@ -13,7 +13,7 @@ const ListOfChats = () => {
                 <PinnedUsers />
                 <SubTabs />
             </div>
-            <RecentChats />
+            <RecentChats activeRecentChat={activeRecentChat} />
         </div>
     )
 }
