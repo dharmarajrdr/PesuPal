5e42b11237e4c8133156cfd032745d886d5402c1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 11:13:15 +0530

    Refactor ChatHeader component to enhance styling and integrate UserAvatar for improved user display

--- client/src/components/Chat/ConversationScreen/ChatHeader.js
+++ client/src/components/Chat/ConversationScreen/ChatHeader.js
@@ -1,15 +1,16 @@
+import UserAvatar from '../../User/UserAvatar';
 import './ChatHeader.css';
 
 const users = {
-    1: { name: 'Dharmaraj', avatar: 'D' },
-    2: { name: 'MohanKumar', avatar: 'M' },
+    1: { name: 'Dharmaraj'  },
+    2: { name: 'Rathinavel' },
 };
 
 const ChatHeader = ({ conversationInfo }) => {
     const receiver = users[conversationInfo.participants[0].id];
     return (
         <div className="chat-header">
-            <div className="avatar">{receiver.avatar}</div>
+            <UserAvatar displayPicture={receiver.avatar} displayName={receiver.name} />
             <div className="name">{receiver.name}</div>
         </div>
     );
