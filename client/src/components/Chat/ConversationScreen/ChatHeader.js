3ece80904bfb834038dff2f912d30121cb034fc4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-20 15:12:19 +0530

    Refactor ChatHeader component: update styles and add close chat functionality

--- client/src/components/Chat/ConversationScreen/ChatHeader.js
+++ client/src/components/Chat/ConversationScreen/ChatHeader.js
@@ -1,17 +1,31 @@
 import UserAvatar from '../../User/UserAvatar';
 import './ChatHeader.css';
+import { useNavigate } from 'react-router-dom';
 
 const users = {
-    1: { name: 'Dharmaraj'  },
+    1: { name: 'Dharmaraj' },
     2: { name: 'Rathinavel' },
 };
 
 const ChatHeader = ({ conversationInfo }) => {
+
+    const navigate = useNavigate();
+
     const receiver = users[conversationInfo.participants[0].id];
+
+    const closeChatHandler = () => {
+        navigate('/chat');
+    }
+
     return (
-        <div className="chat-header">
-            <UserAvatar displayPicture={receiver.avatar} displayName={receiver.name} />
-            <div className="name">{receiver.name}</div>
+        <div className="chat-header FRCB w100">
+            <div className='FRCS'>
+                <UserAvatar displayPicture={receiver.avatar} displayName={receiver.name} />
+                <div className="name">{receiver.name}</div>
+            </div>
+            <div>
+                <i className='fa fa-close' id='close-chat' onClick={closeChatHandler}></i>
+            </div>
         </div>
     );
 };
