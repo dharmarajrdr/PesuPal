1c98c18b6f8bfcafdd748608df7f703b809a6db4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-21 15:30:37 +0530

    refactor PinnedDirectMessageController to streamline imports and improve code organization

--- server/src/main/java/com/pesupal/server/controller/PinnedDirectMessageController.java
+++ server/src/main/java/com/pesupal/server/controller/PinnedDirectMessageController.java
@@ -1,14 +1,13 @@
 package com.pesupal.server.controller;
 
+import com.pesupal.server.dto.request.CreatePinDirectMessageDto;
 import com.pesupal.server.dto.request.PinnedDirectMessageDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.helpers.CurrentValueRetriever;
 import com.pesupal.server.service.interfaces.PinnedDirectMessageService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +23,12 @@ public class PinnedDirectMessageController extends CurrentValueRetriever {
 
         List<PinnedDirectMessageDto> pinnedDirectMessageDtos = pinnedDirectMessageService.getAllPinnedDirectMessages(getCurrentUserId(), getCurrentOrgId());
         return ResponseEntity.ok().body(new ApiResponseDto("Pinned direct messages retrieved successfully", pinnedDirectMessageDtos));
+    }
+
+    @PostMapping("/pin")
+    public ResponseEntity<ApiResponseDto> pinDirectMessage(@RequestBody CreatePinDirectMessageDto createPinDirectMessageDto) {
+
+        PinnedDirectMessageDto pinnedDirectMessageDto = pinnedDirectMessageService.pinDirectMessage(createPinDirectMessageDto, getCurrentUserId(), getCurrentOrgId());
+        return ResponseEntity.ok().body(new ApiResponseDto("Direct message pinned successfully", pinnedDirectMessageDto));
     }
 }
