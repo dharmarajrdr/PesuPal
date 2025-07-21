0f97414ccae12ff467b6eceeb77dfa447f1cbf75
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-21 18:57:54 +0530

    refactor ChatHeader to integrate OptionsModal and manage options display with Redux

--- client/src/components/Chat/ConversationScreen/ChatHeader.js
+++ client/src/components/Chat/ConversationScreen/ChatHeader.js
@@ -3,10 +3,15 @@ import Profile from '../../OthersProfile/Profile';
 import UserAvatar from '../../User/UserAvatar';
 import './ChatHeader.css';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setShowChatHeaderOptionsModal } from '../../../store/reducers/ShowChatHeaderOptionsModalSlice';
+import OptionsModal from '../../Utils/OptionsModal';
 
 const ChatHeader = ({ activeRecentChatState, activeChatPreview, setCurrentChatId }) => {
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const showChatHeaderOptionsModalSlice = useSelector(state => state.showChatHeaderOptionsModalSlice);
     const [showProfile, setShowProfile] = useState(false);
     const [, setActiveRecentChat] = activeRecentChatState;
     const { id: otherUserId, displayName, displayPicture } = activeChatPreview?.otherUser || {};
@@ -17,6 +22,15 @@ const ChatHeader = ({ activeRecentChatState, activeChatPreview, setCurrentChatId
         navigate('/chat');
     }
 
+    const chatHeaderOptionsClickHandler = () => {
+        dispatch(setShowChatHeaderOptionsModal(!showChatHeaderOptionsModalSlice));
+    }
+
+    const options = [
+        { name: 'Pin Conversation', icon: 'fa fa-thumbtack' },
+        { name: 'View Media', icon: 'fa fa-image' }
+    ];
+
     return (
         <div className="chat-header FRCB w100">
             {showProfile && <Profile userId={otherUserId} setShowProfile={setShowProfile} />}
@@ -24,8 +38,10 @@ const ChatHeader = ({ activeRecentChatState, activeChatPreview, setCurrentChatId
                 <UserAvatar displayPicture={displayPicture} displayName={displayName} setShowProfile={setShowProfile} />
                 <div className="name">{displayName}</div>
             </div>
-            <div>
-                <i className='fa fa-close' id='close-chat' onClick={closeChatHandler}></i>
+            <div className='FRCE'>
+                {showChatHeaderOptionsModalSlice && <OptionsModal options={options} />}
+                <i className='header-icons fa fa-ellipsis-v' id='chat-header-options' onClick={chatHeaderOptionsClickHandler} />
+                <i className='header-icons fa fa-close mL10' id='close-chat' onClick={closeChatHandler}></i>
             </div>
         </div>
     );
