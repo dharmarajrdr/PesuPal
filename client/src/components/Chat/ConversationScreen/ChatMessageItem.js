a31b90dcb16d14244da92c07055117168ed83a8c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 01:07:05 +0530

    enhance media display in ChatMessageItem; add responsive styles for images and media container

--- client/src/components/Chat/ConversationScreen/ChatMessageItem.js
+++ client/src/components/Chat/ConversationScreen/ChatMessageItem.js
@@ -4,9 +4,16 @@ import MessageActions from './MessageActions';
 import MessageMeta from './MessageMeta';
 import { useSelector } from 'react-redux';
 
+const ChatMessageImage = ({ fileUrl }) => {
+    return fileUrl ? <div className="message-media">
+        <img src={fileUrl} alt="Media" className="media-image cursP" />
+    </div> : null;
+}
+
 const ChatMessageItem = ({ msg }) => {
 
-    const { sender, deleted, createdAt, readReceipt, message } = msg;
+    const { sender, deleted, createdAt, readReceipt, message, media } = msg;
+    const { id, fileUrl } = media || {};
 
     const currentChatPreview = useSelector(state => state.currentChatPreviewSlice);
     const isCurrentUser = sender == currentChatPreview?.currentUser?.id;
@@ -15,6 +22,7 @@ const ChatMessageItem = ({ msg }) => {
         <div className='row w100 FRCS'>
             <div className={`message ${isCurrentUser ? 'sent' : 'received'}`}>
                 {deleted ? <MessageDeleted /> : <>
+                    <ChatMessageImage fileUrl={fileUrl} />
                     <div className="message-content">
                         <Message html={message} />
                         <MessageActions />
