58623679c57a3549fdaeb4b7d854be252774f126
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-21 13:55:49 +0530

    add clickSendMessageHandler prop to StartNewConversation for sending greetings

--- client/src/components/Chat/ConversationScreen/StartNewConversation.js
+++ client/src/components/Chat/ConversationScreen/StartNewConversation.js
@@ -1,15 +1,21 @@
 import UserAvatar from "../../User/UserAvatar";
 
-const StartNewConversation = ({ activeChatPreview }) => {
+const StartNewConversation = ({ activeChatPreview, clickSendMessageHandler }) => {
 
-    const { currentUser, otherUser } = activeChatPreview;
+    const { currentUser, otherUser, chatId } = activeChatPreview;
+    const { displayName: otherUserName } = otherUser || {};
+
+    const sayHelloButtonClickHandler = () => {
+        clickSendMessageHandler({ message: `Hello ${otherUserName || 'there'}! 👋` });
+    }
+
     return <div id='start-new-conversation' className='FCCE'>
         <div className='FRCC' id='users-avatars'>
             <UserAvatar displayPicture={currentUser.displayPicture} />
             <UserAvatar displayPicture={otherUser.displayPicture} />
         </div>
-        <button id='say-hello-button' className='mT15'>
-            Say hello <i className="fa fa-hand" id='wave-hands'></i>
+        <button id='say-hello-button' className='mT15' onClick={sayHelloButtonClickHandler}>
+            Say Hello <i className="fa fa-hand" id='wave-hands'></i>
         </button>
     </div>
 };
