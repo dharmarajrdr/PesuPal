6dea4593405c501752fa8362e8d9142c5926c1b0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 10:41:55 +0530

    implement createGroupMessage endpoint in GroupController

--- server/src/main/java/com/pesupal/server/controller/group/GroupController.java
+++ server/src/main/java/com/pesupal/server/controller/group/GroupController.java
@@ -1,8 +1,14 @@
 package com.pesupal.server.controller.group;
 
+import com.pesupal.server.dto.request.group.CreateGroupDto;
+import com.pesupal.server.dto.response.ApiResponseDto;
+import com.pesupal.server.dto.response.group.GroupDto;
 import com.pesupal.server.helpers.CurrentValueRetriever;
 import com.pesupal.server.service.interfaces.group.GroupService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController extends CurrentValueRetriever {
 
     private final GroupService groupService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponseDto> createGroupMessage(@RequestBody CreateGroupDto createGroupDto) {
+
+        GroupDto groupDto = groupService.createGroup(createGroupDto, getCurrentUserId(), getCurrentOrgId());
+        return ResponseEntity.ok().body(new ApiResponseDto("Group message created successfully", groupDto));
+    }
 }
