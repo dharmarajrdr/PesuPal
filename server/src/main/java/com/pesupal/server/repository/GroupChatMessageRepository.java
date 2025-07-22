6c93bc46e6e728f9adaacfa2d5aeceb82cee51e9
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-23 01:17:26 +0530

    Add GroupMessageMediaFileRepository to manage media files associated with group chat messages

--- server/src/main/java/com/pesupal/server/repository/GroupChatMessageRepository.java
+++ server/src/main/java/com/pesupal/server/repository/GroupChatMessageRepository.java
@@ -2,6 +2,8 @@ package com.pesupal.server.repository;
 
 import com.pesupal.server.model.group.Group;
 import com.pesupal.server.model.group.GroupChatMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,8 @@ import org.springframework.stereotype.Repository;
 public interface GroupChatMessageRepository extends JpaRepository<GroupChatMessage, Long> {
 
     void deleteAllByGroup(Group group);
+
+    Page<GroupChatMessage> findAllByGroupIdAndIdLessThan(Long groupId, Long pivotMessageId, Pageable pageable);
+
+    Page<GroupChatMessage> findAllByGroupId(Long groupId, Pageable pageable);
 }
