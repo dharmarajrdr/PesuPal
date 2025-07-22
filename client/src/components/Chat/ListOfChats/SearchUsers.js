5445466ab6c81c80877fad8cd4e92f992f6d15fc
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 19:11:34 +0530

    implement SearchUsers component with options modal and create group functionality

--- client/src/components/Chat/ListOfChats/SearchUsers.js
+++ client/src/components/Chat/ListOfChats/SearchUsers.js
@@ -1,7 +1,13 @@
-import React from 'react'
 import './SearchUsers.css'
+import { useState } from 'react';
+import OptionsModal from '../../Utils/OptionsModal';
+import CreateGroupModal from '../Group/CreateGroupModal';
 
 const SearchUsers = () => {
+
+    const [showOptionsModal, setShowOptionsModal] = useState(false);
+    const [showCreateGroupModal, setShowCreateGroupModal] = useState(false);
+
     const showLeftNavContainer = () => {
         const LeftNavigationOverlay = document.getElementById('LeftNavigationOverlay'),
             leftNavContainer = document.getElementById('LeftNavigation');
@@ -12,6 +18,16 @@ const SearchUsers = () => {
             clearTimeout(timer);
         }, 100);
     }
+
+    const options = [{
+        "icon": "fa fa-users",
+        "name": "Create Group",
+        "onClick": () => {
+            setShowCreateGroupModal(true);
+            setShowOptionsModal(false);
+        }
+    }];
+
     return (
         <div className='FRCB w100' id='SearchUserQuickAction'>
             <i className='fas fa-bars navbar cursP' onClick={showLeftNavContainer}></i>
@@ -19,7 +35,10 @@ const SearchUsers = () => {
                 <input type='text' placeholder='Search Users' className='w100' autoComplete='off' spellCheck='false' />
                 <i className='fas fa-search'></i>
             </div>
-            <i className='fas fa-plus quickActions cursP'></i>
+
+            {showCreateGroupModal && <CreateGroupModal setShowCreateGroupModal={setShowCreateGroupModal} />}
+            {showOptionsModal && <OptionsModal options={options} style={{ right: '-140px', top: '10px' }} />}
+            <i className='fas fa-plus quickActions cursP' onClick={() => setShowOptionsModal(!showOptionsModal)}></i>
         </div>
     )
 }
