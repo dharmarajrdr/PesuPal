8239062e9d41790c3d961d964e56b0475cbc2b7d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 10:42:40 +0530

    implement initializeGroupChatConfiguration method in GroupChatConfigurationServiceImpl for setting up default group chat permissions

--- server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatConfigurationServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatConfigurationServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pesupal.server.service.implementations.group;
 
+import com.pesupal.server.enums.Role;
+import com.pesupal.server.model.group.Group;
+import com.pesupal.server.model.group.GroupChatConfiguration;
 import com.pesupal.server.repository.GroupChatConfigurationRepository;
 import com.pesupal.server.service.interfaces.group.GroupChatConfigurationService;
 import lombok.AllArgsConstructor;
@@ -10,4 +13,39 @@ import org.springframework.stereotype.Service;
 public class GroupChatConfigurationServiceImpl implements GroupChatConfigurationService {
 
     private final GroupChatConfigurationRepository groupChatConfigurationRepository;
+
+    /**
+     * Initializes the default configuration of group chat for a given group.
+     *
+     * @param group
+     */
+    @Override
+    public void initializeGroupChatConfiguration(Group group) {
+
+        for (Role role : Role.values()) {
+            GroupChatConfiguration groupChatConfiguration = GroupChatConfiguration.builder().group(group).role(role).addMember(true).removeMember(true).changeName(true).changeDescription(true).deleteGroup(true).leaveGroup(true).changeProfilePicture(true).viewMembers(true).postMessage(true).pinMessage(true).deleteMessage(true).clearChat(true).roleUpdate(true).build();
+            switch (role) {
+                case SUPER_ADMIN: {
+                    break;  // Super Admin has all permissions by default
+                }
+                case ADMIN: {
+                    groupChatConfiguration.setDeleteGroup(false);
+                    groupChatConfiguration.setClearChat(false);
+                    groupChatConfiguration.setChangeName(false);
+                    groupChatConfiguration.setRemoveMember(false);
+                    groupChatConfiguration.setChangeDescription(false);
+                    groupChatConfiguration.setChangeProfilePicture(false);
+                }
+                case USER: {
+                    groupChatConfiguration.setAddMember(false);
+                    groupChatConfiguration.setLeaveGroup(false);
+                    groupChatConfiguration.setViewMembers(false);
+                    groupChatConfiguration.setPinMessage(false);
+                    groupChatConfiguration.setDeleteMessage(false);
+                    groupChatConfiguration.setRoleUpdate(false);
+                }
+            }
+            groupChatConfigurationRepository.save(groupChatConfiguration);
+        }
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/group/GroupChatConfigurationService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/group/GroupChatConfigurationService.java
@@ -1,4 +1,8 @@
 package com.pesupal.server.service.interfaces.group;
 
+import com.pesupal.server.model.group.Group;
+
 public interface GroupChatConfigurationService {
+
+    void initializeGroupChatConfiguration(Group group);
 }
