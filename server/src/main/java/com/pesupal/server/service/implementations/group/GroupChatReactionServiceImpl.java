a236bc84ab19ec625af596be05631ac6585272c9
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-23 01:18:36 +0530

    Implement getReactionsCountForMessage method to retrieve reaction counts for group chat messages

--- server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatReactionServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatReactionServiceImpl.java
@@ -1,13 +1,28 @@
 package com.pesupal.server.service.implementations.group;
 
+import com.pesupal.server.enums.Reaction;
+import com.pesupal.server.model.group.GroupChatMessage;
 import com.pesupal.server.repository.GroupChatReactionRepository;
 import com.pesupal.server.service.interfaces.group.GroupChatReactionService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
 public class GroupChatReactionServiceImpl implements GroupChatReactionService {
 
     private final GroupChatReactionRepository groupChatReactionRepository;
+
+    /**
+     * Retrieves the count of reactions for a specific group chat message.
+     *
+     * @param gm
+     * @return
+     */
+    @Override
+    public Map<Reaction, Integer> getReactionsCountForMessage(GroupChatMessage gm) {
+        return Map.of();
+    }
 }
