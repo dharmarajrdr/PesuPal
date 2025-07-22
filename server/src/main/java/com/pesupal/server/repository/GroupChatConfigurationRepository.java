f98eb222f3568278b9352175fbdfecd84b6ad641
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 12:43:30 +0530

    add findByGroupAndRole method in GroupChatConfigurationRepository; add findByGroupIdAndUserId method in GroupChatMemberRepository

--- server/src/main/java/com/pesupal/server/repository/GroupChatConfigurationRepository.java
+++ server/src/main/java/com/pesupal/server/repository/GroupChatConfigurationRepository.java
@@ -1,9 +1,15 @@
 package com.pesupal.server.repository;
 
+import com.pesupal.server.enums.Role;
+import com.pesupal.server.model.group.Group;
 import com.pesupal.server.model.group.GroupChatConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupChatConfigurationRepository extends JpaRepository<GroupChatConfiguration, Long> {
+
+    Optional<GroupChatConfiguration> findByGroupAndRole(Group group, Role role);
 }
--- server/src/main/java/com/pesupal/server/repository/GroupChatMemberRepository.java
+++ server/src/main/java/com/pesupal/server/repository/GroupChatMemberRepository.java
@@ -4,6 +4,10 @@ import com.pesupal.server.model.group.GroupChatMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GroupChatMemberRepository extends JpaRepository<GroupChatMember, Long> {
+
+    Optional<GroupChatMember> findByGroupIdAndUserId(Long groupId, Long userId);
 }
