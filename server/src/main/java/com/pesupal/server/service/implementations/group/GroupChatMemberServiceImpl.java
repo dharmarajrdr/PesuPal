284b8bf914ccdd74cfa455c4ac643a2e3a4aca80
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 10:42:32 +0530

    implement initializeGroupChatMember method in GroupChatMemberServiceImpl for adding new members to group chats

--- server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatMemberServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatMemberServiceImpl.java
@@ -1,5 +1,9 @@
 package com.pesupal.server.service.implementations.group;
 
+import com.pesupal.server.enums.Role;
+import com.pesupal.server.model.group.Group;
+import com.pesupal.server.model.group.GroupChatMember;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.repository.GroupChatMemberRepository;
 import com.pesupal.server.service.interfaces.group.GroupChatMemberService;
 import lombok.AllArgsConstructor;
@@ -10,4 +14,21 @@ import org.springframework.stereotype.Service;
 public class GroupChatMemberServiceImpl implements GroupChatMemberService {
 
     private final GroupChatMemberRepository groupChatMemberRepository;
+
+    /**
+     * Initializes the group chat member for a given group and organization member.
+     *
+     * @param group
+     * @param orgMember
+     */
+    @Override
+    public void initializeGroupChatMember(Group group, OrgMember orgMember) {
+
+        GroupChatMember groupChatMember = new GroupChatMember();
+        groupChatMember.setGroup(group);
+        groupChatMember.setUser(orgMember.getUser());
+        groupChatMember.setActive(true);
+        groupChatMember.setRole(Role.SUPER_ADMIN);
+        groupChatMemberRepository.save(groupChatMember);
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/group/GroupChatMemberService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/group/GroupChatMemberService.java
@@ -1,4 +1,9 @@
 package com.pesupal.server.service.interfaces.group;
 
+import com.pesupal.server.model.group.Group;
+import com.pesupal.server.model.user.OrgMember;
+
 public interface GroupChatMemberService {
+
+    void initializeGroupChatMember(Group group, OrgMember orgMember);
 }
