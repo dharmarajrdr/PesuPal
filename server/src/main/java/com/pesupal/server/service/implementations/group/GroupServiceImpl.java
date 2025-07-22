f8d448523472c94799c1b9fbd4c8a0bfa53560ff
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-22 10:42:24 +0530

    implement createGroup method in GroupService and GroupServiceImpl for group creation

--- server/src/main/java/com/pesupal/server/service/implementations/group/GroupServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/group/GroupServiceImpl.java
@@ -1,7 +1,15 @@
 package com.pesupal.server.service.implementations.group;
 
+import com.pesupal.server.dto.request.group.CreateGroupDto;
+import com.pesupal.server.dto.response.group.GroupDto;
+import com.pesupal.server.model.group.Group;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.repository.GroupRepository;
+import com.pesupal.server.service.interfaces.OrgMemberService;
+import com.pesupal.server.service.interfaces.group.GroupChatConfigurationService;
+import com.pesupal.server.service.interfaces.group.GroupChatMemberService;
 import com.pesupal.server.service.interfaces.group.GroupService;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +18,29 @@ import org.springframework.stereotype.Service;
 public class GroupServiceImpl implements GroupService {
 
     private final GroupRepository groupRepository;
+    private final OrgMemberService orgMemberService;
+    private final GroupChatMemberService groupChatMemberService;
+    private final GroupChatConfigurationService groupChatConfigurationService;
+
+    /**
+     * Creates a new group based on the provided CreateGroupDto.
+     *
+     * @param createGroupDto
+     * @param userId
+     * @param orgId
+     * @return
+     */
+    @Override
+    @Transactional
+    public GroupDto createGroup(CreateGroupDto createGroupDto, Long userId, Long orgId) {
+
+        OrgMember orgMember = orgMemberService.getOrgMemberByUserIdAndOrgId(userId, orgId);
+        Group group = createGroupDto.toGroup();
+        group.setOwner(orgMember.getUser());
+        group.setOrg(orgMember.getOrg());
+        groupRepository.save(group);
+        groupChatConfigurationService.initializeGroupChatConfiguration(group);
+        groupChatMemberService.initializeGroupChatMember(group, orgMember);
+        return GroupDto.fromGroupAndOrgMember(group, orgMember);
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/group/GroupService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/group/GroupService.java
@@ -1,4 +1,9 @@
 package com.pesupal.server.service.interfaces.group;
 
+import com.pesupal.server.dto.request.group.CreateGroupDto;
+import com.pesupal.server.dto.response.group.GroupDto;
+
 public interface GroupService {
+
+    GroupDto createGroup(CreateGroupDto createGroupDto, Long userId, Long orgId);
 }
