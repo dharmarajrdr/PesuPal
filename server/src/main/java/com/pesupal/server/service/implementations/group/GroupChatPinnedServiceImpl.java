72c01870c53c49786a4eadca9d09173c706eb7cc
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-23 12:29:58 +0530

    Refactor GroupChatPinnedService and GroupChatPinnedServiceImpl to improve code organization and maintainability by adding necessary imports and ensuring consistent method declaration.

--- server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatPinnedServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/group/GroupChatPinnedServiceImpl.java
@@ -1,13 +1,31 @@
 package com.pesupal.server.service.implementations.group;
 
+import com.pesupal.server.model.group.Group;
+import com.pesupal.server.model.group.GroupChatPinned;
+import com.pesupal.server.model.user.User;
 import com.pesupal.server.repository.GroupChatPinnedRepository;
 import com.pesupal.server.service.interfaces.group.GroupChatPinnedService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class GroupChatPinnedServiceImpl implements GroupChatPinnedService {
 
     private final GroupChatPinnedRepository groupChatPinnedRepository;
+
+    /**
+     * Retrieves a pinned group by the ID of the user who pinned it, the group ID, and the organization ID.
+     *
+     * @param userId
+     * @param group
+     * @return
+     */
+    @Override
+    public Optional<GroupChatPinned> getPinnedGroupByPinnedByAndGroup(User pinnedBy, Group group) {
+
+        return groupChatPinnedRepository.findByPinnedByAndGroup(pinnedBy, group);
+    }
 }
