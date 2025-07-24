457f18a1168d30ac9e75aa813da41ffff6838187
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-24 22:27:53 +0530

    Refactor WebSocketController to utilize ChatServiceFactory for message handling and broadcasting

--- server/src/main/java/com/pesupal/server/controller/WebSocketController.java
+++ server/src/main/java/com/pesupal/server/controller/WebSocketController.java
@@ -2,7 +2,9 @@ package com.pesupal.server.controller;
 
 import com.pesupal.server.dto.request.ChatMessageDto;
 import com.pesupal.server.dto.response.MessageDto;
+import com.pesupal.server.enums.ChatMode;
 import com.pesupal.server.exceptions.WebsocketException;
+import com.pesupal.server.factory.ChatServiceFactory;
 import com.pesupal.server.service.interfaces.ChatService;
 import lombok.AllArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -14,16 +16,29 @@ import org.springframework.stereotype.Controller;
 @AllArgsConstructor
 public class WebSocketController {
 
-    private final ChatService chatService;
+    private final ChatServiceFactory chatServiceFactory;
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.direct-message")
-    public void sendMessage(@Payload ChatMessageDto chatMessage) {
+    public void sendDirectMessageMessage(@Payload ChatMessageDto chatMessage) {
 
         try {
+            ChatService chatService = chatServiceFactory.getService(ChatMode.DIRECT_MESSAGE);
             MessageDto messageDto = chatService.save(chatMessage);
-            messagingTemplate.convertAndSend("/topic/direct-message." + chatMessage.getReceiverId(), messageDto);
-            messagingTemplate.convertAndSend("/topic/message-delivery." + chatMessage.getSenderId(), messageDto);
+            chatService.broadcastMessage(messageDto, messagingTemplate);
+        } catch (Exception e) {
+            WebsocketException error = new WebsocketException(e.getMessage());
+            messagingTemplate.convertAndSend("/queue/errors." + chatMessage.getSenderId(), error);
+        }
+    }
+
+    @MessageMapping("/chat.group-message")
+    public void sendGroupMessage(@Payload ChatMessageDto chatMessage) {
+
+        try {
+            ChatService chatService = chatServiceFactory.getService(ChatMode.GROUP_MESSAGE);
+            MessageDto messageDto = chatService.save(chatMessage);
+            chatService.broadcastMessage(messageDto, messagingTemplate);
         } catch (Exception e) {
             WebsocketException error = new WebsocketException(e.getMessage());
             messagingTemplate.convertAndSend("/queue/errors." + chatMessage.getSenderId(), error);
