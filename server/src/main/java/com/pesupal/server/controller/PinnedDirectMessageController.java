ac48170aac50fcd22d245d5b6900ab2d98ed7da7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-25 22:08:35 +0530

    Refactor PinnedDirectMessageController to use PinnedChatDto and update GroupChatPinned model to include orderIndex field

--- server/src/main/java/com/pesupal/server/controller/PinnedDirectMessageController.java
+++ server/src/main/java/com/pesupal/server/controller/PinnedDirectMessageController.java
@@ -1,7 +1,7 @@
 package com.pesupal.server.controller;
 
 import com.pesupal.server.dto.request.CreatePinDirectMessageDto;
-import com.pesupal.server.dto.request.PinnedDirectMessageDto;
+import com.pesupal.server.dto.request.PinnedChatDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.helpers.CurrentValueRetriever;
 import com.pesupal.server.service.interfaces.PinnedDirectMessageService;
@@ -21,14 +21,14 @@ public class PinnedDirectMessageController extends CurrentValueRetriever {
     @GetMapping("")
     public ResponseEntity<ApiResponseDto> getAllPinnedDirectMessages() {
 
-        List<PinnedDirectMessageDto> pinnedDirectMessageDtos = pinnedDirectMessageService.getAllPinnedDirectMessages(getCurrentUserId(), getCurrentOrgId());
+        List<PinnedChatDto> pinnedDirectMessageDtos = pinnedDirectMessageService.getAllPinnedDirectMessages(getCurrentUserId(), getCurrentOrgId());
         return ResponseEntity.ok().body(new ApiResponseDto("Pinned direct messages retrieved successfully", pinnedDirectMessageDtos));
     }
 
     @PostMapping("/pin")
     public ResponseEntity<ApiResponseDto> pinDirectMessage(@RequestBody CreatePinDirectMessageDto createPinDirectMessageDto) {
 
-        PinnedDirectMessageDto pinnedDirectMessageDto = pinnedDirectMessageService.pinDirectMessage(createPinDirectMessageDto, getCurrentUserId(), getCurrentOrgId());
+        PinnedChatDto pinnedDirectMessageDto = pinnedDirectMessageService.pinDirectMessage(createPinDirectMessageDto, getCurrentUserId(), getCurrentOrgId());
         return ResponseEntity.ok().body(new ApiResponseDto("Direct message pinned successfully", pinnedDirectMessageDto));
     }
 
