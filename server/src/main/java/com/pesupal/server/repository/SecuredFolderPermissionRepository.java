5821851653e61125bb8c73ef9c22108f38a51455
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-26 23:39:31 +0530

    Refactor SecuredFileReadAccess and SecuredFolderPermission classes to replace User with OrgMember for access management, enhancing clarity in permission handling.

--- server/src/main/java/com/pesupal/server/repository/SecuredFolderPermissionRepository.java
+++ server/src/main/java/com/pesupal/server/repository/SecuredFolderPermissionRepository.java
@@ -1,6 +1,6 @@
 package com.pesupal.server.repository;
 
-import com.pesupal.server.model.user.User;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.model.workdrive.Folder;
 import com.pesupal.server.model.workdrive.SecuredFolderPermission;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +11,5 @@ import java.util.Optional;
 @Repository
 public interface SecuredFolderPermissionRepository extends JpaRepository<SecuredFolderPermission, Long> {
 
-    Optional<SecuredFolderPermission> findByFolderAndUser(Folder folder, User user);
+    Optional<SecuredFolderPermission> findByFolderAndAccessGrantedTo(Folder folder, OrgMember accessGrantedTo);
 }
