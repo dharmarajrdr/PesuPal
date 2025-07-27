6703e09e08c9b41303b2c8ad19f03d08f773e2a7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-27 14:22:43 +0530

    Refactor AddGroupMemberDto to change groupId type from Long to String; update GroupChatPinnedRepository to use OrgMember instead of User; modify GroupRepository to return RecentGroupChatProjection and add findByPublicId method

--- server/src/main/java/com/pesupal/server/repository/GroupChatPinnedRepository.java
+++ server/src/main/java/com/pesupal/server/repository/GroupChatPinnedRepository.java
@@ -3,7 +3,6 @@ package com.pesupal.server.repository;
 import com.pesupal.server.model.group.Group;
 import com.pesupal.server.model.group.GroupChatPinned;
 import com.pesupal.server.model.user.OrgMember;
-import com.pesupal.server.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,7 +12,7 @@ import java.util.Optional;
 @Repository
 public interface GroupChatPinnedRepository extends JpaRepository<GroupChatPinned, Long> {
 
-    Optional<GroupChatPinned> findByPinnedByAndGroup(User pinnedBy, Group group);
+    Optional<GroupChatPinned> findByPinnedByAndGroup(OrgMember pinnedBy, Group group);
 
     List<GroupChatPinned> findAllByPinnedByIdAndGroup_Org_IdOrderByOrderIndexAsc(Long userId, Long orgId);
 
