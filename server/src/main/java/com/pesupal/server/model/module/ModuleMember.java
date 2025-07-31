f5d6fb8d7db6a0346dca1f8ed4998fd9538b940b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-31 19:05:06 +0530

    Add ModuleMember entity with relationships to Module and OrgMember

---
+++ server/src/main/java/com/pesupal/server/model/module/ModuleMember.java
@@ -0,0 +1,24 @@
+package com.pesupal.server.model.module;
+
+import com.pesupal.server.model.CreationTimeAuditable;
+import com.pesupal.server.model.user.OrgMember;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class ModuleMember extends CreationTimeAuditable {
+
+    @ManyToOne
+    private Module module;
+
+    @ManyToOne
+    private OrgMember orgMember;
+
+    @Enumerated
+    private ModuleRole role;
+
+    private boolean active;
+}
