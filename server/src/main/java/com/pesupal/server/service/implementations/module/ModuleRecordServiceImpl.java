bc5370dc8018f2288b9e193fa7953c8ca7c47ed0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-31 23:25:22 +0530

    Implement createRecord method in ModuleRecordServiceImpl with permission checks

--- server/src/main/java/com/pesupal/server/service/implementations/module/ModuleRecordServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/module/ModuleRecordServiceImpl.java
@@ -1,13 +1,53 @@
 package com.pesupal.server.service.implementations.module;
 
+import com.pesupal.server.dto.request.module.CreateModuleRecordDto;
+import com.pesupal.server.dto.response.module.ModuleRecordDto;
+import com.pesupal.server.exceptions.PermissionDeniedException;
+import com.pesupal.server.helpers.CurrentValueRetriever;
+import com.pesupal.server.model.module.Module;
+import com.pesupal.server.model.module.ModuleMember;
+import com.pesupal.server.model.module.ModulePermission;
+import com.pesupal.server.model.module.ModuleRole;
+import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.repository.ModuleRecordRepository;
+import com.pesupal.server.service.interfaces.module.ModuleMemberService;
+import com.pesupal.server.service.interfaces.module.ModulePermissionService;
 import com.pesupal.server.service.interfaces.module.ModuleRecordService;
+import com.pesupal.server.service.interfaces.module.ModuleService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class ModuleRecordServiceImpl implements ModuleRecordService {
+public class ModuleRecordServiceImpl extends CurrentValueRetriever implements ModuleRecordService {
 
     private final ModuleRecordRepository moduleRecordRepository;
+    private final ModuleService moduleService;
+    private final ModulePermissionService modulePermissionService;
+    private final ModuleMemberService moduleMemberService;
+
+    /**
+     * Creates a new record in a module.
+     *
+     * @param createModuleRecordDto
+     * @return
+     */
+    @Override
+    public ModuleRecordDto createRecord(CreateModuleRecordDto createModuleRecordDto) {
+
+        OrgMember orgMember = getCurrentOrgMember();
+
+        String moduleId = createModuleRecordDto.getModuleId();
+        Module module = moduleService.getModuleById(moduleId);
+
+        ModuleMember moduleMember = moduleMemberService.getModuleMemberByOrgMemberAndModule(orgMember, module);
+        ModuleRole moduleRole = moduleMember.getRole();
+
+        ModulePermission modulePermission = modulePermissionService.getModulePermissionByModuleAndRole(module, moduleRole);
+        if (!modulePermission.isCreateRecord()) {
+            throw new PermissionDeniedException("You do not have permission to create a record in this module.");
+        }
+        
+        return null;
+    }
 }
--- server/src/main/java/com/pesupal/server/service/interfaces/module/ModuleRecordService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/module/ModuleRecordService.java
@@ -1,4 +1,9 @@
 package com.pesupal.server.service.interfaces.module;
 
+import com.pesupal.server.dto.request.module.CreateModuleRecordDto;
+import com.pesupal.server.dto.response.module.ModuleRecordDto;
+
 public interface ModuleRecordService {
+
+    ModuleRecordDto createRecord(CreateModuleRecordDto createModuleRecordDto);
 }
