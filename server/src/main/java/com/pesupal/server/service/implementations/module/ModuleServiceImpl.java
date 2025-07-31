1afbaedc2af8ffc29bcb7c9dfa9d9edd1956290c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-31 20:26:47 +0530

    Add ModuleController, ModuleService, and ModuleRepository for module management

---
+++ server/src/main/java/com/pesupal/server/service/implementations/module/ModuleServiceImpl.java
@@ -0,0 +1,33 @@
+package com.pesupal.server.service.implementations.module;
+
+import com.pesupal.server.dto.request.module.CreateModuleDto;
+import com.pesupal.server.helpers.CurrentValueRetriever;
+import com.pesupal.server.model.module.Module;
+import com.pesupal.server.model.user.OrgMember;
+import com.pesupal.server.repository.ModuleRepository;
+import com.pesupal.server.service.interfaces.module.ModuleService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ModuleServiceImpl extends CurrentValueRetriever implements ModuleService {
+
+    private final ModuleRepository moduleRepository;
+
+    /**
+     * Creates a new module.
+     *
+     * @param createModuleDto
+     * @return
+     */
+    @Override
+    public Module createModule(CreateModuleDto createModuleDto) {
+
+        OrgMember orgMember = getCurrentOrgMember();
+        Module module = createModuleDto.toModule();
+        module.setCreatedBy(orgMember);
+        module.setActive(false);
+        return moduleRepository.save(module);
+    }
+}
