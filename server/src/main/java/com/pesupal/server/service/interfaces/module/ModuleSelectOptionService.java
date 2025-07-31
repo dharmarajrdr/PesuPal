02c80b6895906bde58704d9d63547fe0ca51363c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-31 22:27:52 +0530

    Implement ModuleSelectOptionService with CRUD operations for module select options

---
+++ server/src/main/java/com/pesupal/server/service/interfaces/module/ModuleSelectOptionService.java
@@ -0,0 +1,15 @@
+package com.pesupal.server.service.interfaces.module;
+
+import com.pesupal.server.model.module.ModuleField;
+import com.pesupal.server.model.module.ModuleSelectOption;
+
+import java.util.List;
+
+public interface ModuleSelectOptionService {
+
+    ModuleSelectOption save(ModuleSelectOption moduleSelectOption);
+
+    void saveAll(List<ModuleSelectOption> selectOptions);
+
+    void deleteAllByModuleField(ModuleField moduleField);
+}
