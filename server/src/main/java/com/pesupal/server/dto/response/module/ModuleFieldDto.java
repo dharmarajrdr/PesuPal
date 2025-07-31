8bce60528e70e61c9ff478d6ac25272e46922c3f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-07-31 22:27:17 +0530

    Add ModuleFieldDto for representing module field data with conversion methods

---
+++ server/src/main/java/com/pesupal/server/dto/response/module/ModuleFieldDto.java
@@ -0,0 +1,54 @@
+package com.pesupal.server.dto.response.module;
+
+import com.pesupal.server.enums.FieldType;
+import com.pesupal.server.model.module.ModuleField;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ModuleFieldDto {
+
+    private String fieldName;
+
+    private FieldType fieldType;
+
+    private boolean required;
+
+    private boolean searchable;
+
+    private boolean filterable;
+
+    private boolean sortable;
+
+    private boolean editable;
+
+    private boolean showInList;
+
+    private boolean showInDetail;
+
+    private List<ModuleSelectOptionDto> options;
+
+    public static ModuleFieldDto fromModuleField(ModuleField moduleField) {
+
+        ModuleFieldDto dto = new ModuleFieldDto();
+        dto.setFieldName(moduleField.getName());
+        dto.setFieldType(moduleField.getFieldType());
+        dto.setRequired(moduleField.isRequired());
+        dto.setSearchable(moduleField.isSearchable());
+        dto.setFilterable(moduleField.isFilterable());
+        dto.setSortable(moduleField.isSortable());
+        dto.setEditable(moduleField.isEditable());
+        dto.setShowInList(moduleField.isShowInList());
+        dto.setShowInDetail(moduleField.isShowInDetail());
+        return dto;
+    }
+
+    public static ModuleFieldDto fromModuleFieldWithOptions(ModuleField moduleField, List<ModuleSelectOptionDto> options) {
+
+        ModuleFieldDto dto = fromModuleField(moduleField);
+        dto.setOptions(options);
+        return dto;
+    }
+
+}
