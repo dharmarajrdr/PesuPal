8b1d8bad9da455ad3c81febd561e4d52fd3ab5e2
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-01 01:09:26 +0530

    Add subject validation, duplicate check, and return ModuleRecordDto after record creation

--- server/src/main/java/com/pesupal/server/dto/response/module/ModuleRecordDto.java
+++ server/src/main/java/com/pesupal/server/dto/response/module/ModuleRecordDto.java
@@ -1,7 +1,12 @@
 package com.pesupal.server.dto.response.module;
 
+import com.pesupal.server.model.module.ModuleRecord;
 import lombok.Data;
 
 @Data
 public class ModuleRecordDto {
+
+    public static ModuleRecordDto fromModuleRecord(ModuleRecord moduleRecord) {
+
+    }
 }
