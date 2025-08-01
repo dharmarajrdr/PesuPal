6088c4147eaba0b74220d5e573f5c86c9f21dade
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-01 12:35:16 +0530

    Implement RecordStringRelation repository and service: Add methods for saving and retrieving module record relations

---
+++ server/src/main/java/com/pesupal/server/repository/RecordStringRelationRepository.java
@@ -0,0 +1,16 @@
+package com.pesupal.server.repository;
+
+import com.pesupal.server.dto.response.module.ModuleFieldDto;
+import com.pesupal.server.model.module.ModuleField;
+import com.pesupal.server.model.module.ModuleRecord;
+import com.pesupal.server.model.module.relation.RecordStringRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RecordStringRelationRepository extends JpaRepository<RecordStringRelation, Long> {
+
+    Optional<ModuleFieldDto> findByRecordAndField(ModuleRecord record, ModuleField field);
+}
---
+++ server/src/main/java/com/pesupal/server/service/implementations/module/relation/RecordStringRelationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.pesupal.server.service.implementations.module.relation;
+
+import com.pesupal.server.dto.response.module.ModuleFieldDto;
+import com.pesupal.server.exceptions.DataNotFoundException;
+import com.pesupal.server.model.module.ModuleField;
+import com.pesupal.server.model.module.ModuleRecord;
+import com.pesupal.server.model.module.relation.RecordStringRelation;
+import com.pesupal.server.repository.RecordStringRelationRepository;
+import com.pesupal.server.service.interfaces.module.relation.RecordStringRelationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RecordStringRelationServiceImpl implements RecordStringRelationService {
+
+    private final RecordStringRelationRepository recordStringRelationRepository;
+
+    /**
+     * Saves the relation data for a module record and field.
+     *
+     * @param record
+     * @param field
+     * @param data
+     */
+    @Override
+    public void save(ModuleRecord record, ModuleField field, Object data) {
+
+        String value = (String) data;
+
+        RecordStringRelation recordStringRelation = new RecordStringRelation();
+        recordStringRelation.setRecord(record);
+        recordStringRelation.setField(field);
+        recordStringRelation.setValue(value);
+        recordStringRelationRepository.save(recordStringRelation);
+    }
+
+    /**
+     * Retrieves the relation data for a module record and field.
+     *
+     * @param moduleRecord
+     * @param moduleField
+     * @return
+     */
+    @Override
+    public ModuleFieldDto getByModuleRecordAndModuleField(ModuleRecord moduleRecord, ModuleField moduleField) {
+
+        return recordStringRelationRepository.findByRecordAndField(moduleRecord, moduleField).orElseThrow(() -> new DataNotFoundException("No data found."));
+    }
+}
