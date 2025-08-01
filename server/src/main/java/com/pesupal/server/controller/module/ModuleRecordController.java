ac8b0ce93a5eb31f33a7e92531ca786c82abd0f2
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-01 12:06:32 +0530

    Add SortColumnDto for sorting functionality in record retrieval

--- server/src/main/java/com/pesupal/server/controller/module/ModuleRecordController.java
+++ server/src/main/java/com/pesupal/server/controller/module/ModuleRecordController.java
@@ -1,10 +1,12 @@
 package com.pesupal.server.controller.module;
 
+import com.pesupal.server.dto.request.SortColumnDto;
 import com.pesupal.server.dto.request.module.CreateModuleRecordDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.dto.response.module.ModuleRecordDto;
 import com.pesupal.server.service.interfaces.module.ModuleRecordService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -33,10 +35,14 @@ public class ModuleRecordController {
     }
 
     @GetMapping("/records")
-    public ResponseEntity<ApiResponseDto> getAllRecords(@RequestParam Integer page, @RequestParam Integer size) {
+    public ResponseEntity<ApiResponseDto> getAllRecords(
+            @RequestParam Integer page,
+            @RequestParam Integer size,
+            @RequestParam(required = false) SortColumnDto sortColumnDto,
+            Sort sort) {
 
         // List view of records
-        List<ModuleRecordDto> records = moduleRecordService.getAllRecords(page, size);
+        List<ModuleRecordDto> records = moduleRecordService.getAllRecords(page, size, sortColumnDto);
         return ResponseEntity.ok().body(new ApiResponseDto("Records retrieved successfully", records));
     }
 }
