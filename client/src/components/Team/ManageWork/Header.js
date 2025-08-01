a14a415e72f6eabe63199403bfd7dd6e6cdb0a74
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-01 20:40:09 +0530

    Refactor Header component to encapsulate button creation in CreateButtons function and update styles for consistency

--- client/src/components/Team/ManageWork/Header.js
+++ client/src/components/Team/ManageWork/Header.js
@@ -6,15 +6,28 @@ import FilterComponent from './FilterComponent';
 import FiltersList from './FiltersList'
 import SearchComponent from './SearchComponent';
 
+const CreateButtons = () => {
+
+    return (
+        <div className='FRCC' id='createButtons'>
+            <div className='FRCC mR10' id='createModule'>
+                <i className='fa fa-plus pR5 w_20'></i>
+                <span>Create Module</span>
+            </div>
+            <div className='FRCC' id='createRecord'>
+                <i className='fa fa-plus pR5 w_20'></i>
+                <span>Create Record</span>
+            </div>
+        </div>
+    )
+}
+
 const Header = () => {
     return (
         <div id='task_header' className='FCSS w100'>
             <div className='FRCB w100 mB10'>
                 <CustomModules CustomModulesList={CustomModulesList} />
-                <div className='FRCC' id='createRecord'>
-                    <i className='fa fa-plus pR5 w_20'></i>
-                    <span>Create Record</span>
-                </div>
+                <CreateButtons />
             </div>
             <div className='mT5 mB10 FRCB w100'>
                 <FilterComponent FiltersList={FiltersList} />
