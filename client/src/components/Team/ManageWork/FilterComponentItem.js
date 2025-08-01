74642eab0a752c6cbca92341631a19dea31ebf5a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-01 22:12:06 +0530

    Refactor Header and ModulesList: update to use moduleId for navigation and enhance module retrieval with API call

--- client/src/components/Team/ManageWork/FilterComponentItem.js
+++ client/src/components/Team/ManageWork/FilterComponentItem.js
@@ -2,13 +2,13 @@ import './FilterComponent.css';
 
 const FilterComponentItem = ({ item, onChange, selectedValue }) => {
 
-    const { id, title, icon, dom, options } = item;
+    const { icon, options } = item || {};
 
     return (
         <div className='FilterComponentItem p5 FRCC'>
             <i className={icon + ' img_40_40 alignCenter'}></i>
             <select onChange={onChange} value={selectedValue}>
-                {options.map((option, index) => <option key={index} value={option.id}>{option.title}</option>)}
+                {options?.map(({ id, name }, index) => <option key={index} value={id}>{name}</option>)}
             </select>
         </div>
     )
--- client/src/components/Team/ManageWork/Header.js
+++ client/src/components/Team/ManageWork/Header.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react'
 import './Header.css'
 import FilterComponentItem from './FilterComponentItem';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { apiRequest } from '../../../http_request';
+import { showPopup } from '../../../store/reducers/PopupSlice';
 
 const CreateButtons = () => {
 
@@ -21,41 +23,48 @@ const CreateButtons = () => {
 }
 
 const FilterIcon = () => {
+
     const [filterApplied, setFilterApplied] = useState(false);
+
     return (
-        <div className='FRCC mL10 mR10' id='filterIcon'>
+        <div className='FRCC mR10' id='filterIcon'>
             <i className='fa fa-filter'></i>
             {filterApplied && <i className='fa fa-circle' style={{ fontSize: '8px', position: 'absolute', top: '-2px', right: '-2px', color: 'red' }}></i>}
         </div>
     )
 }
 
-const ModulesList = () => {
-    const [modules, setModules] = useState([
-        { id: 1, title: "Bugs" },
-        { id: 2, title: "Tasks" },
-        { id: 3, title: "Projects" },
-        { id: 4, title: "Issues" }
-    ]);
-    return (
-        <FilterComponentItem item={{ id: 1, title: 'Module 1', icon: 'fa fa-chart-bar', dom: 'module1', options: modules }} />
-    )
+const ModulesList = ({ modules }) => {
+
+    const navigate = useNavigate();
+    const params = useParams();
+    const [moduleId, view] = params['*'].split('/') || [];
+
+    const onChange = (e) => {
+        const moduleId = e.target.value;
+        const route = `/team/manage_work/${moduleId}/${view || 'list'}`;
+        navigate(route);
+    }
+
+    return <span className='mR10'>
+        <FilterComponentItem item={{ id: 1, title: 'Module 1', icon: 'fa fa-chart-bar', options: modules }} onChange={onChange} selectedValue={moduleId} />
+    </span>
 }
 
 const ViewsList = () => {
 
     const navigate = useNavigate();
 
-    const [views, setViews] = useState([
-        { id: "list", title: 'List View' },
-        { id: "kanban", title: 'Kanban View' }
-    ]);
+    const views = [
+        { id: "list", name: 'List View' },
+        { id: "kanban", name: 'Kanban View' }
+    ];
 
-    const currentView = useSelector((state) => state.currentModuleView);
+    const { view: currentView, moduleId: currentModuleId } = useSelector((state) => state.currentModule);
 
     const onChange = (e) => {
         const view = e.target.value;
-        const route = "/team/manage_work/" + view;
+        const route = "/team/manage_work/" + currentModuleId + "/" + view;
         navigate(route);
     }
 
@@ -65,12 +74,32 @@ const ViewsList = () => {
 }
 
 const Header = () => {
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const params = useParams();
+    const [modules, setModules] = useState([]);
+
+    const [moduleId, view] = params['*'].split('/') || [];
+
+    useEffect(() => {
+        apiRequest("/api/v1/module/all", "GET").then(({ data }) => {
+            setModules(data);
+            if (data.length > 0 && !moduleId?.length) {
+                const { id } = data[0] || {};
+                navigate(`/team/manage_work/${id}/${view || 'list'}`);
+            }
+        }).catch(({ message }) => {
+            dispatch(showPopup({ message, type: 'error' }));
+        });
+    }, []);
+
     return (
         <div id='task_header' className='FCSS w100'>
             <div className='FRCB w100 mB10'>
                 {/* <CustomModules CustomModulesList={CustomModulesList} /> */}
                 <div className='FRCS' id='modulesList-filter-view'>
-                    <ModulesList />
+                    <ModulesList modules={modules} />
                     <FilterIcon />
                     <ViewsList />
                 </div>
