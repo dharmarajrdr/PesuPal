45923b9565a5c0b423d940f7e02a3afaf0661db3
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-02 12:25:53 +0530

    Refactor ManageWorkBody: extract DataView component and streamline routing logic

--- client/src/components/Team/ManageWork/ManageWorkBody.js
+++ client/src/components/Team/ManageWork/ManageWorkBody.js
@@ -1,12 +1,12 @@
 import './ManageWorkBody.css'
-import KanbanView from './Views/KanbanView'
 import ManageWorkListKanban from './ManageWorkListKanban';
-import ListView from './Views/ListView';
 import ManageWorkList from './ManageWorkList';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { closeFilterBox, showFilterBox } from '../../../store/reducers/ModuleFilterSlice';
+import PageNotFound from '../../Auth/PageNotFound';
+import DataView from './DataView';
 
 const ManageWorkBody = () => {
 
@@ -23,20 +23,10 @@ const ManageWorkBody = () => {
 
     return (
         <div id='ManageWorkBody' className='w100 custom-scrollbar FRSE'>
-            <div id='manage-work-slider' className={`FRSE ${filterBoxShowing ? 'filter-enabled' : ''}`}>
-                <div id='filter-container' className='FRCC'>
-
-                </div>
-                <div id='views-render-frame'>
-                    <Routes>
-                        <Route path='/:moduleId/list' element={<ListView ManageWorkList={ManageWorkList} />} />
-                        <Route path='/:moduleId/kanban' element={<KanbanView ManageWorkListKanban={ManageWorkListKanban} />} />
-                        <Route path='/:moduleId/*' element={<Navigate to="/manage/module" />} />
-                        {/* Wildcard route to catch all unmatched routes */}
-                        {/* <Route path="*" element={<Navigate to="/manage/module/list" />} /> */}
-                    </Routes>
-                </div>
-            </div>
+            <Routes>
+                <Route path='/:moduleId/create' element={<PageNotFound />} />
+                <Route path='/:moduleId/*' element={<DataView filterBoxShowing={filterBoxShowing} ManageWorkList={ManageWorkList} ManageWorkListKanban={ManageWorkListKanban} />} />
+            </Routes>
         </div>
     )
 }
