64ac4710a43c756f242ca4f164c57a4fc393718d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-02 12:01:05 +0530

    Refactor ModuleFilterSlice and ManageWorkBody: update filter box visibility logic and improve transition duration

--- client/src/components/Team/ManageWork/ManageWorkBody.js
+++ client/src/components/Team/ManageWork/ManageWorkBody.js
@@ -4,11 +4,22 @@ import ManageWorkListKanban from './ManageWorkListKanban';
 import ListView from './Views/ListView';
 import ManageWorkList from './ManageWorkList';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { closeFilterBox, showFilterBox } from '../../../store/reducers/ModuleFilterSlice';
 
 const ManageWorkBody = () => {
 
     const { filterBoxShowing } = useSelector((state) => state.moduleFilter);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (filterBoxShowing) {
+            dispatch(showFilterBox());
+        } else {
+            dispatch(closeFilterBox());
+        }
+    }, []);
 
     return (
         <div id='ManageWorkBody' className='w100 custom-scrollbar FRSE'>
--- client/src/store/reducers/ModuleFilterSlice.js
+++ client/src/store/reducers/ModuleFilterSlice.js
@@ -3,17 +3,23 @@ import { createSlice } from "@reduxjs/toolkit";
 const ModuleFilterSilce = createSlice({
     'name': 'moduleFilter',
     'initialState': {
-        'filterBoxShowing': false
+        'filterBoxShowing': sessionStorage.getItem('filterBoxShowing') === 'true'
     },
     'reducers': {
         'toggleFilterBox': (state, action) => {
             state.filterBoxShowing = !state.filterBoxShowing;
+            sessionStorage.setItem('filterBoxShowing', state.filterBoxShowing);
+        },
+        'showFilterBox': (state, action) => {
+            state.filterBoxShowing = true;
+            sessionStorage.setItem('filterBoxShowing', true);
         },
         'closeFilterBox': (state, action) => {
             state.filterBoxShowing = false;
+            sessionStorage.setItem('filterBoxShowing', false);
         }
     }
 });
 
-export const { toggleFilterBox, closeFilterBox } = ModuleFilterSilce.actions;
+export const { toggleFilterBox, showFilterBox, closeFilterBox } = ModuleFilterSilce.actions;
 export default ModuleFilterSilce.reducer;
