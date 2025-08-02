51205dbd67dfe061664006c58ba85010bbe9c673
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-02 15:35:26 +0530

    Add findAllByCreatedBy_PublicIdOrderByCreatedAtDesc method to ModuleRepository for retrieving modules created by a specific user

--- server/src/main/java/com/pesupal/server/repository/ModuleRepository.java
+++ server/src/main/java/com/pesupal/server/repository/ModuleRepository.java
@@ -4,10 +4,13 @@ import com.pesupal.server.model.module.Module;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ModuleRepository extends JpaRepository<Module, Long> {
 
     Optional<Module> findByPublicId(String moduleId);
+
+    List<Module> findAllByCreatedBy_PublicIdOrderByCreatedAtDesc(String publicId);
 }
