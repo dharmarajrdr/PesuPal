a939ae441a00ebf7d2e751f19e687f4947a4d5c2
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-03 18:22:22 +0530

    Add method to find all RecordTransitionRelation by Transition with pagination support

--- server/src/main/java/com/pesupal/server/repository/RecordTransitionRelationRepository.java
+++ server/src/main/java/com/pesupal/server/repository/RecordTransitionRelationRepository.java
@@ -1,10 +1,12 @@
 package com.pesupal.server.repository;
 
-import com.pesupal.server.enums.FieldType;
 import com.pesupal.server.model.module.Module;
 import com.pesupal.server.model.module.ModuleField;
 import com.pesupal.server.model.module.ModuleRecord;
+import com.pesupal.server.model.module.Transition;
 import com.pesupal.server.model.module.relation.RecordTransitionRelation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +21,5 @@ public interface RecordTransitionRelationRepository extends JpaRepository<Record
 
     void deleteAllByRecordAndField(ModuleRecord record, ModuleField field);
 
-    Optional<RecordTransitionRelation> findByField_ModuleAndField_FieldType(Module module, FieldType fieldType);
+    Page<RecordTransitionRelation> findAllByTransition(Transition transition, Pageable pageable);
 }
