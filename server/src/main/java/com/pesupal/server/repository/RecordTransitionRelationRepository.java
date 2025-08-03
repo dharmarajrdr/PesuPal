4beab5ba117587748141647ff00fe849826a19ea
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-03 16:58:28 +0530

    Refactor RecordTransitionRelationServiceImpl to extend RecordRelationServiceImpl and implement beforeFieldCreation method for transition field validation

--- server/src/main/java/com/pesupal/server/repository/RecordTransitionRelationRepository.java
+++ server/src/main/java/com/pesupal/server/repository/RecordTransitionRelationRepository.java
@@ -1,5 +1,6 @@
 package com.pesupal.server.repository;
 
+import com.pesupal.server.enums.FieldType;
 import com.pesupal.server.model.module.Module;
 import com.pesupal.server.model.module.ModuleField;
 import com.pesupal.server.model.module.ModuleRecord;
@@ -17,4 +18,6 @@ public interface RecordTransitionRelationRepository extends JpaRepository<Record
     Optional<RecordTransitionRelation> findByRecordAndField(ModuleRecord record, ModuleField field);
 
     void deleteAllByRecordAndField(ModuleRecord record, ModuleField field);
+
+    Optional<RecordTransitionRelation> findByField_ModuleAndField_FieldType(Module module, FieldType fieldType);
 }
