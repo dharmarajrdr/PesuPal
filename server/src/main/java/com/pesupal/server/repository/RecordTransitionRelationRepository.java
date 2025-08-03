3347ea937551fb1e91d00a8f12caacdc865f69d1
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-03 16:21:18 +0530

    Add RecordTransitionRelationRepository and RecordTransitionRelationService interfaces

---
+++ server/src/main/java/com/pesupal/server/repository/RecordTransitionRelationRepository.java
@@ -0,0 +1,20 @@
+package com.pesupal.server.repository;
+
+import com.pesupal.server.model.module.Module;
+import com.pesupal.server.model.module.ModuleField;
+import com.pesupal.server.model.module.ModuleRecord;
+import com.pesupal.server.model.module.relation.RecordTransitionRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RecordTransitionRelationRepository extends JpaRepository<RecordTransitionRelation, Long> {
+
+    void deleteAllByRecord_Module(Module module);
+
+    Optional<RecordTransitionRelation> findByRecordAndField(ModuleRecord record, ModuleField field);
+
+    void deleteAllByRecordAndField(ModuleRecord record, ModuleField field);
+}
