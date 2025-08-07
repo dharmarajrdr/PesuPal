b04b91a296f9fa39b9a57248046121a55d8cc72f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-07 10:46:32 +0530

    Pass groupId prop to ChatFooter for proper group message handling

--- client/src/components/Chat/ConversationScreen/ChatFooter.js
+++ client/src/components/Chat/ConversationScreen/ChatFooter.js
@@ -2,12 +2,12 @@ import ChatInputGroupArchived from './ChatInputGroupArchived'
 import ChatInputUserArchived from './ChatInputUserArchived'
 import ChatInput from './ChatInput'
 
-const ChatFooter = ({ active, groupActive, currentTab, displayName, clickSendMessageHandler }) => {
+const ChatFooter = ({ groupId, active, groupActive, currentTab, displayName, clickSendMessageHandler }) => {
 
     if (currentTab === 'groupMessage') {
         if (active) {
             if (!groupActive) {
-                return <ChatInputGroupArchived />
+                return <ChatInputGroupArchived groupId={groupId} />
             }
         } else {
             return <ChatInputUserArchived message="You are no longer part of this conversation." />
