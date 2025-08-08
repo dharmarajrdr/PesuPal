7e48874d6f0dade66dbf273734f790fbc35e43e5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-08 18:48:28 +0530

    Add GroupConfigurationUpdateDto and updateGroupChatConfiguration method in GroupChatConfigurationController

--- server/src/main/java/com/pesupal/server/controller/chat/group_message/GroupChatConfigurationController.java
+++ server/src/main/java/com/pesupal/server/controller/chat/group_message/GroupChatConfigurationController.java
@@ -3,6 +3,7 @@ package com.pesupal.server.controller.chat.group_message;
 import com.pesupal.server.dto.request.chat.group_message.UpdateGroupChatConfigurationDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.dto.response.chat.group_message.GroupChatPermissionDto;
+import com.pesupal.server.dto.response.chat.group_message.GroupConfigurationUpdateDto;
 import com.pesupal.server.service.interfaces.chat.group_message.GroupChatConfigurationService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +18,17 @@ public class GroupChatConfigurationController {
 
     private final GroupChatConfigurationService groupChatConfigurationService;
 
-    @PatchMapping("")
-    public ResponseEntity<ApiResponseDto> updateGroupChatConfiguration(@RequestBody UpdateGroupChatConfigurationDto updateGroupChatConfigurationDto) {
+    @PutMapping("")
+    public ResponseEntity<ApiResponseDto> setGroupChatConfiguration(@RequestBody UpdateGroupChatConfigurationDto updateGroupChatConfigurationDto) {
 
         groupChatConfigurationService.updateGroupChatConfiguration(updateGroupChatConfigurationDto);
+        return ResponseEntity.ok().body(new ApiResponseDto("Group chat configuration updated successfully"));
+    }
+
+    @PatchMapping("")
+    public ResponseEntity<ApiResponseDto> updateGroupChatConfiguration(@RequestBody GroupConfigurationUpdateDto groupConfigurationUpdateDto) throws IllegalAccessException {
+
+        groupChatConfigurationService.updateGroupChatConfiguration(groupConfigurationUpdateDto);
         return ResponseEntity.ok().body(new ApiResponseDto("Group chat configuration updated successfully"));
     }
 
