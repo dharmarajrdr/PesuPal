bee7f4ed9d9f5655224d75f0179cb7971cdbef3e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-09 15:57:34 +0530

    Refactor CreateRecordLayout to enhance functionality and improve expand/cancel handling

--- client/src/components/Team/ManageWork/CreateRecord/CreateRecordLayout.js
+++ client/src/components/Team/ManageWork/CreateRecord/CreateRecordLayout.js
@@ -4,9 +4,11 @@ import './CreateRecordLayout.css';
 import RecordFormLayout from './RecordFormLayout';
 import { useNavigate, useParams } from 'react-router-dom';
 
-const CreateRecordLayout = () => {
+const CreateRecordLayout = ({ moduleId, setShowQuickCreateRecord, view }) => {
 
-  const { moduleId } = useParams();
+  const params = useParams();
+  view = view || 'create';
+  moduleId = moduleId || params.moduleId;
   const navigate = useNavigate();
 
   const { data: currentModuleData } = useSelector((state) => state.currentModule);
@@ -15,14 +17,25 @@ const CreateRecordLayout = () => {
   const onCancel = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    navigate(`/manage/module/${moduleId}/list`);
+    if (setShowQuickCreateRecord) {
+      setShowQuickCreateRecord(false);
+    } else {
+      navigate(`/manage/module/${moduleId}/list`);
+    }
+  }
+
+  const onExpand = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    navigate(`/manage/module/${moduleId}/create`);
   }
 
   return createRecord ? (
     <div id='create-new-record' className='w100 FCCS h100P'>
-      <div className='FRCB w100'>
+      <div className='FRCB w100' id='create-new-record-header'>
         <h1 id='new-record-title'>New Record</h1>
         <div className='FRCE' id='new-record-actions'>
+          {view != 'create' && <i class="fa-solid fa-up-right-and-down-left-from-center mR10" id='expand-create-record-page' onClick={onExpand} title='Expand Create Record'></i>}
           <button id='cancel-button' onClick={onCancel}>Cancel</button>
           <button id='create-button'>Create</button>
         </div>
