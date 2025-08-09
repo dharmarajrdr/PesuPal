cd5f92933224aaf779075f79aec340c53bd9154e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-09 10:38:32 +0530

    Fix overflow issue in record-field class for better layout handling

---
+++ client/src/components/Team/ManageWork/CreateRecord/data.js
@@ -0,0 +1,394 @@
+export default [
+    {
+        "fieldId": 40,
+        "fieldName": "Subject",
+        "fieldType": "STRING",
+        "required": true,
+        "searchable": false,
+        "filterable": true,
+        "sortable": true,
+        "editable": true,
+        "showInList": true,
+        "showInDetail": true,
+        "data": "/webapps/zohocrm/javascript/crm.js"
+    },
+    {
+        "fieldId": 41,
+        "fieldName": "Created By",
+        "fieldType": "USER",
+        "required": true,
+        "searchable": false,
+        "filterable": true,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "id": "AMwYOJWFAkoQ",
+            "displayName": "Dharmaraj R",
+            "displayPicture": "https://www.updatenews360.com/english/wp-content/uploads/2022/04/Xefntr7z_400x400.jpg",
+            "archived": false
+        }
+    },
+    {
+        "fieldId": 42,
+        "fieldName": "Created At",
+        "fieldType": "DATE_TIME",
+        "required": true,
+        "searchable": false,
+        "filterable": true,
+        "sortable": true,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": "2025-08-03T16:06:20.801513"
+    },
+    {
+        "fieldId": 43,
+        "fieldName": "Updated By",
+        "fieldType": "USER",
+        "required": false,
+        "searchable": false,
+        "filterable": true,
+        "sortable": true,
+        "editable": false,
+        "showInList": false,
+        "showInDetail": true
+    },
+    {
+        "fieldId": 44,
+        "fieldName": "Updated At",
+        "fieldType": "DATE_TIME",
+        "required": false,
+        "searchable": false,
+        "filterable": true,
+        "sortable": true,
+        "editable": false,
+        "showInList": false,
+        "showInDetail": true
+    },
+    {
+        "fieldId": 45,
+        "fieldName": "Notes",
+        "fieldType": "TEXT",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": false,
+        "showInDetail": true
+    },
+    {
+        "fieldId": 46,
+        "fieldName": "File Owner",
+        "fieldType": "USER",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "id": "AMwYOJWFAkoQ",
+            "displayName": "Dharmaraj R",
+            "displayPicture": "https://www.updatenews360.com/english/wp-content/uploads/2022/04/Xefntr7z_400x400.jpg",
+            "archived": false
+        }
+    },
+    {
+        "fieldId": 47,
+        "fieldName": "Service Name",
+        "fieldType": "SELECT",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": false,
+        "showInDetail": true,
+        "data": [
+            {
+                "id": 22,
+                "value": "zohocrm",
+                "selected": true,
+                "default": false
+            },
+            {
+                "id": 23,
+                "value": "ignite",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 24,
+                "value": "abmclient",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 25,
+                "value": "zohocommandcenter",
+                "selected": false,
+                "default": false
+            }
+        ]
+    },
+    {
+        "fieldId": 51,
+        "fieldName": "Status",
+        "fieldType": "TRANSITION",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "name": "Backlog",
+            "score": 0
+        }
+    },
+    {
+        "fieldId": 54,
+        "fieldName": "Subscription (Per Month)",
+        "fieldType": "CURRENCY",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "currency": "INR",
+            "amount": 14.4
+        }
+    },
+    {
+        "fieldId": 31,
+        "fieldName": "Notes",
+        "fieldType": "TEXT",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true
+    },
+    {
+        "fieldId": 32,
+        "fieldName": "Service Name",
+        "fieldType": "SELECT",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": [
+            {
+                "id": 10,
+                "value": "zohocrm",
+                "selected": true,
+                "default": false
+            },
+            {
+                "id": 11,
+                "value": "abmclient",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 12,
+                "value": "ignite",
+                "selected": false,
+                "default": false
+            }
+        ]
+    },
+    {
+        "fieldId": 33,
+        "fieldName": "File Name",
+        "fieldType": "STRING",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": "/webapps/zohocrm/javascript/crm.js"
+    },
+    {
+        "fieldId": 34,
+        "fieldName": "Bug Owner",
+        "fieldType": "USER",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "id": "uexJUG4ral4j",
+            "displayName": "Mohankumar R",
+            "displayPicture": "https://pbs.twimg.com/media/E03qKiHUYAE9tNW.jpg:large",
+            "archived": false
+        }
+    },
+    {
+        "fieldId": 35,
+        "fieldName": "Status",
+        "fieldType": "SELECT",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": [
+            {
+                "id": 13,
+                "value": "Open",
+                "selected": true,
+                "default": true
+            },
+            {
+                "id": 14,
+                "value": "In Progress",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 15,
+                "value": "Fixed",
+                "selected": false,
+                "default": false
+            }
+        ]
+    },
+    {
+        "fieldId": 36,
+        "fieldName": "Due Date",
+        "fieldType": "DATE_TIME",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": "2025-08-06T02:04:08.575855"
+    },
+    {
+        "fieldId": 37,
+        "fieldName": "Screenshot",
+        "fieldType": "LINK",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": {
+            "title": "Reference image",
+            "url": "https://sample-image.com"
+        }
+    },
+    {
+        "fieldId": 38,
+        "fieldName": "Bug Source",
+        "fieldType": "SELECT",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": [
+            {
+                "id": 16,
+                "value": "Shredder",
+                "selected": true,
+                "default": false
+            },
+            {
+                "id": 17,
+                "value": "Flash",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 18,
+                "value": "Murphy",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 19,
+                "value": "Aalam",
+                "selected": false,
+                "default": false
+            }
+        ]
+    },
+    {
+        "fieldId": 39,
+        "fieldName": "Layout",
+        "fieldType": "SELECT",
+        "required": true,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true,
+        "data": [
+            {
+                "id": 20,
+                "value": "Feature & Enhancement",
+                "selected": false,
+                "default": false
+            },
+            {
+                "id": 21,
+                "value": "Release",
+                "selected": true,
+                "default": false
+            }
+        ]
+    },
+    {
+        "fieldId": 55,
+        "fieldName": "Attachement",
+        "fieldType": "FILE",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true
+    },
+    {
+        "fieldId": 56,
+        "fieldName": "Datacenter Location",
+        "fieldType": "GEO_LOCATION",
+        "required": false,
+        "searchable": false,
+        "filterable": false,
+        "sortable": false,
+        "editable": false,
+        "showInList": true,
+        "showInDetail": true
+    }
+]
