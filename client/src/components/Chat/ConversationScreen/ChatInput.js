b6031c9f7f03f88bf1f8ee134d579cb80f5e7339
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-09 11:17:57 +0530

    Refactor ChatInput component to include ref for textarea and improve focus handling

--- client/src/components/Chat/ConversationScreen/ChatInput.js
+++ client/src/components/Chat/ConversationScreen/ChatInput.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './ChatInput.css'
 import { useSelector } from 'react-redux';
 
@@ -9,17 +9,20 @@ const ChatInput = ({ clickSendMessageHandler }) => {
 
     const chatId = useSelector((state) => state.chatId);
 
+    const chatInputRef = useRef(null);
+
     const handleSend = () => {
         if (message.trim()) {
             clickSendMessageHandler({ message });
             setMessage("");
+            chatInputRef.current.focus();
             // fileInputRef?.current?.value = '';
         }
     };
 
     return (
         <div className="chat-input w100 FRSS">
-            <textarea type="text" value={message} autoFocus onChange={(e) => setMessage(e.target.value)} placeholder="Type your message..." />
+            <textarea ref={chatInputRef} type="text" value={message} autoFocus onChange={(e) => setMessage(e.target.value)} placeholder="Type your message..." />
             {/* <input type="file" ref={fileInputRef} className="file-upload" /> */}
             <button onClick={handleSend} className="send-button">
                 <i className='fa fa-paper-plane' />
