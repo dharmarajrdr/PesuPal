bd58bf2f63e087f72fde121350cb7a2406b847db
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-11 20:27:48 +0530

    Enhance AttachmentPreview component: implement file removal functionality and improve styling for attachment items

--- client/src/components/Chat/AttachmentPreview.js
+++ client/src/components/Chat/AttachmentPreview.js
@@ -1,13 +1,42 @@
+import utils from '../../utils';
 import './AttachmentPreview.css';
 
-const AttachmentPreview = () => {
-    return (
+const AttachmentPreview = ({ files, setFiles }) => {
+
+    const removeFile = (index) => {
+        setFiles((prev) => {
+            const updated = [...prev];
+            updated.splice(index, 1);
+            return updated;
+        });
+    };
+
+    return files.length > 0 ? (
         <div className='entire-screen-overlay FRCC'>
             <div id='attachment-preview' className='centerMe'>
-
+                <div className="w100 FCSS mB10">
+                    {files.map(({ preview, file }, index) => (
+                        <div key={index} className="attachment-item">
+                            <div className="file-preview">
+                                {preview ? (
+                                    <img src={preview} alt={file.name} className="file-thumbnail" />
+                                ) : (
+                                    <i className={`${utils.getFileIcon(file)} file-icon`} />
+                                )}
+                            </div>
+                            <div className="file-info FCSS">
+                                <span className="file-name mB5">{file.name}</span>
+                                <span className="file-size">
+                                    {utils.formatFileSize(file.size)}
+                                </span>
+                            </div>
+                            <i className="fa fa-trash remove-btn" onClick={() => removeFile(index)} title="Remove file" />
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
-    )
+    ) : null;
 }
 
 export default AttachmentPreview
