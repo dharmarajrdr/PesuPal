02849555a7c8e188e781a5d77ab059575c1efb46
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-11 18:03:41 +0530

    Add emoji picker functionality to ChatInput component

--- client/src/components/Chat/ConversationScreen/ChatInput.js
+++ client/src/components/Chat/ConversationScreen/ChatInput.js
@@ -1,11 +1,13 @@
 import { useRef, useState } from 'react';
 import './ChatInput.css'
+import EmojiPicker from 'emoji-picker-react';
 import { useSelector } from 'react-redux';
 
 const ChatInput = ({ clickSendMessageHandler }) => {
 
     // const fileInputRef = useRef();
     const [message, setMessage] = useState('');
+    const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
     const chatId = useSelector((state) => state.chatId);
 
@@ -15,15 +17,38 @@ const ChatInput = ({ clickSendMessageHandler }) => {
         if (message.trim()) {
             clickSendMessageHandler({ message });
             setMessage("");
+            setShowEmojiPicker(false);
             chatInputRef.current.focus();
             // fileInputRef?.current?.value = '';
         }
     };
 
+    const onEmojiClick = (emojiData, event) => {
+
+        setMessage((prevMessage) => prevMessage + emojiData.emoji);
+    };
+
+    const emojiPickerClickHandler = () => {
+
+        setShowEmojiPicker(!showEmojiPicker);
+        if (showEmojiPicker) {
+            chatInputRef.current.focus();
+        } else {
+            chatInputRef.current.blur();
+        }
+    }
+
+
     return (
         <div className="chat-input w100 FRSS">
             <textarea ref={chatInputRef} type="text" value={message} autoFocus onChange={(e) => setMessage(e.target.value)} placeholder="Type your message..." />
             {/* <input type="file" ref={fileInputRef} className="file-upload" /> */}
+            {showEmojiPicker && <div id='emoji-picker'>
+                <EmojiPicker onEmojiClick={onEmojiClick} />
+            </div>}
+            <button onClick={emojiPickerClickHandler} className="emoji-button">
+                <i className='fa fa-smile' />
+            </button>
             <button onClick={handleSend} className="send-button">
                 <i className='fa fa-paper-plane' />
             </button>
