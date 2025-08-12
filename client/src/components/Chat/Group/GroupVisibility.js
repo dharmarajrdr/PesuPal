c695c54e1375ac3423879389a5d6ad4fc7787fac
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-12 20:27:47 +0530

    Refactor group visibility handling by creating GroupVisibility component and implement UpdateGroupModal for group updates

---
+++ client/src/components/Chat/Group/GroupVisibility.js
@@ -0,0 +1,19 @@
+const GroupVisibility = ({ isPublic, setIsPublic }) => {
+
+    return <div className="visibility-toggle-container">
+        <label className="toggle-label">Visibility</label>
+        <div className="segmented-toggle">
+            <div className={`toggle-option ${isPublic ? 'active' : ''}`} onClick={() => setIsPublic(true)}>
+                <i className="fa fa-globe" />
+                <span className='mL5'>Open to all</span>
+            </div>
+            <div className={`toggle-option ${!isPublic ? 'active' : ''}`} onClick={() => setIsPublic(false)}>
+                <i className="fa fa-lock" />
+                <span className='mL5'>Closed</span>
+            </div>
+            <div className={`toggle-bg ${isPublic ? 'left' : 'right'}`} />
+        </div>
+    </div>
+}
+
+export default GroupVisibility
--- client/src/components/Chat/Group/UpdateGroupModal.js
+++ client/src/components/Chat/Group/UpdateGroupModal.js
@@ -4,11 +4,11 @@ import { apiRequest } from '../../../http_request';
 import { useNavigate } from 'react-router-dom';
 import GroupVisibility from './GroupVisibility';
 
-const CreateGroupModal = ({ setShowCreateGroupModal }) => {
+const UpdateGroupModal = ({ setShowCreateGroupModal, groupData }) => {
 
-    const [groupName, setGroupName] = useState('');
-    const [groupDescription, setGroupDescription] = useState('');
-    const [isPublic, setIsPublic] = useState(true);
+    const [groupName, setGroupName] = useState(groupData.name);
+    const [groupDescription, setGroupDescription] = useState(groupData.description);
+    const [isPublic, setIsPublic] = useState(groupData.visibility === 'PUBLIC');
 
     const navigate = useNavigate();
 
@@ -30,34 +30,34 @@ const CreateGroupModal = ({ setShowCreateGroupModal }) => {
             visibility: isPublic ? 'PUBLIC' : 'PRIVATE',
         };
 
-        apiRequest(`/api/v1/group/create`, 'POST', groupData).then(({ data }) => {
+        apiRequest(`/api/v1/group/update`, 'PATCH', groupData).then(({ data }) => {
             const { id } = data;
             closeCreateGroupModal();
-            navigate(`/chat/groups/${id}`);
         }).catch(({ message }) => {
-            console.error("Error creating group:", message);
+            console.error("Error updating group:", message);
             closeCreateGroupModal();
         });
     };
 
     return (
         <div className="FCCC entire-screen-overlay">
-            <div id="create-group-modal-content" className="FCCC modal-box">
+            <div id="update-group-modal-content" className="FCCC modal-box">
 
-                <h2>Create Group</h2>
+                <h2>Update Group</h2>
 
                 <div className='FCSS w100'>
                     <input type="text" placeholder="Enter group name" value={groupName} onChange={(e) => setGroupName(e.target.value)} className="group-name-input" />
                     <input type="text" placeholder="Enter group description (optional)" value={groupDescription} onChange={(e) => setGroupDescription(e.target.value)} className="group-description-input" />
                     <GroupVisibility isPublic={isPublic} setIsPublic={setIsPublic} />
                 </div>
+
                 <div className='FRCC w100'>
                     <button className="cancel-btn mR5" onClick={closeCreateGroupModal}>Cancel</button>
-                    <button className="submit-btn mL5" onClick={handleSubmit}>Create</button>
+                    <button className="submit-btn mL5" onClick={handleSubmit}>Update</button>
                 </div>
             </div>
         </div>
     );
 };
 
-export default CreateGroupModal;
+export default UpdateGroupModal;
