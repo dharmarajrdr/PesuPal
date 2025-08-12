ffaf58db96da973b466c618161f83f4bb99609fd
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-12 18:19:11 +0530

    Update media file retrieval to use Optional for better null safety; unlink media files upon message deletion

--- server/src/main/java/com/pesupal/server/repository/chat/direct_message/DirectMessageRepository.java
+++ server/src/main/java/com/pesupal/server/repository/chat/direct_message/DirectMessageRepository.java
@@ -45,23 +45,23 @@ public interface DirectMessageRepository extends JpaRepository<DirectMessage, Lo
                 dm.deleted AS deleted
             
             FROM direct_message dm
-            
+
             JOIN direct_message_chat dmc ON dmc.id = dm.direct_message_chat_id
             
             JOIN org_member om ON om.user_id = CASE
-                WHEN dm.sender_id = :userId THEN dm.receiver_id
-                ELSE dm.sender_id
-            END AND om.org_id = :orgId
+                    WHEN dm.sender_id = :userId THEN dm.receiver_id
+                    ELSE dm.sender_id
+                END AND om.org_id = :orgId
             
             JOIN org_member sender_member ON sender_member.user_id = dm.sender_id AND sender_member.org_id = :orgId
             
             JOIN (
-                SELECT direct_message_chat_id, MAX(created_at) AS latest
+                SELECT direct_message_chat_id, MAX(id) AS latest_id
                 FROM direct_message
                 WHERE org_id = :orgId
                   AND (sender_id = :userId OR receiver_id = :userId)
                 GROUP BY direct_message_chat_id
-            ) latest_msg ON dm.direct_message_chat_id = latest_msg.direct_message_chat_id AND dm.created_at = latest_msg.latest
+            ) latest_msg ON dm.id = latest_msg.latest_id
             
             WHERE dm.org_id = :orgId
               AND (dm.sender_id = :userId OR dm.receiver_id = :userId)
@@ -70,7 +70,7 @@ public interface DirectMessageRepository extends JpaRepository<DirectMessage, Lo
             LIMIT :limit OFFSET :offset
             """, nativeQuery = true)
     List<RecentPrivateChatProjection> findRecentChatsPaged(@Param("userId") Long userId, @Param("orgId") Long orgId, @Param("limit") int limit, @Param("offset") int offset);
-
+    
 
     @Query(value = """
             SELECT COUNT(*)
