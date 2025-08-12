5cbf94897454b821c2d61da8067aea09656e5f1e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-12 18:19:43 +0530

    Add GroupMessageMediaFileService interface for group message media file management

--- server/src/main/java/com/pesupal/server/service/interfaces/chat/direct_message/DirectMessageMediaFileService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/chat/direct_message/DirectMessageMediaFileService.java
@@ -1,8 +1,11 @@
 package com.pesupal.server.service.interfaces.chat.direct_message;
 
+import com.pesupal.server.model.chat.direct_message.DirectMessage;
 import com.pesupal.server.model.chat.direct_message.DirectMessageMediaFile;
 
 public interface DirectMessageMediaFileService {
 
     DirectMessageMediaFile save(DirectMessageMediaFile directMessageMediaFile);
+
+    void unlinkMediaFilesByDirectMessage(DirectMessage directMessage);
 }
