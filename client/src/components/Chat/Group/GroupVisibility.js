cb872c68bb8f092316ca2d59061798fe7ae7823d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-13 18:04:41 +0530

    Add groupVisibilityEditable prop to GroupVisibility component for conditional toggling

--- client/src/components/Chat/Group/GroupVisibility.js
+++ client/src/components/Chat/Group/GroupVisibility.js
@@ -1,15 +1,17 @@
 import './GroupVisibility.css';
 
-const GroupVisibility = ({ isPublic, setIsPublic }) => {
+const GroupVisibility = ({ isPublic, setIsPublic, groupVisibilityEditable }) => {
+
+    groupVisibilityEditable = groupVisibilityEditable !== undefined ? groupVisibilityEditable : true;
 
     return <div className="visibility-toggle-container">
         <label className="toggle-label">Visibility</label>
         <div className="segmented-toggle">
-            <div className={`toggle-option ${isPublic ? 'active' : ''}`} onClick={() => setIsPublic(true)}>
+            <div className={`toggle-option ${isPublic ? 'active' : ''}`} onClick={() => groupVisibilityEditable ? setIsPublic(true) : null}>
                 <i className="fa fa-globe" />
                 <span className='mL5'>Open to all</span>
             </div>
-            <div className={`toggle-option ${!isPublic ? 'active' : ''}`} onClick={() => setIsPublic(false)}>
+            <div className={`toggle-option ${!isPublic ? 'active' : ''}`} onClick={() => groupVisibilityEditable ? setIsPublic(false) : null}>
                 <i className="fa fa-lock" />
                 <span className='mL5'>Closed</span>
             </div>
