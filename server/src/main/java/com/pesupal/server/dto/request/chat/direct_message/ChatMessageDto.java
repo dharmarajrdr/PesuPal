cbc44b0ab67ebdecf57958ce26b46ecd7ebee219
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-14 21:05:50 +0530

    Add message status and scheduling fields to ChatMessageDto and LastMessageDto

--- server/src/main/java/com/pesupal/server/dto/request/chat/direct_message/ChatMessageDto.java
+++ server/src/main/java/com/pesupal/server/dto/request/chat/direct_message/ChatMessageDto.java
@@ -2,8 +2,11 @@ package com.pesupal.server.dto.request.chat.direct_message;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.pesupal.server.dto.response.MediaUploadDto;
+import com.pesupal.server.model.chat.MessageStatus;
 import lombok.Data;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Data
 @ToString
@@ -21,4 +24,8 @@ public class ChatMessageDto {
     private String message;
 
     private MediaUploadDto media;
+
+    private MessageStatus messageStatus = MessageStatus.SENT;
+
+    private LocalDateTime scheduleAt;
 }
