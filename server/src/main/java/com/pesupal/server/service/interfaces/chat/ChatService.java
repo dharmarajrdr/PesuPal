695890ecd1c64ad3dd4f194fffe932ddbbf5a93a
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-14 22:30:26 +0530

    Update ChatHeader to hide scheduled messages by default and pass chatId to ScheduledMessageOverlay

--- server/src/main/java/com/pesupal/server/service/interfaces/chat/ChatService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/chat/ChatService.java
@@ -4,6 +4,8 @@ import com.pesupal.server.dto.request.chat.direct_message.ChatMessageDto;
 import com.pesupal.server.dto.response.chat.MessageDto;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
+import java.util.List;
+
 public interface ChatService {
 
     MessageDto save(ChatMessageDto chatMessageDto);
@@ -11,4 +13,6 @@ public interface ChatService {
     void broadcastMessage(MessageDto messageDto, SimpMessagingTemplate messagingTemplate);
 
     void schedule(ChatMessageDto chatMessageDto);
+
+    List<MessageDto> getScheduledMessages(String chatId);
 }
