91f3c210cf3296af707542bea953ff8139af0b4c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-15 23:03:00 +0530

    Add GeneralLayout component with AccountSection for managing email and mobile numbers

--- client/src/components/Settings/SettingsMainContainer.js
+++ client/src/components/Settings/SettingsMainContainer.js
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import SubscriptionPlan from './subscription/SubscriptionPlan'
 import PermissionDenied from '../Auth/PermissionDenied'
 import ManagePeopleLayout from '../ManagePeople/ManagePeopleLayout'
+import GeneralLayout from './general/GeneralLayout'
 
 const SettingsMainContainer = ({ width }) => {
 
@@ -9,6 +10,7 @@ const SettingsMainContainer = ({ width }) => {
         <div id='settings-main-container' style={{ width }}>
             <Routes>
                 <Route path="" element={<Navigate to="/settings/pricing" />} />
+                <Route path="/general/*" element={<GeneralLayout />} />
                 <Route path="/pricing/*" element={<SubscriptionPlan />} />
                 <Route path='/manage-people/*' element={<ManagePeopleLayout />} />
                 <Route path="*" element={<PermissionDenied />} />
