514b131ca5b1e255af7d1b0f8f155f4536cbcc2c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-15 19:12:41 +0530

    Add image upload functionality to CreateGroupModal and improve error handling in S3Service

--- client/src/components/Chat/Group/CreateGroupModal.js
+++ client/src/components/Chat/Group/CreateGroupModal.js
@@ -3,12 +3,18 @@ import './CreateGroupModal.css';
 import { apiRequest } from '../../../http_request';
 import { useNavigate } from 'react-router-dom';
 import GroupVisibility from './GroupVisibility';
+import ImageUploader from '../../ImageUploader';
+import { useDispatch } from 'react-redux';
+import { showPopup } from '../../../store/reducers/PopupSlice';
+import Media from '../../../Media';
 
 const CreateGroupModal = ({ setShowCreateGroupModal }) => {
 
+    const dispatch = useDispatch();
     const [groupName, setGroupName] = useState('');
     const [groupDescription, setGroupDescription] = useState('');
     const [isPublic, setIsPublic] = useState(true);
+    const [file, setFile] = useState(null);
 
     const navigate = useNavigate();
 
@@ -21,7 +27,7 @@ const CreateGroupModal = ({ setShowCreateGroupModal }) => {
 
     const handleSubmit = () => {
         if (!groupName.trim()) {
-            alert("Group name can't be empty!");
+            dispatch(showPopup({ message: "Group name can't be empty!", type: 'error' }));
             return;
         }
         const groupData = {
@@ -30,14 +36,30 @@ const CreateGroupModal = ({ setShowCreateGroupModal }) => {
             visibility: isPublic ? 'PUBLIC' : 'PRIVATE',
         };
 
-        apiRequest(`/api/v1/group/create`, 'POST', groupData).then(({ data }) => {
-            const { id } = data;
-            closeCreateGroupModal();
-            navigate(`/chat/groups/${id}`);
-        }).catch(({ message }) => {
-            console.error("Error creating group:", message);
-            closeCreateGroupModal();
-        });
+        const createGroup = async (groupData) => {
+
+            apiRequest(`/api/v1/group/create`, 'POST', groupData).then(({ data, message }) => {
+                const { id } = data;
+                closeCreateGroupModal();
+                navigate(`/chat/groups/${id}`);
+                dispatch(showPopup({ message, type: 'success' }));
+            }).catch(({ message }) => {
+                dispatch(showPopup({ message, type: 'error' }));
+            });
+        }
+
+        if (file == null) {
+            createGroup(groupData);
+        } else {
+            Media.uploadSingleMedia({ file }).then(({ data }) => {
+                const { mediaId, extension } = data || {};
+                Object.assign(groupData, { 'displayPicture': `${mediaId}.${extension}` });
+                createGroup(groupData);
+            }).catch(({ message }) => {
+                dispatch(showPopup({ message, type: 'error' }));
+            });
+        }
+
     };
 
     return (
@@ -47,6 +69,7 @@ const CreateGroupModal = ({ setShowCreateGroupModal }) => {
                 <h2>Create Group</h2>
 
                 <div className='FCSS w100'>
+                    <ImageUploader onImageSelect={(file) => setFile(file)} style={{ width: '100px', height: '100px', marginBottom: '20px' }} />
                     <input type="text" placeholder="Enter group name" value={groupName} onChange={(e) => setGroupName(e.target.value)} className="group-name-input" />
                     <input type="text" placeholder="Enter group description (optional)" value={groupDescription} onChange={(e) => setGroupDescription(e.target.value)} className="group-description-input" />
                     <GroupVisibility isPublic={isPublic} setIsPublic={setIsPublic} />
