b8471980f58b2e5dda2545edecfdddb121b0e3ff
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-15 18:00:35 +0530

    Add ImageUploader component with styling and functionality for image upload and preview

---
+++ client/src/components/ImageUploader.js
@@ -0,0 +1,54 @@
+import React, { useRef, useState } from "react";
+import "./ImageUploader.css";
+
+const ImageContainer = ({ image, onEditClick, onDeleteClick }) => {
+
+    return <div id="image-container">
+        <div className="FRCC centerMe" id="actionButtons">
+            <i className="fa fa-edit mR5" aria-hidden="true" onClick={onEditClick}></i>
+            <i className="fa fa-trash mL5" aria-hidden="true" onClick={onDeleteClick}></i>
+        </div>
+        <img src={image} alt="Uploaded" />
+    </div>
+}
+
+const Placeholder = ({ onClick }) => {
+    return <div className="placeholder" onClick={onClick}>
+        <i className="fas fa-image"></i> Upload
+    </div>
+}
+
+const ImageUploader = ({ defaultImage, onImageSelect, style }) => {
+
+    const [image, setImage] = useState(defaultImage || null);
+    const fileInputRef = useRef(null);
+
+    const handleImageClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const imageUrl = URL.createObjectURL(file);
+            setImage(imageUrl);
+            if (onImageSelect) {
+                onImageSelect(file);
+            }
+        }
+    };
+
+    return (
+        <div className="image-uploader w100 FRCC">
+            <div id="image-preview" style={{ ...style }}>
+                {image ?
+                    <ImageContainer image={image} onEditClick={handleImageClick} onDeleteClick={() => setImage(null)} /> :
+                    <Placeholder onClick={handleImageClick} />
+                }
+            </div>
+            <input type="file" accept="image/*" ref={fileInputRef} style={{ display: "none" }} onChange={handleFileChange} />
+        </div>
+    );
+};
+
+export default ImageUploader;
