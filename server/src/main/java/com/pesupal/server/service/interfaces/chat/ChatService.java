f310217a1f84466496283dfffa0f6415f2de655d
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-15 09:21:31 +0530

    Refactor ChatService interface: enhance type safety by making ChatMessageDto generic

--- server/src/main/java/com/pesupal/server/service/interfaces/chat/ChatService.java
+++ server/src/main/java/com/pesupal/server/service/interfaces/chat/ChatService.java
@@ -1,18 +1,27 @@
 package com.pesupal.server.service.interfaces.chat;
 
+import com.pesupal.server.dto.request.chat.RescheduleMessageDto;
 import com.pesupal.server.dto.request.chat.direct_message.ChatMessageDto;
 import com.pesupal.server.dto.response.chat.MessageDto;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.util.List;
 
-public interface ChatService {
+public interface ChatService<T> {
 
-    MessageDto save(ChatMessageDto chatMessageDto);
+    MessageDto save(ChatMessageDto<T> chatMessageDto);
 
     void broadcastMessage(MessageDto messageDto, SimpMessagingTemplate messagingTemplate);
 
-    void schedule(ChatMessageDto chatMessageDto);
+    void schedule(ChatMessageDto<T> chatMessageDto);
 
     List<MessageDto> getScheduledMessages(String chatId);
+
+    void reschedule(Long messageId, RescheduleMessageDto rescheduleMessageDto);
+
+    void unschedule(Long messageId);
+
+    void deleteSchedule(Long messageId);
+
+    void deleteAllScheduledMessages(String chatId);
 }
