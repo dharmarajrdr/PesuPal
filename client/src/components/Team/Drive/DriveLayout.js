77b68b2ae14c51299953a56a5f97ac0e8321a719
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-17 16:21:58 +0530

    Refactor DriveLayout component to handle permission and page not found states

--- client/src/components/Team/Drive/DriveLayout.js
+++ client/src/components/Team/Drive/DriveLayout.js
@@ -1,22 +1,29 @@
-import PageNotFound from '../../Auth/PageNotFound'
-import DriveDashboard from './DriveDashboard'
-import FileCategoryShortcut from './FileCategoryShortcut'
+import { useState } from 'react'
 import FileManager from './FileManager'
+import InsideDirectory from './InsideDirectory'
+import PageNotFound from '../../Auth/PageNotFound'
+import FileCategoryShortcut from './FileCategoryShortcut'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import PermissionDenied from '../../Auth/PermissionDenied'
 
 const DriveLayout = () => {
 
-    return <div className='FCSS p20 w100 Layout'>
-        <FileManager />
-        <FileCategoryShortcut />
-        <Routes>
-            <Route path='/' element={<Navigate to='/store/personal_space' />} />
-            <Route path='/trash' element={<PageNotFound />} />
-            <Route path='/:space' element={<DriveDashboard />} />
-            <Route path='/:space/folder' element={<Navigate to='/store/personal_space' />} />
-            <Route path='/:space/folder/:folderId' element={<DriveDashboard />} />
-        </Routes>
-    </div>
+    const [pageNotFound, setPageNotFound] = useState(false);
+    const [permissionDenied, setPermissionDenied] = useState(false);
+
+    return permissionDenied ? <PermissionDenied /> :
+        pageNotFound ? <PageNotFound /> :
+            <div className='FCSS p20 w100 Layout'>
+                <FileManager />
+                <FileCategoryShortcut />
+                <Routes>
+                    <Route path='/' element={<Navigate to='/store/personal_space' />} />
+                    <Route path='/trash' element={<PageNotFound />} />
+                    <Route path='/:space' element={<InsideDirectory setPageNotFound={setPageNotFound} setPermissionDenied={setPermissionDenied} />} />
+                    <Route path='/:space/folder' element={<Navigate to='/store/personal_space' />} />
+                    <Route path='/:space/folder/:folderId' element={<InsideDirectory setPageNotFound={setPageNotFound} setPermissionDenied={setPermissionDenied} />} />
+                </Routes>
+            </div>
 }
 
 export default DriveLayout
