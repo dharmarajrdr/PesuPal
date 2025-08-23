ce4dd9b1f2ad31f13bc77b38e3b28df4d3cda0f2
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-23 20:16:47 +0530

    Refactor unlinkMediaFromPost method to improve media deletion logic

--- server/src/main/java/com/pesupal/server/service/implementations/post/PostMediaServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/post/PostMediaServiceImpl.java
@@ -3,13 +3,17 @@ package com.pesupal.server.service.implementations.post;
 import com.pesupal.server.model.post.Post;
 import com.pesupal.server.repository.post.PostMediaRepository;
 import com.pesupal.server.service.interfaces.post.PostMediaService;
+import com.pesupal.server.strategies.media_storage.S3Service;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class PostMediaServiceImpl implements PostMediaService {
 
+    private final S3Service s3Service;
     private final PostMediaRepository postMediaRepository;
 
     /**
@@ -20,6 +24,10 @@ public class PostMediaServiceImpl implements PostMediaService {
     @Override
     public void unlinkMediaFromPost(Post post) {
         post.getPostMedia().forEach(postMedia -> {
+            UUID mediaId = postMedia.getMediaId();
+            String extension = postMedia.getExtension();
+            String key = mediaId + "." + extension;
+            s3Service.deleteFile(key);
             postMedia.setPost(null);
             postMediaRepository.save(postMedia);
         });
