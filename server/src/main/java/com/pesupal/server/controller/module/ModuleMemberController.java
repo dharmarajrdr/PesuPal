c679faeaff3b75db0ba99f65fa21d2f563adbfad
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-23 08:27:26 +0530

    Add getMembersOfModule endpoint to ModuleMemberController and create ModuleMemberDto for member representation

--- server/src/main/java/com/pesupal/server/controller/module/ModuleMemberController.java
+++ server/src/main/java/com/pesupal/server/controller/module/ModuleMemberController.java
@@ -3,6 +3,7 @@ package com.pesupal.server.controller.module;
 import com.pesupal.server.dto.request.module.AddModuleMemberDto;
 import com.pesupal.server.dto.response.ApiResponseDto;
 import com.pesupal.server.dto.response.UserPreviewDto;
+import com.pesupal.server.dto.response.module.ModuleMemberDto;
 import com.pesupal.server.service.interfaces.module.ModuleMemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -25,7 +26,7 @@ public class ModuleMemberController {
         moduleMemberService.addMemberToModule(moduleId, addModuleMemberDto);
         return ResponseEntity.ok(new ApiResponseDto("Member added to module successfully"));
     }
-    
+
     @GetMapping("/{moduleId}/non-members")
     public ResponseEntity<ApiResponseDto> getNonMembersOfModule(@PathVariable String moduleId,
                                                                 @RequestParam(required = false) String search,
@@ -35,5 +36,16 @@ public class ModuleMemberController {
         Pageable pageable = Pageable.ofSize(size).withPage(page);
         List<UserPreviewDto> nonParticipantMembers = moduleMemberService.getNonMembersOfModule(moduleId, search, pageable);
         return ResponseEntity.ok(new ApiResponseDto("Non-member users retrieved successfully", nonParticipantMembers));
+    }
+
+    @GetMapping("/{moduleId}/members")
+    public ResponseEntity<ApiResponseDto> getMembersOfModule(@PathVariable String moduleId,
+                                                             @RequestParam(required = false) String search,
+                                                             @RequestParam(defaultValue = "0") int page,
+                                                             @RequestParam(defaultValue = "25") int size) {
+
+        Pageable pageable = Pageable.ofSize(size).withPage(page);
+        List<ModuleMemberDto> participantMembers = moduleMemberService.getMembersOfModule(moduleId, search, pageable);
+        return ResponseEntity.ok(new ApiResponseDto("Member users retrieved successfully", participantMembers));
     }
 }
