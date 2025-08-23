bd4057e5b895c49b104903ca36871552f91bc44b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-23 11:25:56 +0530

    Implement module permissions retrieval and update methods in ModulePermissionService

--- server/src/main/java/com/pesupal/server/service/implementations/module/ModulePermissionServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/module/ModulePermissionServiceImpl.java
@@ -1,24 +1,37 @@
 package com.pesupal.server.service.implementations.module;
 
+import com.pesupal.server.dto.request.module.UpdateModulePermissionDto;
+import com.pesupal.server.dto.response.module.ModulePermissionDto;
 import com.pesupal.server.exceptions.DataNotFoundException;
 import com.pesupal.server.exceptions.PermissionDeniedException;
 import com.pesupal.server.helpers.CurrentValueRetriever;
+import com.pesupal.server.helpers.ModuleHelper;
+import com.pesupal.server.helpers.StringHelper;
 import com.pesupal.server.model.module.*;
 import com.pesupal.server.model.module.Module;
 import com.pesupal.server.model.user.OrgMember;
 import com.pesupal.server.repository.module.ModulePermissionRepository;
 import com.pesupal.server.service.interfaces.module.ModuleMemberService;
 import com.pesupal.server.service.interfaces.module.ModulePermissionService;
+import com.pesupal.server.service.interfaces.module.ModuleService;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class ModulePermissionServiceImpl extends CurrentValueRetriever implements ModulePermissionService {
 
+    private final ModuleService moduleService;
     private final ModuleMemberService moduleMemberService;
     private final ModulePermissionRepository modulePermissionRepository;
 
-    public ModulePermissionServiceImpl(@Lazy ModuleMemberService moduleMemberService, ModulePermissionRepository modulePermissionRepository) {
+    public ModulePermissionServiceImpl(@Lazy ModuleMemberService moduleMemberService, ModulePermissionRepository modulePermissionRepository, ModuleService moduleService) {
+        this.moduleService = moduleService;
         this.moduleMemberService = moduleMemberService;
         this.modulePermissionRepository = modulePermissionRepository;
     }
@@ -95,4 +108,82 @@ public class ModulePermissionServiceImpl extends CurrentValueRetriever implement
             }
         }
     }
+
+    /**
+     * Retrieves the permissions for a module.
+     *
+     * @param moduleId
+     * @return
+     */
+    @Override
+    public List<ModulePermissionDto> getModulePermissions(String moduleId) {
+
+        OrgMember orgMember = getCurrentOrgMember();
+        Module module = moduleService.getModuleById(moduleId);
+        if (!ModuleHelper.isModuleOwner(module, orgMember)) {
+            throw new PermissionDeniedException("You do not have permission to access this module.");
+        }
+
+        List<ModulePermission> modulePermissions = modulePermissionRepository.findAllByModule(module);
+        Map<String, ModulePermissionDto> permissions = new HashMap<>();
+        for (ModulePermission modulePermission : modulePermissions) {
+            ModuleRole role = modulePermission.getRole();
+            for (Field field : ModulePermission.class.getDeclaredFields()) {
+                if (field.getType().equals(boolean.class) || field.getType().equals(Boolean.class)) {
+                    field.setAccessible(true); // allow access to private fields
+                    try {
+                        String fieldName = StringHelper.camelCaseToTitle(field.getName());
+                        boolean fieldValue = field.getBoolean(modulePermission);
+                        ModulePermissionDto modulePermissionDto = permissions.computeIfAbsent(fieldName, k -> new ModulePermissionDto(fieldName));
+                        switch (role) {
+                            case OWNER -> modulePermissionDto.setOwner(fieldValue);
+                            case MAINTAINER -> modulePermissionDto.setMaintainer(fieldValue);
+                            case MEMBER -> modulePermissionDto.setMember(fieldValue);
+                        }
+                    } catch (IllegalAccessException ignored) {
+                    }
+                }
+            }
+        }
+
+        List<ModulePermissionDto> permissionDtos = new ArrayList<>();
+        for (Map.Entry<String, ModulePermissionDto> entry : permissions.entrySet()) {
+            permissionDtos.add(entry.getValue());
+        }
+        return permissionDtos;
+    }
+
+    /**
+     * Updates the permissions for a module.
+     *
+     * @param updateModulePermissionDto
+     */
+    @Override
+    public void updateModulePermissions(UpdateModulePermissionDto updateModulePermissionDto) throws IllegalAccessException {
+
+        OrgMember orgMember = getCurrentOrgMember();
+        Module module = moduleService.getModuleById(updateModulePermissionDto.getModuleId());
+
+        if (!ModuleHelper.isModuleOwner(module, orgMember)) {
+            throw new PermissionDeniedException("You do not have permission to update permissions in this module.");
+        }
+
+        String fieldName = StringHelper.toCamelCase(updateModulePermissionDto.getName());
+        boolean isEnabled = updateModulePermissionDto.isEnable();
+        ModuleRole role = updateModulePermissionDto.getRole();
+
+        ModulePermission modulePermission = getModulePermissionByModuleAndRole(module, role);
+
+        for (Field field : ModulePermission.class.getDeclaredFields()) {
+            if (field.getName().equals(fieldName) && (field.getType().equals(boolean.class) || field.getType().equals(Boolean.class))) {
+                field.setAccessible(true); // allow access to private fields
+                field.setBoolean(modulePermission, isEnabled);
+                modulePermissionRepository.save(modulePermission);
+                return;
+            }
+        }
+
+        throw new DataNotFoundException("Field '" + fieldName + "' does not exist.");
+    }
+
 }
