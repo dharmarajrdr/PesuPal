c177ec4879a1457645e812992db3b41f32f2421b
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-23 22:54:57 +0530

    Refactor Feeds component to remove unused left navigation state and integrate FeedsLeftPanel in FeedsLayout

--- client/src/components/Feeds/Feeds.js
+++ client/src/components/Feeds/Feeds.js
@@ -1,11 +1,9 @@
-import FeedsLeftPanel from './FeedsLeftPanel/FeedsLeftPanel'
 import FeedsMainPanel from './FeedsMainPanel/FeedsMainPanel'
 import FeedsRightPanel from './FeedsRightPanel/FeedsRightPanel'
 
-const Feeds = ({ leftNavigationState, leftNavOpened, width }) => {
+const Feeds = ({ leftNavOpened, width }) => {
     return (
         <>
-            <FeedsLeftPanel leftNavigationState={leftNavigationState} width={leftNavOpened ? width.leftNavOpened : width.leftNavClosed} />
             <div className='FRSC h100' id='FeedsMain' width={leftNavOpened ? `calc(100% - ${width.leftNavOpened})` : `calc(100% - ${width.leftNavClosed})`} >
                 <FeedsMainPanel />
                 <FeedsRightPanel />
