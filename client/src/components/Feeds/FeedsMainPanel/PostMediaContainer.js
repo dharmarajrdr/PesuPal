73c726dafd839f6040bc3e804373f4184abc3a60
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-24 17:47:25 +0530

    Refactor PostMediaContainer to enhance image handling; implement ResourceNotFound component and streamline image rendering logic.

--- client/src/components/Feeds/FeedsMainPanel/PostMediaContainer.js
+++ client/src/components/Feeds/FeedsMainPanel/PostMediaContainer.js
@@ -1,19 +1,29 @@
+import { useState } from 'react';
 import './PostMediaContainer.css';
 import { useDispatch } from "react-redux";
 import { showFullScreenImageAt } from "../../../store/reducers/FullScreenImageSlice";
 
+const ResourceNotFound = ({ style }) => <div className='resource-not-found FRCC' style={{ ...style }}>
+    <i className='fa fa-exclamation-triangle w20 mR5'></i>
+    <span>Resource Not Found</span>
+</div>
+
+const ImageItem = ({ url, index, onClick, style }) => {
+    const [resourceNotFound, setResourceNotFound] = useState(false);
+    return resourceNotFound ? <ResourceNotFound style={{ ...style }} /> : <img src={url} className='media_image' style={{ ...style }} onClick={() => onClick(index)} onError={() => setResourceNotFound(true)} />
+}
+
 const OneImage = ({ mediaUrl, onClick }) => {
 
     return <div className='one_image_container FRCC w100'>
-        <img src={mediaUrl} className='media_image' onClick={onClick} />
+        <ImageItem url={mediaUrl} index={0} onClick={onClick} />
     </div>
 }
 
 const TwoImages = ({ mediaUrls, onClick }) => {
 
     return <div className='two_images_container FRCC w100'>
-        <img src={mediaUrls[0]} className='media_image' style={{ width: '50%' }} onClick={() => onClick(0)} />
-        <img src={mediaUrls[1]} className='media_image' style={{ width: '50%' }} onClick={() => onClick(1)} />
+        {mediaUrls.map((url, index) => <ImageItem key={index} url={url} index={index} onClick={onClick} style={{ width: '50%' }} />)}
     </div>
 }
 
@@ -21,7 +31,7 @@ const MoreThanTwoImages = ({ mediaUrls, onClick }) => {
 
     return <div className='more_than_two_images_container FRCS w100'>
         <div style={{ width: '75%' }} >
-            <img src={mediaUrls[0]} className='media_image w100' onClick={() => onClick(0)} />
+            <ImageItem url={mediaUrls[0]} index={0} onClick={onClick} style={{ width: '100%' }} />
         </div>
         <div style={{ width: '25%' }} className='FCSS'>
             {mediaUrls.slice(1, 3).map((url, index) => {
@@ -30,7 +40,7 @@ const MoreThanTwoImages = ({ mediaUrls, onClick }) => {
                     {showOverlay ? <div className='FRCC more-imagesoverlay'>
                         <span>+{mediaUrls.length - 3}</span>
                     </div> : null}
-                    <img key={index} src={url} className={`media_image w100 ${showOverlay ? 'partially-visible' : ''}`} onClick={() => onClick(index + 1)} />
+                    <ImageItem key={index} url={url} index={index + 1} onClick={onClick} style={{ width: '100%' }} />
                 </div>
             })}
         </div>
