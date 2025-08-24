a1bb217e43d5fc7425061fd61b5d1e6e205d301f
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-24 15:35:35 +0530

    Refactor FullScreenImageView component to manage media URLs and navigation more effectively; add error handling for missing resources.

--- client/src/components/FullScreenImageView.js
+++ client/src/components/FullScreenImageView.js
@@ -1,33 +1,54 @@
-import { useState } from 'react';
 import './FullScreenImageView.css';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { hideFullScreenImage } from '../store/reducers/FullScreenImageSlice';
+import { hideFullScreenImage, showImageAt } from '../store/reducers/FullScreenImageSlice';
+
+const ResourceNotFound = () => (
+    <div className='error-message'>
+        <i className='fa fa-exclamation-triangle'></i>
+        <span>Resource Not Found</span>
+    </div>
+);
 
 const FullScreenImageView = () => {
 
     const dispatch = useDispatch();
     const [showError, setShowError] = useState(false);
 
-    const { mediaUrl } = useSelector(state => state.fullScreenImage);
+    const { mediaUrls, currentIndex } = useSelector(state => state.fullScreenImage);
+    const [mediaUrl, setMediaUrl] = useState(mediaUrls[currentIndex]);
 
-    return mediaUrl ? (
-        <div className='entire-screen-overlay' id='full-screen-ticket-attachment-overlay' onClick={(e) => {
-            e.stopPropagation();
-            if (e.target.id === 'full-screen-ticket-attachment-overlay') {
-                dispatch(hideFullScreenImage());
-            }
-        }}>
+    const showPreviousNextIcons = mediaUrls.length > 1;
+
+    useEffect(() => {
+        setMediaUrl(mediaUrls[currentIndex]);
+    }, [currentIndex, mediaUrls]);
+
+    const toggleImageViewer = (e, toIndex) => {
+        e.stopPropagation();
+        if (toIndex >= 0 && toIndex < mediaUrls.length) {
+            dispatch(showImageAt(toIndex));
+        }
+    }
+
+    const closeFullScreenImage = (e) => {
+        e.stopPropagation();
+        if (e.target.id === 'full-screen-ticket-attachment-overlay') {
+            dispatch(hideFullScreenImage());
+        }
+    }
+
+    const enableLeftArrow = currentIndex > 0, enableRightArrow = currentIndex < mediaUrls.length - 1;
+
+    return mediaUrls.length > 0 ? (
+        <div className='entire-screen-overlay FRCB' id='full-screen-ticket-attachment-overlay' onClick={closeFullScreenImage}>
+            {showPreviousNextIcons && <div className={`FRCC full-screen-image-toggle-icons ${!enableLeftArrow ? 'disabled' : ''}`} onClick={(e) => toggleImageViewer(e, currentIndex - 1)}><i className='fa fa-chevron-left'></i></div>}
             <div id='full-screen-image-viewer' className='centerMe FRCC'>
-
-                {showError ? (
-                    <div className='error-message'>
-                        <i className='fa fa-exclamation-triangle'></i>
-                        <span>Resource Not Found</span>
-                    </div>
-                ) : (
+                {showError ? <ResourceNotFound /> : (
                     <img src={mediaUrl} onError={() => setShowError(true)} />
                 )}
             </div>
+            {showPreviousNextIcons && <div className={`FRCC full-screen-image-toggle-icons ${!enableRightArrow ? 'disabled' : ''}`} onClick={(e) => toggleImageViewer(e, currentIndex + 1)}><i className='fa fa-chevron-right'></i></div>}
         </div>
     ) : null;
 }
