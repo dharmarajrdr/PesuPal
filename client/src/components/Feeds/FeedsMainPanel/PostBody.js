3689ef57255a3bcce5a70fcd174e50f19b2e6d66
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-24 22:29:41 +0530

    Add PostBody.css for HTML content styling and refactor PostDescription component

--- client/src/components/Feeds/FeedsMainPanel/PostBody.js
+++ client/src/components/Feeds/FeedsMainPanel/PostBody.js
@@ -1,8 +1,17 @@
+import './PostBody.css';
 import Poll from "./Poll"
 import { NavLink } from "react-router-dom"
 import PostMediaContainer from "./PostMediaContainer"
 
-const PostDescription = ({ html }) => <div className="post-description html-content-renderer postContent" dangerouslySetInnerHTML={{ __html: html }} />
+const PostDescription = ({ html }) => {
+
+    html = html?.replace(/\t/mg, '<span style="padding-left: 2em;"></span>');   // `\t` -> indentation spaces
+    html = html?.replace(/\n/mg, '<br/>');  // `\n` -> line breaks
+
+    return (
+        <div className="post-description html-content-renderer postContent" dangerouslySetInnerHTML={{ __html: html }} />
+    );
+};
 
 const TagsContainer = ({ tags }) => {
     return <div className='FRCS tagsContainer'>
