844b6dac01e3c7f2aa8669d7fa17fc90ca9ab207
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-24 19:46:32 +0530

    Refactor model classes to replace @Data with @Getter, @Setter, and @EqualsAndHashCode for improved clarity and control over serialization behavior.

--- server/src/main/java/com/pesupal/server/model/post/Post.java
+++ server/src/main/java/com/pesupal/server/model/post/Post.java
@@ -6,13 +6,17 @@ import com.pesupal.server.model.PublicAccessModel;
 import com.pesupal.server.model.org.Org;
 import com.pesupal.server.model.user.OrgMember;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
+@EqualsAndHashCode(callSuper = false)
 public class Post extends PublicAccessModel {
 
     @ManyToOne
--- server/src/main/java/com/pesupal/server/model/user/OrgMember.java
+++ server/src/main/java/com/pesupal/server/model/user/OrgMember.java
@@ -6,18 +6,24 @@ import com.pesupal.server.model.PublicAccessModel;
 import com.pesupal.server.model.department.Department;
 import com.pesupal.server.model.org.Org;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity
+@EqualsAndHashCode(callSuper = false)
 public class OrgMember extends PublicAccessModel {
 
     @ManyToOne
+    @JsonIgnore
     private Org org;
 
     @ManyToOne
+    @JsonIgnore
     private User user;
 
     @Column(nullable = false)
