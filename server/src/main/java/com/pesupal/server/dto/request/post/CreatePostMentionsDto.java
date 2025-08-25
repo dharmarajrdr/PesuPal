89aa3cf341530ee401d30167c662fc8c03fc5708
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-25 22:08:58 +0530

    Refactor CreatePostMentionsDto to use Set instead of List for data field

--- server/src/main/java/com/pesupal/server/dto/request/post/CreatePostMentionsDto.java
+++ server/src/main/java/com/pesupal/server/dto/request/post/CreatePostMentionsDto.java
@@ -2,12 +2,12 @@ package com.pesupal.server.dto.request.post;
 
 import lombok.Data;
 
-import java.util.List;
+import java.util.Set;
 
 @Data
 public class CreatePostMentionsDto {
 
     private String label;
 
-    private List<String> data;
+    private Set<String> data;
 }
