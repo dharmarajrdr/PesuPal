ae90eea55a62bbdf7af91869ad7fbdd32b14a897
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-25 22:47:12 +0530

    Refactor ShareWithSchedule component to remove isPostCreation prop and simplify rendering logic

--- client/src/components/Feeds/FeedsMainPanel/ShareWithSchedule.js
+++ client/src/components/Feeds/FeedsMainPanel/ShareWithSchedule.js
@@ -3,7 +3,7 @@ import { useDispatch } from 'react-redux';
 import SchedulePicker from '../../Chat/ConversationScreen/SchedulePicker';
 import { hideConfirmationPopup, showConfirmationPopup } from '../../../store/reducers/ConfirmationPopupSlice';
 
-const ShareWithSchedule = ({ onShare, onSchedule, isPostCreation }) => {
+const ShareWithSchedule = ({ onShare, onSchedule, scheduledAt }) => {
 
     const dispatch = useDispatch();
     const [showPicker, setShowPicker] = useState(false);
@@ -46,14 +46,14 @@ const ShareWithSchedule = ({ onShare, onSchedule, isPostCreation }) => {
     }
 
     return (
-        <div className={`${isPostCreation ? 'create-post' : 'update-post'}-wrapper FCSS w100`}>
+        <div className={`create-post-wrapper FCSS w100`}>
             <div className="FRCC w100" id='share-post-button-wrapper'>
-                <button className="share-main" onClick={shareClickHandler}>{isPostCreation ? 'Share' : 'Update'}</button>
-                {isPostCreation && <i className={`fa ${showSchedule ? 'fa-chevron-up' : 'fa-chevron-down'}`} id="share-chevron" onClick={() => setShowSchedule(prev => !prev)}></i>}
+                <button className="share-main" onClick={shareClickHandler}>Share</button>
+                {<i className={`fa ${showSchedule ? 'fa-chevron-up' : 'fa-chevron-down'}`} id="share-chevron" onClick={() => setShowSchedule(prev => !prev)}></i>}
             </div>
 
-            {onSchedule && showPicker && <SchedulePicker onSchedule={scheduleClickHandler} showPicker={showPicker} setShowPicker={setShowPicker} />}
-            {isPostCreation && <div className={`w100 schedule-slide ${showSchedule ? 'slide-visible' : ''}`}>
+            {showPicker && <SchedulePicker onSchedule={scheduleClickHandler} showPicker={showPicker} setShowPicker={setShowPicker} defaultTime={scheduledAt} />}
+            {<div className={`w100 schedule-slide ${showSchedule ? 'slide-visible' : ''}`}>
                 <button className="schedule-btn" onClick={() => setShowPicker(!showPicker)}>Schedule</button>
             </div>}
         </div>
