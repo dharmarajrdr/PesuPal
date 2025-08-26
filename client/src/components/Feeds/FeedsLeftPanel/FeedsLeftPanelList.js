da79c10ffd0883e3dd084655e90787fba1c460db
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-26 20:58:26 +0530

    Refactor FeedsLeftPanelList to remove unused entries and streamline feed options

--- client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelList.js
+++ client/src/components/Feeds/FeedsLeftPanel/FeedsLeftPanelList.js
@@ -2,9 +2,14 @@ export default [
     {
         'title': 'My Feed',
         'icon': 'fa-regular fa-newspaper',
-        'is_active': true,
         'color': '#0000FF',
         'route': '/feeds'
+    },
+    {
+        'title': 'Scheduled Posts',
+        'icon': 'fa-regular fa-calendar-days',
+        'color': '#FFC0CB',
+        'route': '/feeds/scheduled-posts'
     },
     {
         'title': 'Bookmarks',
@@ -13,33 +18,15 @@ export default [
         'route': '/feeds/bookmarks'
     },
     {
-        'title': 'Read Later',
-        'icon': 'fa-regular fa-clock',
-        'color': '#FFA500',
-        'route': '/feeds/read-later'
-    },
-    {
-        'title': 'My Scheduled Posts',
-        'icon': 'fa-regular fa-calendar-days',
-        'color': '#FFC0CB',
-        'route': '/feeds/scheduled-posts'
-    },
-    {
-        'title': 'Corp Announcements',
+        'title': 'Announcements',
         'icon': 'fa-solid fa-bullhorn',
         'color': '#800080',
-        'route': '/feeds/corp-announcements'
+        'route': '/feeds/announcements'
     },
     {
         'title': 'Mandatory Reads',
         'icon': 'fa-solid fa-triangle-exclamation',
         'color': '#ff0000',
         'route': '/feeds/mandatory-reads'
-    },
-    {
-        'title': 'Quick Links',
-        'icon': 'fa-solid fa-link',
-        'color': '#008080',
-        'route': '/feeds/quick-links'
     }
 ]
