4eb5096aea25b544eb5ac3325187a7b25b7d8ec4
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-31 11:19:24 +0530

    Change status field type to MemberStatus enum in UserBasicInfoDto

--- server/src/main/java/com/pesupal/server/dto/response/UserBasicInfoDto.java
+++ server/src/main/java/com/pesupal/server/dto/response/UserBasicInfoDto.java
@@ -1,6 +1,7 @@
 package com.pesupal.server.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.pesupal.server.enums.MemberStatus;
 import com.pesupal.server.model.user.OrgMember;
 import lombok.Data;
 
@@ -20,7 +21,7 @@ public class UserBasicInfoDto {
 
     private String department;
 
-    private String status;
+    private MemberStatus status;
 
     private String email;
 
