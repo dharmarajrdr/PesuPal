7e137973536fb556f143de591d12a064bf593b56
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-31 10:32:59 +0530

    Refactor import statements in Department class for cleaner code

--- server/src/main/java/com/pesupal/server/model/department/Department.java
+++ server/src/main/java/com/pesupal/server/model/department/Department.java
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pesupal.server.model.PublicAccessModel;
 import com.pesupal.server.model.org.Org;
 import com.pesupal.server.model.user.OrgMember;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 @Entity
@@ -26,4 +25,8 @@ public class Department extends PublicAccessModel {
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnore
     private OrgMember head;
+
+    @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<OrgMember> members;
 }
