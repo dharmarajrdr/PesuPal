46ea64940b9a2632cdc767c182f33d1c835b77ad
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-31 13:17:00 +0530

    Refactor informPresence method to manage online status using Redis and notify subscribers

--- server/src/main/java/com/pesupal/server/service/implementations/org/PresenceServiceImpl.java
+++ server/src/main/java/com/pesupal/server/service/implementations/org/PresenceServiceImpl.java
@@ -1,20 +1,24 @@
 package com.pesupal.server.service.implementations.org;
 
+import com.pesupal.server.dto.response.org.UserPresenceUpdateDto;
 import com.pesupal.server.enums.MemberStatus;
 import com.pesupal.server.helpers.CurrentValueRetriever;
 import com.pesupal.server.model.user.OrgMember;
-import com.pesupal.server.repository.org.OrgMemberRepository;
 import com.pesupal.server.service.interfaces.org.PresenceService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
+
 @Service
 @AllArgsConstructor
 public class PresenceServiceImpl extends CurrentValueRetriever implements PresenceService {
 
-    private final OrgMemberRepository orgMemberRepository;
     private final RedisTemplate<String, Object> redisTemplate;
+    private final PresenceUpdateSubscriber presenceUpdateSubscriber;
+
+    private static final String ORG_MEMBER_PRESENCE_KEY_PREFIX = "presence:org_member:";
 
     /**
      * Informs presence of an organization member.
@@ -23,8 +27,19 @@ public class PresenceServiceImpl extends CurrentValueRetriever implements Presen
     public void informPresence() {
 
         OrgMember orgMember = getCurrentOrgMember();
-        orgMember.setStatus(MemberStatus.AVAILABLE);
-        orgMemberRepository.save(orgMember);
+        String orgMemberId = orgMember.getPublicId();
+        String orgId = orgMember.getOrg().getPublicId();
+
+        String presenceKey = ORG_MEMBER_PRESENCE_KEY_PREFIX + orgId + ":" + orgMemberId;
+        boolean isUserAlreadyOnline = redisTemplate.opsForValue().get(presenceKey) != null;
+
+        if (isUserAlreadyOnline) { // Status remains ONLINE, just refresh the expiration
+            redisTemplate.expire(presenceKey, Duration.ofSeconds(20));
+        } else {    // Status changed from OFFLINE to ONLINE
+            redisTemplate.opsForValue().set(presenceKey, "ONLINE");
+            redisTemplate.expire(presenceKey, Duration.ofSeconds(20));
+            presenceUpdateSubscriber.onStatusUpdate(new UserPresenceUpdateDto(orgId, orgMemberId, MemberStatus.AVAILABLE));
+        }
     }
 
     /**
