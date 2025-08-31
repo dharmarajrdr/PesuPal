f23b9bba6aa179539a420e168582e8491868cbac
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-31 15:21:28 +0530

    Refactor usePresenceService to improve user online status handling and integrate Redux for member status updates

--- client/src/hooks/usePresenceService.js
+++ client/src/hooks/usePresenceService.js
@@ -1,14 +1,24 @@
+import { useDispatch } from "react-redux";
 import { apiRequest } from "../http_request";
+import { updateMemberStatus } from "../store/reducers/DepartmentSlice";
 
 export default function usePresenceService() {
 
+    const dispatch = useDispatch();
+    const setUserOnline = () => {
+        apiRequest(`/api/v1/presence/inform`, 'PATCH').catch(({ message }) => {
+            console.error(message);
+        });
+    };
+
     return {
-        setUserOnline: () => {
-            apiRequest(`/api/v1/presence/inform`, 'PATCH').then(({ message }) => {
-                console.log(message);
-            }).catch(({ message }) => {
-                console.error(message);
-            });
+        informUserOnlineAtInterval: (seconds) => {
+            setUserOnline();
+            const interval = setInterval(setUserOnline, seconds * 1000);
+            return () => clearInterval(interval);
+        },
+        onPresenceUpdate: (message) => {
+            dispatch(updateMemberStatus({ 'userId': message.orgMemberId, 'status': message.memberStatus }));
         }
     };
 }
