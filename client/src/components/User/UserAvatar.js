8f31a15219f11ae56a7064f07771815917884eb3
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-08-31 16:12:49 +0530

    [FIX] Alignment issues

--- client/src/components/User/UserAvatar.js
+++ client/src/components/User/UserAvatar.js
@@ -9,16 +9,18 @@ const UserAvatar = ({ displayPicture, displayName, userId }) => {
     const dispatch = useDispatch();
     const [imageError, setImageError] = useState(false);
 
+    const showUserProfile = () => { dispatch(showProfile(userId)); }
+
     return (imageError || !displayPicture) ? (
         displayName ? (
-            <p className="user-avatar-placeholder-first-character img_40_40" style={{ backgroundColor: utils.uniqueColorGenerator(displayName) }}>
+            <p className="user-avatar-placeholder-first-character img_40_40" style={{ backgroundColor: utils.uniqueColorGenerator(displayName) }} onClick={showUserProfile}>
                 {displayName.charAt(0).toUpperCase()}
             </p>
         ) : (
             <i className="fa fa-user-circle user-avatar-placeholder" aria-hidden="true" />
         )
     ) : (
-        <img src={displayPicture} alt="User" className="img_40_40 cursP objectFitCover" onError={() => setImageError(true)} onClick={() => { dispatch(showProfile(userId)); }} />
+        <img src={displayPicture} alt="User" className="img_40_40 cursP objectFitCover" onError={() => setImageError(true)} onClick={showUserProfile} />
     );
 };
 
