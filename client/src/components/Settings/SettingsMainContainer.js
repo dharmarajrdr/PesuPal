b88af2921357cd38f774f2c8b56e2827cadb7497
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-09-04 16:37:00 +0530

    Update navigation titles and routes for roles and permissions in settings

--- client/src/components/Settings/SettingsMainContainer.js
+++ client/src/components/Settings/SettingsMainContainer.js
@@ -1,6 +1,7 @@
 import GeneralLayout from './general/GeneralLayout'
 import AccountLayout from './account/AccountLayout'
 import PermissionDenied from '../Auth/PermissionDenied'
+import OrgRolesLayout from '../OrgRules/OrgRolesLayout'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import SubscriptionPlan from './subscription/SubscriptionPlan'
 import ManagePeopleLayout from '../ManagePeople/ManagePeopleLayout'
@@ -15,6 +16,7 @@ const SettingsMainContainer = ({ width }) => {
                 <Route path="/account/*" element={<AccountLayout />} />
                 <Route path="/pricing/*" element={<SubscriptionPlan />} />
                 <Route path='/manage-people/*' element={<ManagePeopleLayout />} />
+                <Route path='/roles-permissions' element={<OrgRolesLayout />} />
                 <Route path="*" element={<PermissionDenied />} />
             </Routes>
         </div>
