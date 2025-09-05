a64ab9bab9e29fc3b3c25216cd6600007742f544
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-09-05 09:01:14 +0530

    Add state management for new role layout visibility in OrgRolesLayout

--- client/src/components/OrgRules/OrgRolesLayout.js
+++ client/src/components/OrgRules/OrgRolesLayout.js
@@ -1,14 +1,23 @@
 import './OrgRolesLayout.css';
+import { useState } from 'react';
 import OrgRolesTable from './OrgRolesTable';
 import OrgRolesHeader from './OrgRolesHeader';
+import NewRulesLayout from './NewRulesLayout';
 
 const OrgRolesLayout = () => {
+
+    const [showNewRoleLayout, setShowNewRoleLayout] = useState(false);
+
+    const onCloseNewRoleLayout = () => {
+        setShowNewRoleLayout(false);
+    }
 
     return (
         <div id='org-roles-layout' className='w100'>
             <div id='org-roles-content'>
-                <OrgRolesHeader />
+                <OrgRolesHeader setShowNewRoleLayout={setShowNewRoleLayout} />
                 <OrgRolesTable />
+                {showNewRoleLayout && <NewRulesLayout onCloseNewRoleLayout={onCloseNewRoleLayout} />}
             </div>
         </div>
     )
